5be2330bc7c68f07eade4fb086503dc06484c7c9
Baochun Li <bli@ece.toronto.edu>
2020-10-23 00:55:36 +0800

    Cleaning up unit tests for distribution generators.

--- tests/dists_tests.py
+++ tests/dists_tests.py
@@ -1,12 +1,10 @@
-"""Tests for distribution generators in Plato."""
+"""Tests for distribution generators."""
 
 import os
 import sys
 import collections
 import unittest
-import numpy as np
-import random
-from scipy.stats import norm, shapiro
+from scipy.stats import shapiro
 
 # To import modules from the parent directory
 currentdir = os.path.dirname(os.path.realpath(__file__))
@@ -16,7 +14,9 @@ sys.path.append(parentdir)
 from utils import dists
 
 class DistsTest(unittest.TestCase):
+    """Tests for distribution generators."""
     def test_uniform_dist(self):
+        """Test the uniform distribution generator."""
         dist, __ = dists.uniform(1000, 5)
         self.assertSequenceEqual(dist, [200] * 5)
 
@@ -25,11 +25,13 @@ class DistsTest(unittest.TestCase):
         self.assertTrue(collections.Counter(dist) == collections.Counter([34, 34, 33]))
 
     def test_normal_dist(self):
+        """Test the normal distribution generator."""
         dist, samples = dists.normal(100, 10)
         print('The distribution is {}'.format(dist))
-        print('p-value from the Shapiro Wilk Test for normality is {0:.2f}'.format(shapiro(samples).pvalue))
+        print('p-value from the Shapiro Wilk Test for normality is {0:.2f}'
+            .format(shapiro(samples).pvalue))
         self.assertTrue(shapiro(samples).pvalue > 0.05)
 
 
 if __name__=='__main__':
-      unittest.main()
+    unittest.main()
