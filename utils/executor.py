e5f6240a05a02604efb92d99ef3c9b24bd3b8808
Baochun Li <bli@ece.toronto.edu>
2020-10-28 12:22:53 +0800

    Improved multiprocessing: using callbacks rather than get() to collect results.

---
+++ utils/executor.py
@@ -0,0 +1,27 @@
+"""
+This class uses torch.multiprocessing to launch multiple processes, each with its 
+own Python interpreter and starts from the starting function provided.
+"""
+
+import logging
+import torch
+
+class Executor:
+    """Launches multiple processes."""
+
+    def __init__(self, process_num):
+        self.reports = []
+        self.pool = torch.multiprocessing.Pool(process_num)
+
+    def prompt(self, report):
+        """Callback function, called when the process finishes."""
+        logging.info('Client #%s finished', report.client_id)
+        if report:
+            self.reports.append(report)
+
+    def schedule(self, function, args):
+        self.pool.apply_async(function, args=args, callback=self.prompt)
+
+    def wait(self):
+        self.pool.close()
+        self.pool.join()
