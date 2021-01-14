d77fd104fc5332b678d41b6ea90a75a87eb72e61
Baochun Li <bli@ece.toronto.edu>
2021-01-14 14:41:37 +0800

    Supported an adaptive algorithm for updating sync masks in APF.

--- config.py
+++ config.py
@@ -153,10 +153,11 @@ class Config:
             'dataset', 'data_path', 'model', 'optimizer', 'learning_rate',
             'weight_decay', 'momentum', 'num_layers', 'num_classes',
             'cut_layer', 'epsilon', 'lr_gamma', 'lr_milestone_steps',
-            'lr_warmup_steps'
+            'lr_warmup_steps', 'moving_average_alpha', 'stability_threshold'
         ]
         defaults = ('basic', 0, 0.9, 0, 128, 'MNIST', './data', 'mnist_cnn',
-                    'SGD', 0.01, 0.0, 0.9, 40, 10, '', 1.0, 0.0, '', '')
+                    'SGD', 0.01, 0.0, 0.9, 40, 10, '', 1.0, 0.0, '', '', 0.99,
+                    0.05)
         params = Config.extract_section('training', fields, defaults)
 
         Config.training = namedtuple('training', fields)(*params)
