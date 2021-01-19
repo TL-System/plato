714aaebd785d811d08bea1fafbcf0505b5294024
Baochun Li <bli@ece.toronto.edu>
2021-01-19 22:44:28 +0800

    Replaced MNIST and FashionMNIST CNN models with LeNet-5.

--- models/registry.py
+++ models/registry.py
@@ -3,10 +3,10 @@ Having a registry of all available classes is convenient for retrieving an insta
 based on a configuration at run-time.
 """
 
-from models import mnist_cnn, fashion_cnn, cifar_resnet, cifar_wideresnet, cifar_vgg
+from models import lenet5_pytorch, fashion_cnn, cifar_resnet, cifar_wideresnet, cifar_vgg
 
 registered_models = [
-    mnist_cnn.Model, fashion_cnn.Model, cifar_resnet.Model,
+    lenet5_pytorch.Model, fashion_cnn.Model, cifar_resnet.Model,
     cifar_wideresnet.Model, cifar_vgg.Model
 ]
 
