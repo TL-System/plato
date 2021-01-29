d2453b04b3839db934f9504194eac502454de4d4
Baochun Li <bli@ece.toronto.edu>
2021-01-29 17:39:24 +0800

    Simplified the FedNova client; Fixed an issue where training parameters cannot be changed before training starts.

--- clients/fednova.py
+++ clients/fednova.py
@@ -4,13 +4,9 @@ generated and communicated to the server at each communication round.
 """
 
 import logging
-import time
 import random
 
 from dataclasses import dataclass
-from trainers import registry as trainers_registry
-from models import registry as models_registry
-from trainers import registry as trainers_registry
 from config import Config
 from clients import simple
 
@@ -18,7 +14,7 @@ from clients import simple
 @dataclass
 class Report(simple.Report):
     """A client report containing the number of local epochs."""
-    epochs: list
+    epochs: int
 
 
 class FedNovaClient(simple.SimpleClient):
@@ -26,21 +22,12 @@ class FedNovaClient(simple.SimpleClient):
     and the number of local epochs."""
     def __init__(self):
         super().__init__()
-        self.epochs = None
         self.pattern = None
         self.max_local_iter = None
         random.seed(3000 + int(self.client_id))
 
-    def configure(self):
-        """Prepare this client for training."""
-        model_name = Config().trainer.model
-        self.model = models_registry.get(model_name)
-        # seperate models with trainers
-        self.trainer = trainers_registry.get(self.model, self.client_id)
-
     async def train(self):
-        """The machine learning training workload on a client."""
-        training_start_time = time.time()
+        """FedNova clients use different number of local epochs."""
 
         # generate the number of local epochs randomly
         if Config().algorithm.pattern == "constant":
@@ -50,26 +37,13 @@ class FedNovaClient(simple.SimpleClient):
                                           Config().algorithm.max_local_epochs)
 
         logging.info('[Client #%s] Training with %d epoches.', self.client_id,
-                     epochs)
+                     local_epochs)
 
         # Perform model training for a specific number of epoches
         Config().trainer = Config().trainer._replace(epochs=local_epochs)
-        self.trainer.train(self.trainset)
 
-        # Extract model weights and biases
-        weights = self.trainer.extract_weights()
+        report = await super().train()
 
-        # Generate a report for the server, performing model testing if applicable
-        if Config().clients.do_test:
-            accuracy = self.trainer.test(self.testset)
-        else:
-            accuracy = 0
-
-        training_time = time.time() - training_start_time
-        data_loading_time = 0
-        if not self.data_loading_time_sent:
-            data_loading_time = self.data_loading_time
-            self.data_loading_time_sent = True
-
-        return Report(self.client_id, len(self.data), weights, accuracy,
-                      training_time, data_loading_time, self.epochs)
+        return Report(report.client_id, report.num_samples, report.weights,
+                      report.accuracy, report.training_time,
+                      report.data_loading_time, local_epochs)
