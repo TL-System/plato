2c3989e5c7cfbf20d81743520da25371c76b875a
Baochun Li <bli@ece.toronto.edu>
2021-02-03 17:11:15 +0800

    Improved dataset downloading with the ability to uncompress .zip archives.

--- datasets/cinic10.py
+++ datasets/cinic10.py
@@ -5,6 +5,7 @@ https://github.com/BayesWatch/cinic-10
 """
 
 import os
+import logging
 from torchvision import datasets, transforms
 
 from datasets import base
@@ -15,17 +16,17 @@ class Dataset(base.Dataset):
     def __init__(self, path):
         super().__init__(path)
 
-        self.cinic_directory = path + '/CINIC10'
+        self.cinic_path = path + '/CINIC10'
 
         # Download and extract CINIC-10 dataset if haven't
-        if not os.path.exists(self.cinic_directory):
-            os.makedirs(self.cinic_directory)
+        if not os.path.exists(self.cinic_path):
+            os.makedirs(self.cinic_path)
 
-        if not os.path.exists(self.cinic_directory + '.tar.gz'):
-            print('Downloading the CINIC-10 dataset. This takes a while.')
+        if not os.path.exists(self.cinic_path + '.tar.gz'):
+            logging.info('Downloading the CINIC-10 dataset. This takes a while.')
 
             url = 'https://datashare.is.ed.ac.uk/bitstream/handle/10283/3192/CINIC-10.tar.gz'
-            Dataset.download(url, self.cinic_directory)
+            Dataset.download(url, self.cinic_path)
 
         self._transform = transforms.Compose([
             transforms.ToTensor(),
@@ -46,9 +47,9 @@ class Dataset(base.Dataset):
         return 10
 
     def get_train_set(self):
-        return datasets.ImageFolder(root=self.cinic_directory + '/train',
+        return datasets.ImageFolder(root=self.cinic_path + '/train',
                                     transform=self._transform)
 
     def get_test_set(self):
-        return datasets.ImageFolder(root=self.cinic_directory + '/test',
+        return datasets.ImageFolder(root=self.cinic_path + '/test',
                                     transform=self._transform)
