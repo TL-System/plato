806bc17644c60770a7879a37a87b7bb472fcacd4
Baochun Li <bli@ece.toronto.edu>
2021-04-20 15:56:58 +0800

    Updated tests to be consistent with the latest codebase.

--- tests/client_tests.py
+++ tests/client_tests.py
@@ -5,21 +5,24 @@ import os
 import sys
 import asyncio
 
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
 # To import modules from the parent directory
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 
 from config import Config
-from clients import SimpleClient
+from clients import simple
 
 
 async def test_training(client):
     print("Testing training on the client.")
 
-    report = await client.train()
-    print(report)
-
+    report, weights = await client.train()
+    print("Client model trained.")
+    print(f"Report to be sent to the server: {report}")
+    print(f"Model weights: {weights}")
 
 def main():
     """Starting a client to connect to the server via WebSockets."""
@@ -27,8 +30,8 @@ def main():
 
     loop = asyncio.get_event_loop()
     coroutines = []
-    client = SimpleClient()
-    client.client_id = "1"
+    client = simple.Client()
+    client.client_id = 1
     client.configure()
     client.load_data()
 
@@ -41,7 +44,7 @@ def main():
         print(exception)
         sys.exit()
 
-    os.remove("./running_trainers")
+    os.remove("./running_trainers.sqlitedb")
 
 
 if __name__ == "__main__":
