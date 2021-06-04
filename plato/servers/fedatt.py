b5d414d76f3e2016b891b121d9ed825d9652c2f0
Baochun Li <bli@ece.toronto.edu>
2021-06-04 12:36:33 +0800

    Fixed an issue to make the FedAtt server compatible with recent changes.

--- plato/servers/fedatt.py
+++ plato/servers/fedatt.py
@@ -3,30 +3,30 @@ A federated learning server using FedAtt.
 
 Reference:
 
-Ji et al., "Learning Private Neural Language Modeling with Attentive Aggregation"
-(https://arxiv.org/pdf/1812.07108.pdf)
+Ji et al., "Learning Private Neural Language Modeling with Attentive Aggregation,"
+in the Proceedings of the 2019 International Joint Conference on Neural Networks (IJCNN).
+
+https://arxiv.org/pdf/1812.07108.pdf
 """
 from collections import OrderedDict
 
 from plato.servers import fedavg
 
-import logging
 import torch
 import torch.nn.functional as F
-import numpy as np
 
 
 class Server(fedavg.Server):
     """A federated learning server using the FedAtt algorithm."""
-    async def federated_averaging(self, reports):
+    async def federated_averaging(self, updates):
         """Aggregate weight updates from the clients using FedAtt."""
-        # Extracting updates from the reports
-        updates = self.extract_client_updates(reports)
+        # Extract weights from the updates
+        weights_received = self.extract_client_updates(updates)
 
         # Performing attentive aggregating
         att_update = {
             name: self.trainer.zeros(weights.shape)
-            for name, weights in updates[0].items()
+            for name, weights in weights_received[0].items()
         }
 
         # Extract baseline model weights
@@ -35,24 +35,21 @@ class Server(fedavg.Server):
         # Calculate attention
         atts = OrderedDict()
         for name, weight in baseline_weights.items():
-            atts[name] = self.trainer.zeros(len(updates))
-            for i, update in enumerate(updates):
+            atts[name] = self.trainer.zeros(len(weights_received))
+            for i, update in enumerate(weights_received):
                 delta = update[name]
                 atts[name][i] = torch.linalg.norm(weight - delta)
             atts[name] = F.softmax(atts[name], dim=0)
 
         for name, weight in baseline_weights.items():
             att_weight = self.trainer.zeros(weight.shape)
-            for i, update in enumerate(updates):
+            for i, update in enumerate(weights_received):
                 delta = update[name]
                 att_weight += (weight - delta).mul(atts[name][i])
-            #TODO: plus random noise?
-            # epsilon as an argument?
+
             att_update[name] = -att_weight.mul(1.2)
 
         # Load updated weights into model
-        updated_weights = OrderedDict()
+        self.updated_weights = OrderedDict()
         for name, weight in baseline_weights.items():
-            updated_weights[name] = weight + att_update[name]
-
-        return updated_weights
+            self.updated_weights[name] = weight + att_update[name]
--- plato/servers/fednova.py
+++ plato/servers/fednova.py
@@ -4,8 +4,11 @@ A federated learning server using FedNova.
 Reference:
 
 Wang et al., "Tackling the Objective Inconsistency Problem in Heterogeneous Federated
-Optimization" (https://arxiv.org/pdf/2007.07481.pdf)
+Optimization", in the Proceedings of NeurIPS 2020.
+
+https://proceedings.neurips.cc/paper/2020/hash/564127c03caab942e503ee6f810f54fd-Abstract.html
 """
+
 from collections import OrderedDict
 
 from plato.servers import fedavg
@@ -13,34 +16,33 @@ from plato.servers import fedavg
 
 class Server(fedavg.Server):
     """A federated learning server using the FedNova algorithm. """
-    async def federated_averaging(self, reports):
+    async def federated_averaging(self, updates):
         """Aggregate weight updates from the clients using FedNova."""
-        # Extracting updates from the reports
-        updates = self.extract_client_updates(reports)
+        # Extracting weights from the updates
+        weights_received = self.extract_client_updates(updates)
 
         # Extracting the total number of samples
         self.total_samples = sum(
-            [report.num_samples for (report, __) in reports])
+            [report.num_samples for (report, __) in updates])
 
-        # Extracting the number of local epoches, tau_i, from the reports
-        local_epochs = [report.epochs for (report, __) in reports]
+        # Extracting the number of local epoches, tau_i, from the updates
+        local_epochs = [report.epochs for (report, __) in updates]
 
         # Performing weighted averaging
-        avg_update = avg_update = {
+        avg_update = {
             name: self.trainer.zeros(weights.shape)
-            for name, weights in updates[0].items()
+            for name, weights in weights_received[0].items()
         }
 
         tau_eff = 0
-        for i, update in enumerate(updates):
-            report, __ = reports[i]
+        for i, update in enumerate(weights_received):
+            report, __ = updates[i]
             num_samples = report.num_samples
             tau_eff_ = local_epochs[i] * num_samples / self.total_samples
             tau_eff += tau_eff_
-            #tau_effs.append(tau_eff)
 
-        for i, update in enumerate(updates):
-            report, __ = reports[i]
+        for i, update in enumerate(weights_received):
+            report, __ = updates[i]
             num_samples = report.num_samples
 
             for name, delta in update.items():
@@ -49,7 +51,7 @@ class Server(fedavg.Server):
                                              ) * tau_eff / local_epochs[i]
 
         # Extract baseline model weights
-        baseline_weights = self.trainer.extract_weights()
+        baseline_weights = self.algorithm.extract_weights()
 
         # Load updated weights into model
         updated_weights = OrderedDict()
