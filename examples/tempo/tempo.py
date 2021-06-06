253582d3bd9273da90000942002dfe992320b0e1
Baochun Li <bli@ece.toronto.edu>
2021-06-06 15:01:52 +0800

    Added .env so that Pylance knows the correct import directories.

--- examples/tempo/tempo.py
+++ examples/tempo/tempo.py
@@ -5,13 +5,11 @@ import os
 
 os.environ['config_file'] = 'tempo_MNIST_lenet5.yml'
 
-import tempo_client
 import tempo_server
 
 
 def main():
     """ A Plato federated learning training session using the Tempo algorithm. """
-    #client = tempo_client.Client()
     server = tempo_server.Server()
     server.run()
 
