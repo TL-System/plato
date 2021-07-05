f2047eac71374ddb9e5bf3366d4de44fc7248ddb
Fei Wang <silviafey.wang@utoronto.ca>
2021-07-06 04:18:59 +0800

    Fixed bugs in the AFL server and trainer.
    Now the  functionality is completed.

--- examples/afl/afl_server.py
+++ examples/afl/afl_server.py
@@ -12,6 +12,7 @@ from collections import OrderedDict
 from plato.servers import fedavg
 
 import logging
+import numpy as np
 import torch
 import torch.nn.functional as F
 import math
@@ -23,8 +24,6 @@ class Server(fedavg.Server):
     """A federated learning server using the AFL algorithm."""
     def __init__(self, model=None, algorithm=None, trainer=None):
         super().__init__(model, algorithm, trainer)
-        self.local_valuations = {}
-        self.sample_probs = []
         # The proportion of clients with the smallest valuations to be set to negative infinities
         self.alpha1 = 0.75
         # The softmax temperature used in distribution
@@ -43,31 +42,26 @@ class Server(fedavg.Server):
         for i, update in enumerate(weights_received):
             report, __ = updates[i]
             client_id = self.selected_clients[i]
-            self.local_valuations[client_id] = report.valuation
+            self.clients[client_id]["valuation"] = report.valuation
         
         return update
 
     def calc_sample_distribution(self):
         """Calculate the sampling probability of each client for the next round."""
-        # New clients connected
-        if len(self.sample_probs) != len(self.clients):
-            self.sample_probs = [None] * len(self.clients)
-            logging.info("_____len samples probs %d", len(self.sample_probs))
-            for client_id in dict(self.clients).keys():
-                if client_id not in self.local_valuations.keys():
-                    logging.info("_____smallest client id#%d", client_id)
-                    self.local_valuations[client_id] = -float("inf")
+        # Initialize valuations and probabilities when new clients are connected
+        for client_id, client in self.clients.items():
+            if "valuation" not in client:
+                client["valuation"] = -float("inf")
+            if "prob" not in client:
+                client["prob"] = 0.0
 
         # For a proportion of clients with smallest valuations, reset these valuations to negative infinities
-        num = int(self.alpha1 * len(self.clients))
-        smallest_valuations = dict(sorted(self.local_valuations.items(), key=lambda item: item[1])[:num])
-        # smallest_valuations = dict(sorted(self.local_valuations.items, key=itemgetter(1))[:num])
+        num_smallest = int(self.alpha1 * len(self.clients))
+        smallest_valuations = dict(sorted(self.clients.items(), key=lambda item: item[1]["valuation"])[:num_smallest])
         for client_id in smallest_valuations.keys():
-            logging.info("_____smallest client id#%d", client_id)
-            self.local_valuations[client_id] = -float("inf")
-        for k in range(len(self.clients)):
-            self.sample_probs[k] = math.exp(self.alpha2 * self.local_valuations[k+1])
-            logging.info("_____sample probs k: %d", self.sample_probs[k])
+            self.clients[client_id]["valuation"] = -float("inf")
+        for client_id, client in self.clients.items():
+            client["prob"] = math.exp(self.alpha2 * client["valuation"])
 
     def choose_clients(self):
         """Choose a subset of the clients to participate in each round."""
@@ -75,12 +69,24 @@ class Server(fedavg.Server):
         self.calc_sample_distribution()
         # 1. Sample a subset of the clients according to the sampling distribution
         num1 = int(math.floor((1 - self.alpha3) * self.clients_per_round))
-        subset1 = random.choices(list(self.clients), weights=self.sample_probs, k=num1)
+        pool = list(self.clients)
+        probs = np.array([self.clients[client_id]["prob"] for client_id in pool])
+        if probs.sum() != 0.0:
+            probs /= probs.sum()
+        else:
+            probs = None
+        subset1 = np.random.choice(pool, num1, p=probs,replace=False).tolist()
+        for i in subset1:
+            logging.info("client in subset1: %s", i)
         # 2. Sample a subset of the remaining clients uniformly at random
         num2 = self.clients_per_round - num1
-        remaining = list(self.clients)
-        for client in subset1:
-            remaining.remove(client)
+        remaining = pool
+        for client_id in subset1:
+            remaining.remove(client_id)
+        for i in remaining:
+            logging.info("client in remaining: %s", i)
         subset2 = random.sample(remaining, num2)
-        # 3. Chosed clients are the union of these two subsets
+        for i in subset2:
+            logging.info("client in subset2: %s", i)
+        # 3. Selected clients are the union of these two subsets
         return subset1 + subset2
