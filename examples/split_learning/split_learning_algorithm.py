33723f2b6a1f8aca85fcbe01ef6d4fd7ffba40f2
wykjLDF <812093265@qq.com>
2021-07-21 18:35:39 +0800

    Fixed some bugs of clients choosing function.

--- examples/split_learning/split_learning_algorithm.py
+++ examples/split_learning/split_learning_algorithm.py
@@ -67,8 +67,6 @@ class Algorithm(fedavg.Algorithm):
 
         feature_dataset = []
 
-        _randomize = getattr(self.trainer, "randomize", None)
-
         for inputs, targets, *__ in data_loader:
             logits = self.model.forward_to(inputs, cut_layer)
 
@@ -107,13 +105,6 @@ class Algorithm(fedavg.Algorithm):
 
         tic = time.perf_counter()
 
-        # Initializing the loss criterion
-        _loss_criterion = getattr(self, "loss_criterion", None)
-        if callable(_loss_criterion):
-            loss_criterion = self.loss_criterion(self.model)
-        else:
-            loss_criterion = nn.CrossEntropyLoss()
-
         # Initializing the optimizer
         get_optimizer = getattr(self, "get_optimizer",
                                 optimizers.get_optimizer)
@@ -121,18 +112,18 @@ class Algorithm(fedavg.Algorithm):
 
         grad_index = 0
 
-        for batch_id, (examples, labels) in enumerate(data_loader):
+        for __, (examples, __) in enumerate(data_loader):
             optimizer.zero_grad()
-
             outputs = self.model.forward_to(examples, cut_layer)
-
             outputs.backward(self.gradients_list[grad_index])
-
             grad_index = grad_index + 1
-
             optimizer.step()
 
         toc = time.perf_counter()
+
+        logging.info("[Client #%d] Training completed.", self.client_id)
+        logging.info("[Client #{}] Time used: {:.2f} seconds.".format(
+            self.client_id, toc - tic))
 
     def train(self, trainset, sampler, cut_layer=None):
         self.trainer.train(FeatureDataset(trainset), sampler, cut_layer)
