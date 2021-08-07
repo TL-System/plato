862c5fa83fcb667d46ab4b2810fec03f0babcb66
Chen Ying <chenying@ece.utoronto.ca>
2021-08-07 17:39:28 -0400

    Fixed mnior bugs of loading CINIC-10 dataset.

--- plato/datasources/cinic10.py
+++ plato/datasources/cinic10.py
@@ -20,12 +20,11 @@ class DataSource(base.DataSource):
         _path = Config().data.data_path
 
         if not os.path.exists(_path):
-            os.makedirs(_path)
-
-        logging.info(
-            "Downloading the CINIC-10 dataset. This may take a while.")
-        url = Config().data.download_url
-        if not os.path.exists(_path + url.split('/')[-1]):
+            logging.info(
+                "Downloading the CINIC-10 dataset. This may take a while.")
+            url = Config().data.download_url if hasattr(
+                Config().data, 'download_url'
+            ) else 'https://iqua.ece.toronto.edu/~bli/CINIC-10.tar.gz'
             DataSource.download(url, _path)
 
         _transform = transforms.Compose([
@@ -33,9 +32,9 @@ class DataSource(base.DataSource):
             transforms.Normalize([0.47889522, 0.47227842, 0.43047404],
                                  [0.24205776, 0.23828046, 0.25874835])
         ])
-        self.trainset = datasets.ImageFolder(root=self.cinic_path + '/train',
+        self.trainset = datasets.ImageFolder(root=os.path.join(_path, 'train'),
                                              transform=_transform)
-        self.testset = datasets.ImageFolder(root=self.cinic_path + '/test',
+        self.testset = datasets.ImageFolder(root=os.path.join(_path, 'test'),
                                             transform=_transform)
 
     def num_train_examples(self):
