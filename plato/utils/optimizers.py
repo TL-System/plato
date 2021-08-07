8acc002caadc5a011305d95d28d823a82d2884d4
Chen Ying <chenying@ece.utoronto.ca>
2021-08-07 18:46:33 -0400

    Added two learning rate schedulers.

--- plato/utils/optimizers.py
+++ plato/utils/optimizers.py
@@ -65,12 +65,30 @@ def get_lr_schedule(optimizer: optim.Optimizer,
         # Combine the lambdas
         return optim.lr_scheduler.LambdaLR(
             optimizer, lambda it: np.product([l(it) for l in lambdas]))
+    elif Config().trainer.lr_schedule == 'StepLR':
+        step_size = Config().trainer.lr_step_size if hasattr(
+            Config().trainer, 'lr_step_size') else 30
+        gamma = Config().trainer.lr_gamma if hasattr(Config().trainer,
+                                                     'lr_gamma') else 0.1
+        return optim.lr_scheduler.StepLR(optimizer,
+                                         step_size=step_size,
+                                         gamma=gamma)
+    elif Config().trainer.lr_schedule == 'ReduceLROnPlateau':
+        factor = Config().trainer.lr_factor if hasattr(Config().trainer,
+                                                       'lr_factor') else 0.1
+        patience = Config().trainer.lr_patience if hasattr(
+            Config().trainer, 'lr_patience') else 10
+        return optim.lr_scheduler.ReduceLROnPlateau(optimizer,
+                                                    mode='min',
+                                                    factor=factor,
+                                                    patience=patience)
     else:
         sys.exit('Error: Unknown learning rate scheduler.')
 
-    def get_loss_criterion():
-        """Obtain the loss criterion used for training the model."""
-        if Config().trainer.loss_criterion == 'BCEWithLogitsLoss':
-            return nn.BCEWithLogitsLoss()
-        else:
-            return nn.CrossEntropyLoss()
+
+def get_loss_criterion():
+    """Obtain the loss criterion used for training the model."""
+    if Config().trainer.loss_criterion == 'BCEWithLogitsLoss':
+        return nn.BCEWithLogitsLoss()
+    else:
+        return nn.CrossEntropyLoss()
