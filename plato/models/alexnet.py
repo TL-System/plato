2d7e90b3f53650a4e506821864d74552b25005da
Chen Ying <chenying@ece.utoronto.ca>
2021-08-12 20:14:31 -0400

    Added comments of the AlexNet model.

--- plato/models/alexnet.py
+++ plato/models/alexnet.py
@@ -13,10 +13,13 @@ from plato.config import Config
 
 
 class Model():
-    """The Inception v3 model."""
+    """The AlexNet model."""
     @staticmethod
     def get_model(*args):
-        """Obtaining an instance of this model."""
+        """Obtaining an instance of the AlexNet model."""
+
+        # If True, will return a AlexNet model pre-trained on ImageNet
         pretrained = Config().trainer.pretrained if hasattr(
             Config().trainer, 'pretrained') else False
+
         return torchvision.models.alexnet(pretrained=pretrained)
