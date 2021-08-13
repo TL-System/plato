87de94d4005e2e843e207430e0b4730785994dbf
Chen Ying <chenying@ece.utoronto.ca>
2021-08-12 20:48:44 -0400

    Added useable parameters of the Inception v3 model.

--- plato/models/inceptionv3.py
+++ plato/models/inceptionv3.py
@@ -9,6 +9,7 @@ Proceedings of the IEEE conference on computer vision and pattern recognition. 2
 """
 
 import torchvision
+from plato.config import Config
 
 
 class Model():
@@ -16,4 +17,16 @@ class Model():
     @staticmethod
     def get_model(*args):
         """Obtaining an instance of this model."""
-        return torchvision.models.inception_v3(aux_logits=False)
+
+        # If True, will return a Inception v3 model pre-trained on ImageNet
+        pretrained = Config().trainer.pretrained if hasattr(
+            Config().trainer, 'pretrained') else False
+
+        # If True, preprocesses the input according to the method
+        # with which it was trained on ImageNet
+        transform_input = Config().trainer.transform_input if hasattr(
+            Config().trainer, 'transform_input') else False
+
+        return torchvision.models.inception_v3(pretrained=pretrained,
+                                               aux_logits=False,
+                                               transform_input=transform_input)
