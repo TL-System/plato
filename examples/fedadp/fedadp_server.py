8a8c7626914732401d563387ff92859edd993c32
Baochun Li <bli@ece.toronto.edu>
2021-08-18 18:55:59 +0800

    Cleaned up the FedAdp example and improved the documentation.

--- examples/fedadp/fedadp_server.py
+++ examples/fedadp/fedadp_server.py
@@ -1,5 +1,5 @@
 """
-A federated learning training session using FedAdp.
+A federated learning training session using the FedAdp algorithm.
 
 Reference:
 
@@ -9,45 +9,46 @@ in IEEE Transactions on Cognitive Communications and Networking (TCCN'21).
 https://ieeexplore.ieee.org/abstract/document/9442814
 """
 
-from plato.servers import fedavg
-
-import logging
-import numpy as np
 import math
+
+import numpy as np
+
+from plato.servers import fedavg
 
 
 class Server(fedavg.Server):
     """A federated learning server using the FedAdp algorithm."""
     def __init__(self):
         super().__init__()
+
         # alpha controls the decreasing rate of the mapping function
         self.alpha = 5
         self.local_correlations = {}
         self.last_global_grads = None
         self.adaptive_weighting = None
-    
+
     def extract_client_updates(self, updates):
         """ Extract the model weights and update directions from clients updates. """
         weights_received = [payload for (__, payload) in updates]
-        
+
         num_samples = [report.num_samples for (report, __) in updates]
 
         # Get adaptive weighting based on both node contribution and date size
         self.adaptive_weighting = self.calc_adaptive_weighting(weights_received, num_samples)
 
         return self.algorithm.compute_weight_updates(weights_received)
-    
+
     async def federated_averaging(self, updates):
         """ Aggregate weight updates and deltas updates from the clients. """
         # Extract weights udpates from the client updates
         weights_received = self.extract_client_updates(updates)
-        
+
         # Perform weighted averaging
         avg_update = {
             name: self.trainer.zeros(weights.shape)
             for name, weights in weights_received[0].items()
         }
-        
+
         # Use adaptive weighted average  
         for i, update in enumerate(weights_received):
             for name, delta in update.items():
@@ -56,10 +57,11 @@ class Server(fedavg.Server):
         return avg_update
 
     def calc_adaptive_weighting(self, updates, num_samples):
-        """Compute the weights for model aggregation considering both node contribution and data size.""" 
-        # Get node contribution
+        """ Compute the weights for model aggregation considering both node contribution
+        and data size. """
+        # Get the node contribution
         contribs = self.calc_contribution(updates)
-        
+
         # Calculate the weighting of each participating client for aggregation
         adaptive_weighting = [None] * len(updates)
         total_weight = 0.0
@@ -69,11 +71,12 @@ class Server(fedavg.Server):
             adaptive_weighting[i] = (num_samples[i] * math.exp(contrib)) / total_weight
 
         return adaptive_weighting
-    
+
     def calc_contribution(self, updates):
-        """Calculate the node contribution based on the angle between local gradient and global gradient."""
+        """ Calculate the node contribution based on the angle between the local
+        and global gradients. """
         correlations, contribs = [None] * len(updates), [None] * len(updates)
-        
+
         # Update the baseline model weights
         curr_global_grads = self.process_grad(self.algorithm.extract_weights())
         if self.last_global_grads is None:
@@ -87,25 +90,30 @@ class Server(fedavg.Server):
             inner = np.inner(global_grads, local_grads)
             norms = np.linalg.norm(global_grads) * np.linalg.norm(local_grads)
             correlations[i] = np.arccos(np.clip(inner / norms, -1.0, 1.0))
-        
+
         for i, correlation in enumerate(correlations):
             client_id = self.selected_clients[i]
+
             # Update the smoothed angle for all clients
             if client_id not in self.local_correlations.keys():
                 self.local_correlations[client_id] = correlation
-            self.local_correlations[client_id] = ((self.current_round - 1) / self.current_round) * self.local_correlations[client_id] + (1 / self.current_round) * correlation
+            self.local_correlations[client_id] = ((self.current_round - 1) 
+            / self.current_round) * self.local_correlations[client_id] 
+            + (1 / self.current_round) * correlation
+
             # Non-linear mapping to node contribution
-            contribs[i] = self.alpha * (1 - math.exp(-math.exp(-self.alpha * (self.local_correlations[client_id] - 1))))
-            
+            contribs[i] = self.alpha * (1 - math.exp(-math.exp(-self.alpha 
+                          * (self.local_correlations[client_id] - 1))))
+
         return contribs
-    
+
     @staticmethod
     def process_grad(grads):
         """Convert gradients to a flattened 1-D array."""
         grads = list(dict(sorted(grads.items(), key=lambda x: x[0].lower())).values())
-        
+
         flattened = grads[0]
         for i in range(1, len(grads)):
             flattened = np.append(flattened, grads[i]) 
-        
+
         return flattened
