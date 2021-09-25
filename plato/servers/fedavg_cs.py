4fa98d15ba8183a72486065057cb02444e14cffa
Chen Ying <chenying@ece.utoronto.ca>
2021-09-25 10:05:17 -0400

    Supported customized trainer in cross-silo FL.

--- plato/servers/fedavg_cs.py
+++ plato/servers/fedavg_cs.py
@@ -17,9 +17,9 @@ from plato.servers import fedavg
 class Server(fedavg.Server):
     """Cross-silo federated learning server using federated averaging."""
     def __init__(self, model=None, algorithm=None, trainer=None):
-        super().__init__(model=None, algorithm=None, trainer=None)
+        super().__init__(model=model, algorithm=algorithm, trainer=trainer)
 
-        self.current_global_round = None
+        self.current_global_round = 0
         # Average accuracy from client reports
         self.average_accuracy = 0
 
@@ -119,9 +119,14 @@ class Server(fedavg.Server):
             if not Config().clients.do_test or Config().server.do_test:
                 # Test the updated model directly at the server
                 self.accuracy = self.trainer.test(self.testset)
-                logging.info(
-                    '[Server #{:d}] Global model accuracy: {:.2f}%\n'.format(
-                        os.getpid(), 100 * self.accuracy))
+                if Config().is_central_server():
+                    logging.info(
+                        '[Server #{:d}] Global model accuracy: {:.2f}%\n'.
+                        format(os.getpid(), 100 * self.accuracy))
+                else:
+                    logging.info(
+                        '[Edge Server #{:d}] Aggregated model accuracy: {:.2f}%\n'
+                        .format(os.getpid(), 100 * self.accuracy))
         else:
             self.accuracy = self.average_accuracy
 
@@ -172,6 +177,7 @@ class Server(fedavg.Server):
                 self.model_aggregated.set()
 
                 self.current_round = 0
+                self.current_global_round += 1
                 self.new_global_round_begins.clear()
                 # Wait until a new global round begins
                 # to avoid selecting clients before a new global round begins
