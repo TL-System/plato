b36c02018ae94eda193d58e9adde06bbf19423b1
Baochun Li <bli@ece.toronto.edu>
2021-12-01 00:11:45 -0500

    Refactored __init__() in Processor classes.

--- plato/processors/mistnet_random_response.py
+++ plato/processors/mistnet_random_response.py
@@ -14,8 +14,8 @@ class Processor(base.Processor):
     """
     Implements a Processor for applying local differential privacy using random response.
     """
-    def __init__(self, *args, client_id=None, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
+    def __init__(self, client_id=None, **kwargs) -> None:
+        super().__init__(**kwargs)
 
         self.client_id = client_id
 
