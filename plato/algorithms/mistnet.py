fe7d24f561b7c550ef6e769232fc85733e46b232
Baochun Li <bli@ece.toronto.edu>
2021-12-05 23:44:12 -0500

    Removed PyTorch dependency for TensorFlow and MindSpore workloads.

--- plato/algorithms/mistnet.py
+++ plato/algorithms/mistnet.py
@@ -15,6 +15,19 @@ from plato.algorithms import fedavg
 from plato.config import Config
 
 
+class FeatureDataset(torch.utils.data.Dataset):
+    """Used to prepare a feature dataset for a DataLoader in PyTorch."""
+    def __init__(self, dataset):
+        self.dataset = dataset
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, item):
+        image, label = self.dataset[item]
+        return image, label
+
+
 class Algorithm(fedavg.Algorithm):
     """The PyTorch-based MistNet algorithm, used by both the client and the
     server.
@@ -57,4 +70,4 @@ class Algorithm(fedavg.Algorithm):
 
     def train(self, trainset, sampler, cut_layer=None):
         """ Train the neural network model after the cut layer. """
-        self.trainer.train(trainset.torch_dataset(), sampler, cut_layer)
+        self.trainer.train(FeatureDataset(trainset), sampler, cut_layer)
