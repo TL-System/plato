c0e150d0bd184f0ae467e8c7cdea98a0c274311a
Baochun Li <bli@ece.toronto.edu>
2021-12-09 09:53:40 -0500

    Fixed MindSpore and TensorFlow CI errors.

--- plato/processors/registry.py
+++ plato/processors/registry.py
@@ -10,7 +10,7 @@ from collections import OrderedDict
 from typing import Tuple
 
 from plato.config import Config
-from plato.processors import feature_dequantize, feature_unbatch, model_deepcopy, pipeline
+from plato.processors import pipeline
 
 if not (hasattr(Config().trainer, 'use_tensorflow')
         or hasattr(Config().trainer, 'use_mindspore')):
@@ -18,12 +18,15 @@ if not (hasattr(Config().trainer, 'use_tensorflow')
         base,
         gradient_gaussian,
         gradient_laplace,
-        inbound_feature_tensors,
-        outbound_feature_ndarrays,
         feature_randomized_response,
         feature_gaussian,
         feature_laplace,
         feature_quantize,
+        feature_dequantize,
+        feature_unbatch,
+        inbound_feature_tensors,
+        outbound_feature_ndarrays,
+        model_deepcopy,
         model_quantize,
         model_dequantize,
         model_randomized_response,
@@ -34,13 +37,13 @@ if not (hasattr(Config().trainer, 'use_tensorflow')
         ('gradient_gaussian', gradient_gaussian.Processor),
         ('gradient_laplace', gradient_laplace.Processor),
         ('feature_randomized_response', feature_randomized_response.Processor),
-        ('feature_unbatch', feature_unbatch.Processor),
-        ('outbound_feature_ndarrays', outbound_feature_ndarrays.Processor),
-        ('inbound_feature_tensors', inbound_feature_tensors.Processor),
         ('feature_gaussian', feature_gaussian.Processor),
         ('feature_laplace', feature_laplace.Processor),
         ('feature_quantize', feature_quantize.Processor),
         ('feature_dequantize', feature_dequantize.Processor),
+        ('feature_unbatch', feature_unbatch.Processor),
+        ('inbound_feature_tensors', inbound_feature_tensors.Processor),
+        ('outbound_feature_ndarrays', outbound_feature_ndarrays.Processor),
         ('model_deepcopy', model_deepcopy.Processor),
         ('model_quantize', model_quantize.Processor),
         ('model_dequantize', model_dequantize.Processor),
