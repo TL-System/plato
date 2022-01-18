a57a6ebb41025182786419cf8b90678733f7c02e
XinYao1994 <xyao@cs.hku.hk>
2022-01-18 13:21:43 +0800

    Fixed a bug in compression when the shape of the target is zero.

--- plato/processors/decompress.py
+++ plato/processors/decompress.py
@@ -25,9 +25,12 @@ class Processor(base.Processor):
             datatype_feature = data[0][1]
             for datacom_feature, datacom_target, datashape_target, datatype_target in data[1:]:
                 datacom_feature = zstd.decompress(datacom_feature)
-                datacom_target = zstd.decompress(datacom_target)
                 datacom_feature = np.frombuffer(datacom_feature, datatype_feature).reshape(datashape_feature)
-                datacom_target = np.frombuffer(datacom_target, datatype_target).reshape(datashape_target)
+                if len(datashape_target) > 0 and datashape_target[0] == 0:
+                    datacom_target = np.zeros(datashape_target)
+                else:
+                    datacom_target = zstd.decompress(datacom_target)
+                    datacom_target = np.frombuffer(datacom_target, datatype_target).reshape(datashape_target)
                 ret.append((datacom_feature, datacom_target))
         else:
             shape, dtype, modelcom = data
