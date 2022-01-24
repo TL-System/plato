f938e016f30216d9e1a94cd7da1d20e95e1c5463
Ningxin Su <ningxin.su@mail.utoronto.ca>
2022-01-24 17:17:50 -0500

    Added port algorithm in examples

---
+++ examples/port_fedasync_mode/port.py
@@ -0,0 +1,28 @@
+"""
+A federated learning server using Port.
+
+Reference:
+
+"How Asynchronous can Federated Learning Be?"
+
+"""
+import os
+
+os.environ['config_file'] = './port_MNIST_lenet5.yml'
+
+from plato.trainers import basic
+from plato.clients import simple
+
+import port_server
+
+
+def main():
+    """ A Plato federated learning training session using FedAsync. """
+    trainer = basic.Trainer()
+    client = simple.Client(trainer=trainer)
+    server = port_server.Server(trainer=trainer)
+    server.run(client)
+
+
+if __name__ == "__main__":
+    main()
