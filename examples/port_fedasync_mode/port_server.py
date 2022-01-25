83bf6f07d2f2f7336f4fffcbed740bd459adfb9a
Ningxin Su <ningxin.su@mail.utoronto.ca>
2022-01-24 21:02:58 -0500

    Updated port_server.py

--- examples/port_fedasync_mode/port_server.py
+++ examples/port_fedasync_mode/port_server.py
@@ -6,112 +6,15 @@ Reference:
 "How Asynchronous can Federated Learning Be?"
 
 """
-
-import asyncio
 import logging
 import os
-import random
-import time
+from collections import OrderedDict
 
-from plato.algorithms import registry as algorithms_registry
 from plato.config import Config
-from plato.datasources import registry as datasources_registry
-from plato.processors import registry as processor_registry
-from plato.servers import base
-from plato.trainers import registry as trainers_registry
-from plato.utils import csv_processor
+from plato.servers import fedavg
 
 
-class Server(base.Server):
-    """Federated learning server using federated averaging."""
-    def __init__(self, model=None, algorithm=None, trainer=None):
-        super().__init__()
-
-        if hasattr(Config().trainer, 'use_wandb'):
-            import wandb
-
-            wandb.init(project="plato", reinit=True)
-
-        self.model = model
-        self.algorithm = algorithm
-        self.trainer = trainer
-
-        self.testset = None
-        self.total_samples = 0
-
-        self.total_clients = Config().clients.total_clients
-        self.clients_per_round = Config().clients.per_round
-
-        logging.info(
-            "[Server #%d] Started training on %s clients with %s per round.",
-            os.getpid(), self.total_clients, self.clients_per_round)
-
-        if hasattr(Config(), 'results'):
-            recorded_items = Config().results.types
-            self.recorded_items = [
-                x.strip() for x in recorded_items.split(',')
-            ]
-
-        random.seed()
-
-    def configure(self):
-        """
-        Booting the federated learning server by setting up the data, model, and
-        creating the clients.
-        """
-        logging.info("[Server #%d] Configuring the server...", os.getpid())
-        super().configure()
-
-        total_rounds = Config().trainer.rounds
-        target_accuracy = Config().trainer.target_accuracy
-
-        if target_accuracy:
-            logging.info("Training: %s rounds or %s%% accuracy\n",
-                         total_rounds, 100 * target_accuracy)
-        else:
-            logging.info("Training: %s rounds\n", total_rounds)
-
-        self.load_trainer()
-
-        # Prepares this server for processors that processes outbound and inbound
-        # data payloads
-        self.outbound_processor, self.inbound_processor = processor_registry.get(
-            "Server", server_id=os.getpid(), trainer=self.trainer)
-
-        if not Config().clients.do_test:
-            dataset = datasources_registry.get(client_id=0)
-            self.testset = dataset.get_test_set()
-
-        # Initialize the csv file which will record results
-        if hasattr(Config(), 'results'):
-            result_csv_file = Config().result_dir + 'result.csv'
-            csv_processor.initialize_csv(result_csv_file, self.recorded_items,
-                                         Config().result_dir)
-
-    def load_trainer(self):
-        """Setting up the global model to be trained via federated learning."""
-        if self.trainer is None:
-            self.trainer = trainers_registry.get(model=self.model)
-
-        self.trainer.set_client_id(0)
-
-        if self.algorithm is None:
-            self.algorithm = algorithms_registry.get(self.trainer)
-
-    async def select_clients(self):
-        await super().select_clients()
-
-    def extract_client_updates(self, updates):
-        """Extract the model weight updates from client updates."""
-        weights_received = [payload for (__, payload, __) in updates]
-        return self.algorithm.compute_weight_updates(weights_received)
-
-    async def aggregate_weights(self, updates):
-        """Aggregate the reported weight updates from the selected clients."""
-        update = await self.federated_averaging(updates)
-        updated_weights = self.algorithm.update_weights(update)
-        self.algorithm.load_weights(updated_weights)
-
+class Server(fedavg.Server):
     async def federated_averaging(self, updates):
         """Aggregate weight updates from the clients using federated averaging."""
         weights_received = self.extract_client_updates(updates)
@@ -154,70 +57,3 @@ class Server(base.Server):
             await asyncio.sleep(0)
 
         return avg_update
-
-    async def process_reports(self):
-        """Process the client reports by aggregating their weights."""
-        await self.aggregate_weights(self.updates)
-
-        # Testing the global model accuracy
-        if Config().clients.do_test:
-            # Compute the average accuracy from client reports
-            self.accuracy = self.accuracy_averaging(self.updates)
-            logging.info(
-                '[Server #{:d}] Average client accuracy: {:.2f}%.'.format(
-                    os.getpid(), 100 * self.accuracy))
-        else:
-            # Testing the updated model directly at the server
-            self.accuracy = await self.trainer.server_test(self.testset)
-
-            logging.info(
-                '[Server #{:d}] Global model accuracy: {:.2f}%\n'.format(
-                    os.getpid(), 100 * self.accuracy))
-
-        if hasattr(Config().trainer, 'use_wandb'):
-            wandb.log({"accuracy": self.accuracy})
-
-        await self.wrap_up_processing_reports()
-
-    async def wrap_up_processing_reports(self):
-        """ Wrap up processing the reports with any additional work. """
-        if hasattr(Config(), 'results'):
-            new_row = []
-
-            for item in self.recorded_items:
-                item_value = {
-                    'round':
-                    self.current_round,
-                    'accuracy':
-                    self.accuracy * 100,
-                    'elapsed_time':
-                    self.wall_time - self.initial_wall_time,
-                    'round_time':
-                    max([
-                        report.training_time
-                        for (report, __, __) in self.updates
-                    ]),
-                }[item]
-                new_row.append(item_value)
-
-            result_csv_file = Config().result_dir + 'result.csv'
-
-            csv_processor.write_csv(result_csv_file, new_row)
-
-    @staticmethod
-    def accuracy_averaging(updates):
-        """Compute the average accuracy across clients."""
-        # Get total number of samples
-        total_samples = sum(
-            [report.num_samples for (report, __, __) in updates])
-
-        # Perform weighted averaging
-        accuracy = 0
-        for (report, __, __) in updates:
-            accuracy += report.accuracy * (report.num_samples / total_samples)
-
-        return accuracy
-
-    def customize_server_payload(self, payload):
-        """ Customize the server payload before sending to the client. """
-        return payload
