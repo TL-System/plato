bbf9ca7e4819ba78e56072e479e2436cb3d9ccef
Chen Ying <c.ying@mail.utoronto.ca>
2022-02-10 00:21:11 -0500

    Added a new pruning method.

--- examples/fedsaw/fedsaw_server.py
+++ examples/fedsaw/fedsaw_server.py
@@ -2,15 +2,12 @@
 A cross-silo federated learning server using FedSaw, as either edge or central servers.
 """
 
-from plato.config import Config
 from plato.servers import fedavg_cs
 
 
 class Server(fedavg_cs.Server):
     """Cross-silo federated learning server using FedSaw."""
-    def customize_server_payload(self, payload):
-        """ Customize the server payload before sending to its clients. """
-        if Config().is_edge_server():
-            pass
-
-        return payload
+    def extract_client_updates(self, updates):
+        """ Extract the model weight updates from client updates. """
+        updates_received = [payload for (__, payload, __) in updates]
+        return updates_received
