d3248f20f0038fd2195b07e4fb12c78184ffac34
Fei Wang <silviafey.wang@utoronto.ca>
2022-03-02 22:04:20 -0500

    Update AFL's trainer

--- examples/afl/afl_trainer.py
+++ examples/afl/afl_trainer.py
@@ -11,29 +11,36 @@ https://arxiv.org/pdf/1909.12641.pdf
 """
 import logging
 import os
+import time
 
 import numpy as np
 import torch
-import torch.nn as nn
-
+from opacus.privacy_engine import PrivacyEngine
 from plato.config import Config
+from plato.models import registry as models_registry
 from plato.trainers import basic
 from plato.utils import optimizers
 
 
 class Trainer(basic.Trainer):
-
+    """ A federated learning trainer for FEI. """
     def train_model(self, config, trainset, sampler, cut_layer=None):
-        """A custom trainer reporting training loss. """
-        log_interval = 10
+        """ A custom trainer reporting training loss. """
         batch_size = config['batch_size']
+        log_interval = 10
+        tic = time.perf_counter()
 
         logging.info("[Client #%d] Loading the dataset.", self.client_id)
+        _train_loader = getattr(self, "train_loader", None)
 
-        train_loader = torch.utils.data.DataLoader(dataset=trainset,
-                                                   shuffle=False,
-                                                   batch_size=batch_size,
-                                                   sampler=sampler)
+        if callable(_train_loader):
+            train_loader = self.train_loader(batch_size, trainset, sampler,
+                                             cut_layer)
+        else:
+            train_loader = torch.utils.data.DataLoader(dataset=trainset,
+                                                       shuffle=False,
+                                                       batch_size=batch_size,
+                                                       sampler=sampler)
 
         iterations_per_epoch = np.ceil(len(trainset) / batch_size).astype(int)
         epochs = config['epochs']
@@ -47,7 +54,7 @@ class Trainer(basic.Trainer):
         if callable(_loss_criterion):
             loss_criterion = self.loss_criterion(self.model)
         else:
-            loss_criterion = nn.CrossEntropyLoss()
+            loss_criterion = torch.nn.CrossEntropyLoss()
 
         # Initializing the optimizer
         get_optimizer = getattr(self, "get_optimizer",
@@ -62,52 +69,77 @@ class Trainer(basic.Trainer):
         else:
             lr_schedule = None
 
-        try:
-            for epoch in range(1, epochs + 1):
-                for batch_id, (examples, labels) in enumerate(train_loader):
-                    examples, labels = examples.to(self.device), labels.to(
-                        self.device)
+        if 'differential_privacy' in config and config['differential_privacy']:
+            privacy_engine = PrivacyEngine(accountant='rdp', secure_mode=False)
+
+            self.model, optimizer, train_loader = privacy_engine.make_private_with_epsilon(
+                module=self.model,
+                optimizer=optimizer,
+                data_loader=train_loader,
+                target_epsilon=config['dp_epsilon']
+                if 'dp_epsilon' in config else 10.0,
+                target_delta=config['dp_delta']
+                if 'dp_delta' in config else 1e-5,
+                epochs=epochs,
+                max_grad_norm=config['dp_max_grad_norm']
+                if 'max_grad_norm' in config else 1.0,
+            )
+
+        for epoch in range(1, epochs + 1):
+            # Use a default training loop
+            for batch_id, (examples, labels) in enumerate(train_loader):
+                examples, labels = examples.to(self.device), labels.to(
+                    self.device)
+                if 'differential_privacy' in config and config[
+                        'differential_privacy']:
+                    optimizer.zero_grad(set_to_none=True)
+                else:
                     optimizer.zero_grad()
 
-                    if cut_layer is None:
-                        outputs = self.model(examples)
+                if cut_layer is None:
+                    outputs = self.model(examples)
+                else:
+                    outputs = self.model.forward_from(examples, cut_layer)
+
+                loss = loss_criterion(outputs, labels)
+
+                loss.backward()
+                optimizer.step()
+
+                if batch_id % log_interval == 0:
+                    if self.client_id == 0:
+                        logging.info(
+                            "[Server #%d] Epoch: [%d/%d][%d/%d]\tLoss: %.6f",
+                            os.getpid(), epoch, epochs, batch_id,
+                            len(train_loader), loss.data.item())
                     else:
-                        outputs = self.model.forward_from(examples, cut_layer)
+                        logging.info(
+                            "[Client #%d] Epoch: [%d/%d][%d/%d]\tLoss: %.6f",
+                            self.client_id, epoch, epochs, batch_id,
+                            len(train_loader), loss.data.item())
 
-                    loss = loss_criterion(outputs, labels)
+            if lr_schedule is not None:
+                lr_schedule.step()
 
-                    loss.backward()
+            if hasattr(optimizer, "params_state_update"):
+                optimizer.params_state_update()
 
-                    optimizer.step()
+            # Simulate client's speed
+            if self.client_id != 0 and hasattr(
+                    Config().clients,
+                    "speed_simulation") and Config().clients.speed_simulation:
+                self.simulate_sleep_time()
 
-                    if lr_schedule is not None:
-                        lr_schedule.step()
-
-                    if batch_id % log_interval == 0:
-                        if self.client_id == 0:
-                            logging.info(
-                                "[Server #{}] Epoch: [{}/{}][{}/{}]\tLoss: {:.6f}"
-                                .format(os.getpid(), epoch, epochs, batch_id,
-                                        len(train_loader), loss.data.item()))
-                        else:
-                            logging.info(
-                                "[Client #{}] Epoch: [{}/{}][{}/{}]\tLoss: {:.6f}"
-                                .format(self.client_id, epoch, epochs,
-                                        batch_id, len(train_loader),
-                                        loss.data.item()))
-
-                if hasattr(optimizer, "params_state_update"):
-                    optimizer.params_state_update()
-
-        except Exception as training_exception:
-            logging.info("Training on client #%d failed.", self.client_id)
-            raise training_exception
-
-        if 'max_concurrency' in config:
-            self.model.cpu()
-            model_type = config['model_name']
-            filename = f"{model_type}_{self.client_id}_{config['run_id']}.pth"
-            self.save_model(filename)
+            # Saving the model at the end of this epoch to a file so that
+            # it can later be retrieved to respond to server requests
+            # in asynchronous mode when the wall clock time is simulated
+            if hasattr(Config().server,
+                       'request_update') and Config().server.request_update:
+                self.model.cpu()
+                training_time = time.perf_counter() - tic
+                filename = f"{self.client_id}_{epoch}_{training_time}.pth"
+                self.save_model(filename)
+                self.model.to(self.device)
 
         # Save the training loss of the last epoch in this round
         model_name = config['model_name']
@@ -128,7 +160,7 @@ class Trainer(basic.Trainer):
         else:
             loss_path = f'{model_dir}/{model_name}.loss'
 
-        with open(loss_path, 'w') as file:
+        with open(loss_path, 'w', encoding='utf-8') as file:
             file.write(str(loss))
 
     @staticmethod
@@ -142,7 +174,7 @@ class Trainer(basic.Trainer):
         else:
             loss_path = f'{model_dir}/{model_name}.loss'
 
-        with open(loss_path, 'r') as file:
+        with open(loss_path, 'r', encoding='utf-8') as file:
             loss = float(file.read())
 
         return loss
