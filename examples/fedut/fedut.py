5a86e69c1e6fe96937424cf41b470145d973c04d
Yufei-Kang <1175801998@qq.com>
2022-03-15 16:27:35 -0400

    Added sharpness-aware minimizer.

--- examples/fedut/fedut.py
+++ examples/fedut/fedut.py
@@ -5,14 +5,20 @@ import os
 
 os.environ['config_file'] = './fedut_MNIST_lenet5.yml'
 
-import fedut_client
-import fedut_server
+#import fedut_client
+#import fedut_server
+from plato.servers import fedavg
+from plato.clients import simple
+import fedut_trainer
 
 
 def main():
     """ A Plato federated learning training session using the FedNova algorithm. """
-    client = fedut_client.Client()
-    server = fedut_server.Server()
+    # client = fedut_client.Client()
+    # server = fedut_server.Server()
+    trainer = fedut_trainer.Trainer()
+    client = simple.Client(trainer=trainer)
+    server = fedavg.Server(trainer=trainer)
     server.run(client)
 
 
