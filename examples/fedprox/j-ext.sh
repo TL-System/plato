19594943156031b2a68bdef6ca5e882337ffa90d
Chen Ying <c.ying@mail.utoronto.ca>
2022-03-26 20:21:02 -0400

    Added new .sh and configuration files of FedProx.

--- examples/fedprox/j-ext.sh
+++ examples/fedprox/j-ext.sh
@@ -3,8 +3,8 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=128G
 #SBATCH --account=def-iamniudi
-#SBATCH --output=o-iid-c.out
+#SBATCH --output=o-ext-c.out
 
 module load gcc/9.3.0 arrow cuda/11 python/3.9 scipy-stack
 source ~/.federated/bin/activate
-python fedprox.py -c configs/iid-c.yml
+python fedprox.py -c configs/ext-c.yml
--- examples/fedprox/j-non-c.sh
+++ examples/fedprox/j-non-c.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --time=4:00:00
+#SBATCH --time=4:30:00
 #SBATCH --gres=gpu:1
-#SBATCH --mem=256G
+#SBATCH --mem=128G
 #SBATCH --account=def-iamniudi
 #SBATCH --output=o-non-c.out
 
