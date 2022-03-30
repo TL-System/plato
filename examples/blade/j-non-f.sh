0e940f80a041454e630a8023241845083ef42a80
Chen Ying <c.ying@mail.utoronto.ca>
2022-03-30 19:43:19 -0400

    Added .sh and configuration files.

---
+++ examples/blade/j-non-f.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --time=8:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --mem=128G
+#SBATCH --account=def-iamniudi
+#SBATCH --output=o-non-f.out
+module load python/3.9
+source ~/.federated/bin/activate
+python blade.py -c configs/non-f.yml
