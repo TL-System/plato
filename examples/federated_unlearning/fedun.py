3797d74da729e96d653de6a82ed86ccbff754f5a
Ningxin Su <ningxin.su@mail.utoronto.ca>
2022-04-13 00:09:23 -0400

    Update fedun.py

--- examples/federated_unlearning/fedun.py
+++ examples/federated_unlearning/fedun.py
@@ -7,19 +7,19 @@ Reference: Liu et al., "The Right to be Forgotten in Federated Learning: An Effi
 
 import os
 
-os.environ['config_file'] = 'fedun_MNIST_lenet5.yml'
+os.environ[
+    'config_file'] = 'examples/federated_unlearning/fedun_MNIST_lenet5.yml'
 
 import fedun_client
-import fedun_server
-import fedun_trainer
+from plato.servers import fedavg
+#import fedun_trainer
 
 
 def main():
     """ A Plato federated learning training session using the FedSarah algorithm. """
-    trainer = fedun_trainer.Trainer()
-    client = fedun_client.Client(trainer=trainer)
-    server = fedun_server.Server(trainer=trainer)
-
+    #    trainer = fedun_trainer.Trainer()
+    client = fedun_client.Client()
+    server = fedavg.Server()
     server.run(client)
 
 
