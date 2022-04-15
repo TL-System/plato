82e16f0c4fa6912748e6717c615d86b74077a326
Chen Ying <c.ying@mail.utoronto.ca>
2022-04-15 14:53:38 -0400

    Updated configs of SCAFFOLD.

--- examples/scaffold/j-ext-c.sh
+++ examples/scaffold/j-ext-c.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-#SBATCH --time=6:00:00
+#SBATCH --time=10:00:00
 #SBATCH --gres=gpu:1
-#SBATCH --mem=136G
-#SBATCH --account=def-iamniudi
+#SBATCH --mem=40G
 #SBATCH --output=o-ext-c.out
 
-module load gcc/9.3.0 arrow cuda/11 python/3.9 scipy-stack
-source ~/.federated/bin/activate
-python scaffold.py -c configs/ext-c.yml
+CUDA_LAUNCH_BLOCKING=1 python scaffold.py -c configs/ext-c.yml
