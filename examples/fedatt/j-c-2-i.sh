22e06da3024b863838cfb0edb416dce76403cd40
Chen Ying <c.ying@mail.utoronto.ca>
2022-04-23 00:31:03 -0400

    Added configs of CIFAR-10.

---
+++ examples/fedatt/j-c-2-i.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --time=6:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --mem=256G
+#SBATCH --account=def-baochun
+#SBATCH --output=o-c-2-i.out
+
+module load gcc/9.3.0 arrow cuda/11 python/3.9 scipy-stack
+source ~/.federated/bin/activate
+python fedatt.py -c configs/c-2-iid.yml
