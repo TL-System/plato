062b9f15817499ca5f59fe86c152c209613ec11d
Chen Ying <c.ying@mail.utoronto.ca>
2022-04-22 20:42:11 -0400

    Added new configs on narval.

---
+++ configs/narval/20 clients/j-n-2-i.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --time=6:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --mem=256G
+#SBATCH --account=def-baochun
+#SBATCH --output=o-n-2-i.out
+
+module load gcc/9.3.0 arrow cuda/11 python/3.9 scipy-stack
+source ~/.federated/bin/activate
+./run -c configs/n-2-iid.yml
