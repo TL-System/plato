965e7a661da9efabc59824769d50036f6e9f9294
Chen Ying <c.ying@mail.utoronto.ca>
2022-05-04 08:33:40 -0400

    Cleaned code.

--- examples/blade/j-ext-f.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --time=8:00:00
-#SBATCH --gres=gpu:1
-#SBATCH --mem=128G
-#SBATCH --account=def-iamniudi
-#SBATCH --output=o-ext-f.out
-module load python/3.9
-source ~/.federated/bin/activate
-python blade.py -c configs/ext-f.yml
