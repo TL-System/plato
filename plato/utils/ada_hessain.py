2b8bcc5c3175ee0cd6b117a68057f77ced72202e
Ningxin Su <ningxin.su@mail.utoronto.ca>
2022-05-07 15:45:00 -0400

    Deleted the test code of ada_hessain

--- plato/utils/ada_hessain.py
+++ plato/utils/ada_hessain.py
@@ -89,9 +89,6 @@ class AdaHessian(torch.optim.Optimizer):
 
         for i in range(self.n_samples):
             zs = [torch.randint(0, 2, p.size(), generator=self.generator, device=p.device) * 2.0 - 1.0 for p in params]  # Rademacher distribution {-1.0, 1.0}
-            print("grads length: ", len(grads))
-            print("params length: ", len(params))
-            print("params: ", params)
             h_zs = torch.autograd.grad(grads, params, grad_outputs=zs, only_inputs=True, retain_graph=i < self.n_samples - 1)
             for h_z, z, p in zip(h_zs, zs, params):
                 p.hess += h_z * z / self.n_samples  # approximate the expected values of z*(H@z)
