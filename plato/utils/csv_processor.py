50bdbde8a7bccc7593416f38cb18c8246bddb0fe
Baochun Li <bli@ece.toronto.edu>
2022-05-09 15:03:06 -0400

    Improved formatting in csv processor.

--- plato/utils/csv_processor.py
+++ plato/utils/csv_processor.py
@@ -14,22 +14,14 @@ def initialize_csv(result_csv_file: str, recorded_items: List,
     if not os.path.exists(result_dir):
         os.makedirs(result_dir)
 
-    with open(
-            result_csv_file,
-            'w',
-            encoding='utf-8',
-    ) as result_file:
+    with open(result_csv_file, 'w', encoding='utf-8') as result_file:
         result_writer = csv.writer(result_file)
-        first_row = recorded_items
-        result_writer.writerow(first_row)
+        header_row = recorded_items
+        result_writer.writerow(header_row)
 
 
 def write_csv(result_csv_file: str, new_row: List) -> None:
     """ Write the results of current round. """
-    with open(
-            result_csv_file,
-            'a',
-            encoding='utf-8',
-    ) as result_file:
+    with open(result_csv_file, 'a', encoding='utf-8') as result_file:
         result_writer = csv.writer(result_file)
         result_writer.writerow(new_row)
