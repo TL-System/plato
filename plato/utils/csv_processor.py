4a4a1961d9821e7768940ec64b26cd2568dee55f
kevinsun203 <kevingrandflyer@gmail.com>
2022-05-09 13:35:23 -0400

    Formatted according to yapf formatting.

--- plato/utils/csv_processor.py
+++ plato/utils/csv_processor.py
@@ -14,7 +14,11 @@ def initialize_csv(result_csv_file: str, recorded_items: List,
     if not os.path.exists(result_dir):
         os.makedirs(result_dir)
 
-    with open(result_csv_file, 'w', encoding='utf-8',) as result_file:
+    with open(
+            result_csv_file,
+            'w',
+            encoding='utf-8',
+    ) as result_file:
         result_writer = csv.writer(result_file)
         first_row = recorded_items
         result_writer.writerow(first_row)
@@ -22,6 +26,10 @@ def initialize_csv(result_csv_file: str, recorded_items: List,
 
 def write_csv(result_csv_file: str, new_row: List) -> None:
     """ Write the results of current round. """
-    with open(result_csv_file, 'a', encoding='utf-8',) as result_file:
+    with open(
+            result_csv_file,
+            'a',
+            encoding='utf-8',
+    ) as result_file:
         result_writer = csv.writer(result_file)
         result_writer.writerow(new_row)
