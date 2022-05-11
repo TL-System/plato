976860ba729110f3a0e5a21dad8830482e4547c3
Ningxin Su <ningxin.su@mail.utoronto.ca>
2022-05-11 16:10:20 -0400

    Finished Step 2. "Do retraining from the start_round rather than the first round." and Step 4. Do retraining from the start_round rather than the first round.

--- examples/fedunlearning_baseline/fedunlearning_server.py
+++ examples/fedunlearning_baseline/fedunlearning_server.py
@@ -12,6 +12,11 @@ import time
 from plato.config import Config
 from plato.servers import fedavg
 
+def decode_config_with_comma(target_string):
+    if isinstance(target_string, int):
+        return [target_string]       
+    else: 
+        return list(map(lambda x: int(x), target_string.split(", ")))
 
 class Server(fedavg.Server):
     """ A federated unlearning server that implements the federated unlearning baseline 
@@ -21,6 +26,14 @@ class Server(fedavg.Server):
     def __init__(self, model=None, algorithm=None, trainer=None):
         super().__init__(model=model, algorithm=algorithm, trainer=trainer)
         self.restarted_session = True
+        self.clients_dic = {}
+
+    async def select_clients(self, for_next_batch=False):
+        await super().select_clients(for_next_batch)
+
+        for client_id in self.selected_clients:
+            if not client_id in self.clients_dic.keys():
+                self.clients_dic[client_id] = self.current_round
 
     async def register_client(self, sid, client_id):
         """ Adding a newly arrived client to the list of clients. """
@@ -28,7 +41,7 @@ class Server(fedavg.Server):
             # The last contact time is stored for each client
             self.clients[client_id] = {
                 'sid': sid,
-                'last_contacted': time.perf_counter()
+                'last_contacted': time.perf_counter() 
             }
             logging.info("[%s] New client with id #%d arrived.", self,
                          client_id)
@@ -36,6 +49,7 @@ class Server(fedavg.Server):
             self.clients[client_id]['last_contacted'] = time.perf_counter()
             logging.info("[%s] New contact from Client #%d received.", self,
                          client_id)
+
 
         if (self.current_round == 0 or self.resumed_session) and len(
                 self.clients) >= self.clients_per_round:
@@ -50,21 +64,44 @@ class Server(fedavg.Server):
     async def wrap_up_processing_reports(self):
         """ Wrap up processing the reports with any additional work. """
         await super().wrap_up_processing_reports()
+        
+        client_requesting_deletion = decode_config_with_comma(Config().clients.client_requesting_deletion)
 
-        if (self.current_round == Config().clients.data_deletion_round
+        are_clients_selected_before_retrain = any([client_id in self.clients_dic.keys() for client_id in client_requesting_deletion])
+        print(self.clients_dic)
+        print("-------")
+        print(client_requesting_deletion)
+        print("-------")
+        print([client_id in self.clients_dic.keys() for client_id in client_requesting_deletion])
+        
+        if not are_clients_selected_before_retrain: 
+             logging.info("[%s] Clients are not selected before data_deletion_round.", client_requesting_deletion)
+
+        elif (self.current_round == Config().clients.data_deletion_round
             ) and self.restarted_session:
             logging.info("[%s] Data deleted. Retraining from the first round.",
                          self)
-            self.current_round = 0
+            print("self.current_round: ", self.current_round)     
+            client_requesting_deletion = decode_config_with_comma(Config().clients.client_requesting_deletion)
+            start_retrain_round = [self.clients_dic[client_id] for client_id in client_requesting_deletion if client_id in self.clients_dic.keys()]
+            initial_checkpoint_round = min(start_retrain_round)
             self.restarted_session = False
-
+            print("*************************")
+            print(client_requesting_deletion)
+            print(start_retrain_round)
+            print("initial_checkpoint_round: ", initial_checkpoint_round)
             # Loading the saved model the server for resuming the training session from round 1
             checkpoint_dir = Config.params['checkpoint_dir']
 
             model_name = Config().trainer.model_name if hasattr(
                 Config().trainer, 'model_name') else 'custom'
-            filename = f"checkpoint_{model_name}_{self.current_round}.pth"
+            filename = f"checkpoint_{model_name}_{initial_checkpoint_round}.pth"
             self.trainer.load_model(filename, checkpoint_dir)
+            # The function select_clients() in server/base.py will add 1 to current_round
+            self.current_round = initial_checkpoint_round - 1
+            print("self.current_round: ", self.current_round)
+        else:
+            pass
 
     async def customize_server_response(self, server_response):
         """ Wrap up generating the server response with any additional information. """
