d3c8fa849849a8c1a2d313768b7d74863e7f82f9
Baochun Li <bli@ece.toronto.edu>
2022-05-14 14:54:51 -0400

    Finished a preliminary draft of the code refactoring work.

--- examples/fedunlearning_baseline/fedunlearning_client.py
+++ examples/fedunlearning_baseline/fedunlearning_client.py
@@ -16,19 +16,46 @@ from plato.config import Config
 class Client(simple.Client):
     """A federated learning client of federated unlearning."""
 
+    def __init__(self,
+                 model=None,
+                 datasource=None,
+                 algorithm=None,
+                 trainer=None):
+        super().__init__(model=model,
+                         datasource=datasource,
+                         algorithm=algorithm,
+                         trainer=trainer)
+
+        self.previous_round = {}
+
     def process_server_response(self, server_response):
-        if server_response['retrain_phase'] or self.current_round > Config(
-        ).clients.data_deletion_round:
-            if self.client_id in Config().clients.client_requesting_deletion:
-                logging.info(
-                    "[%s] Unlearning sampler deployed: %s%% of the samples were deleted.",
-                    self,
-                    Config().clients.deleted_data_ratio * 100)
+        """
+        If a client requested deletion, replace its sampler accordingly in the
+        retraining phase.
+        """
+        if self.client_id in self.previous_round:
+            previous_round = self.previous_round[self.client_id]
+        else:
+            previous_round = 0
 
-                if (hasattr(Config().data, 'reload_data')
-                        and Config().data.reload_data) or not self.data_loaded:
-                    logging.info("[%s] Loading the dataset.", self)
-                    self.load_data()
+        client_pool = Config().clients.clients_requesting_deletion
+        print(self.client_id)
+        print(self.current_round)
+        print(previous_round)
+        print(client_pool)
 
-                self.sampler = unlearning_iid.Sampler(self.datasource,
-                                                      self.client_id, False)
+        if self.client_id in client_pool and self.current_round <= previous_round:
+            logging.info(
+                "[%s] Unlearning sampler deployed: %s%% of the samples were deleted.",
+                self,
+                Config().clients.deleted_data_ratio * 100)
+
+            if (hasattr(Config().data, 'reload_data')
+                    and Config().data.reload_data) or not self.data_loaded:
+                logging.info("[%s] Loading the dataset.", self)
+                self.load_data()
+
+            self.sampler = unlearning_iid.Sampler(self.datasource,
+                                                  self.client_id, False)
+
+        self.previous_round[self.client_id] = self.current_round
--- examples/fedunlearning_baseline/fedunlearning_server.py
+++ examples/fedunlearning_baseline/fedunlearning_server.py
@@ -31,19 +31,17 @@ class Server(fedavg.Server):
 
     def __init__(self, model=None, algorithm=None, trainer=None):
         super().__init__(model=model, algorithm=algorithm, trainer=trainer)
-        self.restarted_session = True
-        # Store client_id as keys, the round that corresponding client first be selected as values
-        self.clients_arrive_round = {}
-        self.retrain_phase = False
-        # If current round is in retrain phase, retrain_phase becomes True
-        self.start_retrain_round = []
+        self.retraining = False
+
+        # A dictionary that maps client IDs to the first round when the server selected it
+        self.round_first_selected = {}
 
     async def select_clients(self, for_next_batch=False):
         await super().select_clients(for_next_batch)
 
         for client_id in self.selected_clients:
-            if not client_id in self.clients_arrive_round:
-                self.clients_arrive_round[client_id] = self.current_round
+            if not client_id in self.round_first_selected:
+                self.round_first_selected[client_id] = self.current_round
 
     async def register_client(self, sid, client_id):
         """ Adding a newly arrived client to the list of clients. """
@@ -74,56 +72,45 @@ class Server(fedavg.Server):
         """ Wrap up processing the reports with any additional work. """
         await super().wrap_up_processing_reports()
 
-        are_clients_selected_before_retrain = any([
-            client_id in self.clients_arrive_round
-            for client_id in Config().clients.client_requesting_deletion
-        ])
+        clients_to_delete = Config().clients.clients_requesting_deletion
 
-        if not are_clients_selected_before_retrain:
-            logging.info(
-                "[%s] Clients are not selected before data_deletion_round.",
-                Config().clients.client_requesting_deletion)
+        if (self.current_round == Config().clients.data_deletion_round
+            ) and not self.retraining:
+            # If data_deletion_round equals to the current round at server for the first time,
+            # and the clients requesting retraining has been selected before, the retraining
+            # phase starts
+            earliest_round = self.current_round
 
-        elif (self.current_round == Config().clients.data_deletion_round
-              ) and self.restarted_session:
-            # If data_deletion_round equals to the current round at server at the first time
-            # retrain phase start
-            self.retrain_phase = True
+            for client_id, first_round in self.round_first_selected.items():
+                if client_id in clients_to_delete:
+                    self.retraining = True
 
-            for client_to_delete in Config(
-            ).clients.client_requesting_deletion:
-                if client_to_delete in self.clients_arrive_round:
-                    self.start_retrain_round.append(
-                        self.clients_arrive_round[client_to_delete])
+                    if earliest_round > first_round:
+                        earliest_round = first_round
 
-            self.current_round = min(self.start_retrain_round) - 1
-            self.restarted_session = False
+            if self.retraining:
+                self.current_round = earliest_round - 1
 
-            logging.info(
-                "[%s] Data deleted. Retraining from the states after round #%s.",
-                self, self.current_round)
-
-            # Loading the saved model on the server for resuming the training session from round 1
-            checkpoint_dir = Config.params['checkpoint_dir']
-
-            model_name = Config().trainer.model_name if hasattr(
-                Config().trainer, 'model_name') else 'custom'
-            filename = f"checkpoint_{model_name}_{self.current_round}.pth"
-            self.trainer.load_model(filename, checkpoint_dir)
-            logging.info(
-                "[Server #%d] Model used for the retraining phase loaded from %s.",
-                os.getpid(), checkpoint_dir)
-
-            if hasattr(Config().clients,
-                       'exact_retrain') and Config().clients.exact_retrain:
-                # Loading the PRNG states on the server in preparation for the retraining phase
                 logging.info(
-                    "[Server #%d] Random states after round #%s restored for exact retraining.",
-                    os.getpid(), self.current_round)
-                self.restore_random_states(self.current_round, checkpoint_dir)
+                    "[%s] Data deleted. Retraining from the states after round #%s.",
+                    self, self.current_round)
 
-    async def customize_server_response(self, server_response):
-        """ Wrap up generating the server response with any additional information. """
-        server_response['current_round'] = self.current_round
-        server_response['retrain_phase'] = self.retrain_phase
-        return server_response
+                # Loading the saved model on the server for starting the retraining phase
+                checkpoint_dir = Config.params['checkpoint_dir']
+
+                model_name = Config().trainer.model_name if hasattr(
+                    Config().trainer, 'model_name') else 'custom'
+                filename = f"checkpoint_{model_name}_{self.current_round}.pth"
+                self.trainer.load_model(filename, checkpoint_dir)
+                logging.info(
+                    "[Server #%d] Model used for the retraining phase loaded from %s.",
+                    os.getpid(), checkpoint_dir)
+
+                if hasattr(Config().clients,
+                           'exact_retrain') and Config().clients.exact_retrain:
+                    # Loading the PRNG states on the server in preparation for the retraining phase
+                    logging.info(
+                        "[Server #%d] Random states after round #%s restored for exact retraining.",
+                        os.getpid(), self.current_round)
+                    self.restore_random_states(self.current_round,
+                                               checkpoint_dir)
