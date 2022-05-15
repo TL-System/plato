8b5b9191e4223d5420ef7cb78b356ebad5fd7f5b
Baochun Li <bli@ece.toronto.edu>
2022-05-15 12:00:48 -0400

    Improved the implementation of federated unlearning (#172)

--- examples/fedunlearning_baseline/fedunlearning_client.py
+++ examples/fedunlearning_baseline/fedunlearning_client.py
@@ -21,21 +21,35 @@ class Client(simple.Client):
                  datasource=None,
                  algorithm=None,
                  trainer=None):
-        super().__init__(model, datasource, algorithm, trainer)
+        super().__init__(model=model,
+                         datasource=datasource,
+                         algorithm=algorithm,
+                         trainer=trainer)
+
         self.previous_round = {}
-        self.testset_sampler = None
 
     def process_server_response(self, server_response):
+        """
+        If a client requested deletion, replace its sampler accordingly in the
+        retraining phase.
+        """
         if self.client_id in self.previous_round:
             previous_round = self.previous_round[self.client_id]
         else:
             previous_round = 0
 
-        if self.current_round == 1 and self.current_round <= previous_round:
+        client_pool = Config().clients.clients_requesting_deletion
+
+        if self.client_id in client_pool and self.current_round <= previous_round:
             logging.info(
                 "[%s] Unlearning sampler deployed: %s%% of the samples were deleted.",
                 self,
                 Config().clients.deleted_data_ratio * 100)
+
+            if (hasattr(Config().data, 'reload_data')
+                    and Config().data.reload_data) or not self.data_loaded:
+                logging.info("[%s] Loading the dataset.", self)
+                self.load_data()
 
             self.sampler = unlearning_iid.Sampler(self.datasource,
                                                   self.client_id, False)
