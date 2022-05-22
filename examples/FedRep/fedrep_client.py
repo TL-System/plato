df275655dc580b34260d5f8d77985506d08a917d
Baochun Li <bli@ece.toronto.edu>
2022-05-22 10:28:41 -0400

    Refactored the FedRep implementation (preliminary work).

---
+++ examples/FedRep/fedrep_client.py
@@ -0,0 +1,41 @@
+"""
+A personalized federated learning client using FedRep.
+
+Reference:
+
+Collins et al., "Exploiting Shared Representations for Personalized Federated
+Learning", in the Proceedings of ICML 2021.
+
+https://arxiv.org/abs/2102.07078
+
+Source code: https://github.com/lgcollins/FedRep
+"""
+
+from plato.clients import simple
+
+
+class Client(simple.Client):
+
+    def __init__(self,
+                 model=None,
+                 datasource=None,
+                 algorithm=None,
+                 trainer=None):
+        super().__init__(model, datasource, algorithm, trainer)
+        self.representation_param_names = []
+
+    def process_server_response(self, server_response) -> None:
+        """Additional client-specific processing on the server response."""
+        super().process_server_response(server_response)
+
+        self.representation_param_names = server_response[
+            "representation_keys"]
+
+        # The representation keys are regarded as the global model.
+        # This needs to be set in the trainer for training the
+        # global and local model according to the FedRep algorithm.
+        self.trainer.set_representation_and_head(
+            global_parameter_names=self.representation_param_names)
+
+        self.algorithm.set_global_parameter_names(
+            global_parameter_names=self.representation_param_names)
---
+++ examples/FedRep/fedrep_server.py
@@ -0,0 +1,64 @@
+"""
+A personalized federated learning server using FedRep.
+
+Reference:
+
+Collins et al., "Exploiting Shared Representations for Personalized Federated
+Learning", in the Proceedings of ICML 2021.
+
+https://arxiv.org/abs/2102.07078
+
+Source code: https://github.com/lgcollins/FedRep
+"""
+
+import logging
+
+from plato.servers import fedavg
+
+
+class Server(fedavg.Server):
+
+    def __init__(self, model=None, algorithm=None, trainer=None):
+        super().__init__(model, algorithm, trainer)
+
+        self.representation_param_names = []
+
+    def extract_representation_param_names(self):
+        """ Obtain the weights responsible for representation. """
+
+        model_full_parameter_names = list(
+            self.trainer.model.state_dict().keys())
+        # in general, the weights before the final layer are regarded as
+        #   the representation.
+        # then the final layer is the last two key in the obtained key list.
+        # For example,
+        #  lenet:
+        #  ['conv1.weight', 'conv1.bias', 'conv2.weight', 'conv2.bias', 'conv3.weight', 'conv3.bias', 'fc4.weight', 'fc4.bias', 'fc5.weight', 'fc5.bias']
+        #  representaion:
+        #   ['conv1.weight', 'conv1.bias', 'conv2.weight', 'conv2.bias', 'conv3.weight', 'conv3.bias', 'fc4.weight', 'fc4.bias']
+        self.representation_param_names = model_full_parameter_names[:-2]
+
+        logging.info("Representation_weights: %s",
+                     self.representation_param_names)
+
+    def load_trainer(self):
+        """ rewrite the load_trainer func to further extract the representaion keys """
+        super().load_trainer()
+
+        self.extract_representation_param_names()
+
+        # the representation keys are regarded as the global model
+        #   this needs to be set in the trainer for training the
+        #   global and local model in the FedRep's way
+        self.trainer.set_representation_and_head(
+            global_parameter_names=self.representation_param_names)
+
+        self.algorithm.set_global_parameter_names(
+            global_parameter_names=self.representation_param_names)
+
+    async def customize_server_response(self, server_response):
+        """ Wrap up generating the server response with any additional information. """
+        # server sends the required the representaion to the client
+        server_response[
+            "representation_keys"] = self.representation_param_names
+        return server_response
