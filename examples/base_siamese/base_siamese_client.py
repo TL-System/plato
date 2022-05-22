61e92c9b2bb072f947be4bae6bfdc9f2ee8bcfaa
CSJdeveloper <csjdeveloper@gmail.cpm>
2022-05-21 22:39:11 -0400

    Updated the improved client, server and trainer for base siamese method

--- examples/base_siamese/base_siamese_client.py
+++ examples/base_siamese/base_siamese_client.py
@@ -3,13 +3,10 @@ Implement the client for the basic siamese method.
 
 """
 
-import logging
-
 from plato.config import Config
 from plato.clients import simple
-from plato.datasources import registry as datasources_registry
+
 from plato.datasources import datawrapper_registry
-from plato.samplers import registry as samplers_registry
 
 
 class Client(simple.Client):
@@ -20,7 +17,6 @@ class Client(simple.Client):
                  algorithm=None,
                  trainer=None):
         super().__init__(model, datasource, algorithm, trainer)
-        self.model_representation_weights_key = []
 
     def load_data(self) -> None:
         """Generating data and loading them onto this client."""
--- examples/base_siamese/base_siamese_trainer.py
+++ examples/base_siamese/base_siamese_trainer.py
@@ -2,12 +2,18 @@
 Implement the trainer for base siamese method.
 
 """
+import os
+import logging
+import time
 
+import numpy as np
 import torch
 import torch.nn as nn
+from opacus.privacy_engine import PrivacyEngine
 
 from plato.config import Config
 from plato.trainers import basic
+from plato.utils import optimizers
 
 
 class ContrastiveLoss(nn.Module):
@@ -28,6 +34,21 @@ class ContrastiveLoss(nn.Module):
         return loss_contrastive
 
 
+#   Here are two stages to test the contrastive learning:
+#   - The first stage is that when training the contrastive learning methods,
+#      the model is tested based on the contrastive evaluation, i.e. similarity.
+#   - The second stage is to test the learned representation contrastive methods.
+#      the learned representation is utilized for downstream tasks, such as classification
+#      thus, the task-specific metric, such as the accuracy should be used.
+#
+#      For example,
+#     In the second stage, there are two methods to test the contrastive learning,
+#       1- linear evaluation,
+#       first learn representations from the framework.
+#       Then, train a new linear classifier on the frozen representations.
+#   2-
+
+
 class Trainer(basic.Trainer):
 
     def __init__(self, model=None):
@@ -39,3 +60,191 @@ class Trainer(basic.Trainer):
         defined_margin = Config().trainer.margin
         constrative_loss_computer = ContrastiveLoss(margin=defined_margin)
         return constrative_loss_computer
+
+    def train_loop(self, config, trainset, sampler, cut_layer):
+        """ The default training loop when a custom training loop is not supplied. """
+        batch_size = config['batch_size']
+        log_interval = 10
+        tic = time.perf_counter()
+
+        logging.info("[Client #%d] Loading the dataset.", self.client_id)
+        _train_loader = getattr(self, "train_loader", None)
+
+        if callable(_train_loader):
+            train_loader = self.train_loader(batch_size, trainset, sampler,
+                                             cut_layer)
+        else:
+            train_loader = torch.utils.data.DataLoader(dataset=trainset,
+                                                       shuffle=False,
+                                                       batch_size=batch_size,
+                                                       sampler=sampler)
+
+        iterations_per_epoch = np.ceil(len(trainset) / batch_size).astype(int)
+        epochs = config['epochs']
+
+        # Sending the model to the device used for training
+        self.model.to(self.device)
+        self.model.train()
+
+        # Initializing the loss criterion
+        _loss_criterion = getattr(self, "loss_criterion", None)
+        if callable(_loss_criterion):
+            loss_criterion = self.loss_criterion(self.model)
+        else:
+            loss_criterion = torch.nn.CrossEntropyLoss()
+
+        # Initializing the optimizer
+        get_optimizer = getattr(self, "get_optimizer",
+                                optimizers.get_optimizer)
+        optimizer = get_optimizer(self.model)
+
+        # Initializing the learning rate schedule, if necessary
+        if hasattr(config, 'lr_schedule'):
+            lr_schedule = optimizers.get_lr_schedule(optimizer,
+                                                     iterations_per_epoch,
+                                                     train_loader)
+        else:
+            lr_schedule = None
+
+        if 'differential_privacy' in config and config['differential_privacy']:
+            privacy_engine = PrivacyEngine(accountant='rdp', secure_mode=False)
+
+            self.model, optimizer, train_loader = privacy_engine.make_private_with_epsilon(
+                module=self.model,
+                optimizer=optimizer,
+                data_loader=train_loader,
+                target_epsilon=config['dp_epsilon']
+                if 'dp_epsilon' in config else 10.0,
+                target_delta=config['dp_delta']
+                if 'dp_delta' in config else 1e-5,
+                epochs=epochs,
+                max_grad_norm=config['dp_max_grad_norm']
+                if 'max_grad_norm' in config else 1.0,
+            )
+
+        for epoch in range(1, epochs + 1):
+            # Use a default training loop
+            for batch_id, (examples1, examples2,
+                           labels) in enumerate(train_loader):
+                examples1, examples2, labels = examples1.to(
+                    self.device), examples2.to(self.device), labels.to(
+                        self.device)
+                examples = (examples1, examples2)
+                if 'differential_privacy' in config and config[
+                        'differential_privacy']:
+                    optimizer.zero_grad(set_to_none=True)
+                else:
+                    optimizer.zero_grad()
+
+                if cut_layer is None:
+                    outputs = self.model(examples)
+                else:
+                    outputs = self.model.forward_from(examples, cut_layer)
+
+                loss = loss_criterion(outputs, labels)
+
+                if 'create_graph' in config:
+                    loss.backward(create_graph=config['create_graph'])
+                else:
+                    loss.backward()
+
+                optimizer.step()
+
+                if batch_id % log_interval == 0:
+                    if self.client_id == 0:
+                        logging.info(
+                            "[Server #%d] Epoch: [%d/%d][%d/%d]\tLoss: %.6f",
+                            os.getpid(), epoch, epochs, batch_id,
+                            len(train_loader), loss.data.item())
+                    else:
+                        logging.info(
+                            "[Client #%d] Epoch: [%d/%d][%d/%d]\tLoss: %.6f",
+                            self.client_id, epoch, epochs, batch_id,
+                            len(train_loader), loss.data.item())
+
+            if lr_schedule is not None:
+                lr_schedule.step()
+
+            if hasattr(optimizer, "params_state_update"):
+                optimizer.params_state_update()
+
+            # Simulate client's speed
+            if self.client_id != 0 and hasattr(
+                    Config().clients,
+                    "speed_simulation") and Config().clients.speed_simulation:
+                self.simulate_sleep_time()
+
+            # Saving the model at the end of this epoch to a file so that
+            # it can later be retrieved to respond to server requests
+            # in asynchronous mode when the wall clock time is simulated
+            if hasattr(Config().server,
+                       'request_update') and Config().server.request_update:
+                self.model.cpu()
+                training_time = time.perf_counter() - tic
+                filename = f"{self.client_id}_{epoch}_{training_time}.pth"
+                self.save_model(filename)
+                self.model.to(self.device)
+
+    def test_process(self, config, testset, sampler=None):
+        """The testing loop, run in a separate process with a new CUDA context,
+        so that CUDA memory can be released after the training completes.
+
+        Arguments:
+        config: a dictionary of configuration parameters.
+        testset: The test dataset.
+        sampler: The sampler that extracts a partition of the test dataset.
+        """
+        self.model.to(self.device)
+        self.model.eval()
+
+        # Initialize accuracy to be returned to -1, so that the client can disconnect
+        # from the server when testing fails
+        accuracy = -1
+
+        try:
+            custom_test = getattr(self, "test_model", None)
+
+            if callable(custom_test):
+                accuracy = self.test_model(config, testset)
+            else:
+                if sampler is None:
+                    test_loader = torch.utils.data.DataLoader(
+                        testset,
+                        batch_size=config['batch_size'],
+                        shuffle=False)
+                # Use a testing set following the same distribution as the training set
+                else:
+                    test_loader = torch.utils.data.DataLoader(
+                        testset,
+                        batch_size=config['batch_size'],
+                        shuffle=False,
+                        sampler=sampler.get())
+
+                correct = 0
+                total = 0
+
+                with torch.no_grad():
+                    for examples1, examples2, labels in test_loader:
+                        examples1, examples2, labels = examples1.to(
+                            self.device), examples2.to(self.device), labels.to(
+                                self.device)
+                        examples = (examples1, examples2)
+                        outputs = self.model(examples)
+
+                        _, predicted = torch.max(outputs.data, 1)
+                        total += labels.size(0)
+                        correct += (predicted == labels).sum().item()
+
+                accuracy = correct / total
+        except Exception as testing_exception:
+            logging.info("Testing on client #%d failed.", self.client_id)
+            raise testing_exception
+
+        self.model.cpu()
+
+        if 'max_concurrency' in config:
+            model_name = config['model_name']
+            filename = f"{model_name}_{self.client_id}_{config['run_id']}.acc"
+            self.save_accuracy(accuracy, filename)
+        else:
+            return accuracy
