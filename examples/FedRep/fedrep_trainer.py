a78664ca646b7924ddc5fc9a3023d1f416e57cd1
CSJdeveloper <csjdeveloper@gmail.cpm>
2022-05-24 21:35:33 -0400

    Added more comments to the trainer

--- examples/FedRep/fedrep_trainer.py
+++ examples/FedRep/fedrep_trainer.py
@@ -35,13 +35,16 @@ class Trainer(basic.Trainer):
         self.head_param_names = []
 
     def set_representation_and_head(self, representation_param_names):
-        """ Setting the global local weights key. """
-        # the representation keys are obtained from
-        #   the server response
+        """ Setting the parameter names for global and local models. """
+
+        # set the parameter names for the representation
+        #   As mentioned by Eq. 1 and Fig. 2 of the paper, the representation
+        #   behaves as the global model.
         self.representation_param_names = representation_param_names
 
         # FedRep calls the weights and biases of the final fully-connected layer
         # in each of the models as the "head"
+        # This insight is obtained from the source code of FedRep.
         model_parameter_names = self.model.state_dict().keys()
 
         self.head_param_names = [
@@ -128,13 +131,17 @@ class Trainer(basic.Trainer):
 
         for epoch in range(1, epochs + 1):
 
+            # As presented in the Section 3 of the FedRep paper,
+            #   the head is optimized for (epochs - 1) while frozing
+            #   the representation
             if epoch <= head_epochs:
                 for name, param in self.model.named_parameters():
                     if name in self.representation_param_names:
                         param.requires_grad = False
                     else:
                         param.requires_grad = True
-
+            # Then, the representation will be optimized for only one
+            #   epoch.
             if epoch > head_epochs:
                 for name, param in self.model.named_parameters():
                     if name in self.representation_param_names:
