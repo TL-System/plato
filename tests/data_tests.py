4dd1e42ada3ba57a86e1c4b08521edcb883705c6
CSJdeveloper <csjdeveloper@gmail.cpm>
2022-05-29 12:04:38 -0400

    Fixed the similar issue in the data_tests and the sampler tests

--- tests/data_tests.py
+++ tests/data_tests.py
@@ -5,11 +5,27 @@ Testing multi-modal datasources of Plato framework.
 import os
 import unittest
 
-os.environ['config_file'] = 'tests/TestsConfig/flickr30k_entities.yml'
+os.environ['config_file'] = 'tests/TestsConfig/referitgame.yml'
+
+import numpy as np
+import torch
+
+from sampler_test_utils import define_sampler
+
+from plato.config import Config
+
+# from plato.datasources.flickr30k_entities import DataSource as f30ke_DataSource
+from plato.datasources.referitgame import DataSource as refer_Datasource
+# from plato.datasources.coco import DataSource as coco_Datasource
+# from plato.datasources.kinetics import DataSource as kinetics_Datasource
+# from plato.datasources.gym import DataSource as GymDataSource
+from plato.samplers import registry as samplers_registry
+
+from plato.samplers import modality_iid
+
+# os.environ['config_file'] = 'tests/TestsConfig/flickr30k_entities.yml'
 
 # os.environ['config_file'] = 'tests/TestsConfig/coco.yml'
-
-# os.environ['config_file'] = 'tests/TestsConfig/referitgame.yml'
 
 # os.environ['config_file'] = 'tests/TestsConfig/kinetics.yml'
 
@@ -18,25 +34,10 @@ os.environ['config_file'] = 'tests/TestsConfig/flickr30k_entities.yml'
 # Note: the plato will search the dir './config' for Pipeline and other configuration files
 #   directly by default. This is achieved by the code in line 83 of 'config.py'
 
-import numpy as np
-import torch
-
-from plato.config import Config
-
-from plato.datasources.flickr30k_entities import DataSource as f30ke_DataSource
-# from plato.datasources.referitgame import DataSource as refer_Datasource
-# from plato.datasources.coco import DataSource as coco_Datasource
-# from plato.datasources.kinetics import DataSource as kinetics_Datasource
-# from plato.datasources.gym import DataSource as GymDataSource
-from plato.samplers import registry as samplers_registry
-
-from plato.samplers import modality_iid
-
-from sampler_test_utils import define_sampler
-
 
 class DatasetsTest(unittest.TestCase):
     """ Aiming to test the correcness of implemented samplers and datasets """
+
     def setUp(self):
         super().setUp()
 
@@ -51,7 +52,7 @@ class DatasetsTest(unittest.TestCase):
         # the datasource being tested
         self.utest_datasource = None
 
-    def assertDataSourceDefinition(self, data_source):
+    def assert_datasource_definition(self, data_source):
         """ Test whether the dataset can be correctly defined.
             This verifies:
             1.1- The datasource can be defined
@@ -86,29 +87,28 @@ class DatasetsTest(unittest.TestCase):
             batch_size=batch_size,
             sampler=defined_sampler.get())
 
-        obtained_mm_sample = next(iter(testset_loader))
-        print("obtained_sample: ", obtained_mm_sample)
+        _ = next(iter(testset_loader))
 
         return True
 
-    def test_f30ke_datasource(self):
-        """ Test the flickr30k entities dataset. """
-        self.utest_datasource = f30ke_DataSource()
-        assert self.assertDataSourceDefinition(self.utest_datasource)
+    # def test_f30ke_datasource(self):
+    #     """ Test the flickr30k entities dataset. """
+    #     self.utest_datasource = f30ke_DataSource()
+    #     assert self.assert_datasource_definition(self.utest_datasource)
 
     # def test_coco_datasource(self):
     #     """ Test the MSCOCO dataset. """
     #     # set the specific
 
     #     self.utest_datasource = coco_Datasource()
-    #     # assert self.assertDataSourceDefinition(self.utest_datasource)
+    #     # assert self.assert_datasource_definition(self.utest_datasource)
 
-    # def test_ref_datasource(self):
-    #     """ Test the ReferItGmae dataset. """
-    #     # set the specific
+    def test_ref_datasource(self):
+        """ Test the ReferItGmae dataset. """
+        # set the specific
 
-    #     self.utest_datasource = refer_Datasource()
-    #     assert self.assertDataSourceDefinition(self.utest_datasource)
+        self.utest_datasource = refer_Datasource()
+        assert self.assert_datasource_definition(self.utest_datasource)
 
     # def test_kinetics_datasource(self):
     #     """ Test the kinetics700 dataset. """
@@ -123,14 +123,14 @@ class DatasetsTest(unittest.TestCase):
     #     print(iter_data["rgb"]["imgs"].shape)
     #     print(iter_data["rgb"]["label"].shape)
 
-    #     assert self.assertDataSourceDefinition(self.utest_datasource)
+    #     assert self.assert_datasource_definition(self.utest_datasource)
 
     # def test_gym_datasource(self):
     #     """ Test the Gym dataset. """
     #     # set the specific
 
     #     self.utest_datasource = GymDataSource()
-    #     assert self.assertDataSourceDefinition(self.utest_datasource)
+    #     assert self.assert_datasource_definition(self.utest_datasource)
 
 
 if __name__ == '__main__':
