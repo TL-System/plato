2480c670d708f376df1c35ffc992c50f656659e8
Ningxin Su <ningxin.su@mail.utoronto.ca>
2022-05-29 11:36:23 -0400

    Correct the config file and list the variables in server

--- examples/fedun_clustering/fedun_cluster_server.py
+++ examples/fedun_clustering/fedun_cluster_server.py
@@ -5,18 +5,26 @@ A customized server for the federated unlearning baseline clustering algorithm.
 import logging
 import os
 import time
+import random
 
 from plato.config import Config
 from plato.servers import fedavg
 
 
 class Server(fedavg.Server):
-    """ A federated unlearning server that implements the federated unlearning 
-    baseline clustering algorithm.
+    """ A federated unlearning server that implements the federated unlearning baseline
+     clustering algorithm.
     The work pipeline of the server is as below.
     1. The server divided total clients randomly into N clusters.
     2. The clients then do the training.
     3. Aggregate the updates in clusters.
-    4. Do the global aggregation(aggregate from all clusters) at the target 
-    round and stop training.
+    4. Do the global aggregation(aggregate from all clusters) at the target round and stop training.
     """
+
+    def clustering_aggregation():
+        """Randomly divide clients by their Ids into several clusters, and aggregation in clusters."""
+        total_clients = Config().clients.total_clients
+        clusters = Config().server.clusters
+        rounds = Config().trainer.rounds
+        #The first row is cluster_id, and second element is the client_id
+        clustering_matrix == [][]
