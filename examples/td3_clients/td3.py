8ad069cb92a0da2f03e02b717e3b124452c0190b
danielliucs <daniell.liu@mail.utoronto.ca>
2022-06-01 16:45:14 -0400

    fixed comments

--- examples/td3_clients/td3.py
+++ examples/td3_clients/td3.py
@@ -10,14 +10,13 @@ import td3_trainer
 
 
 def main():
-    """ A Plato federated learning training session using the FEI algorithm. """
+    """ A Plato federated learning training session using TD3. """
     logging.info("Starting RL Environment's process.")
     trainer = td3_trainer.Trainer
     client = td3_learning_client.RLClient(trainer=trainer)
     agent = td3_learning_agent.RLAgent()
     server = fedavg.Server(trainer=trainer, model=agent)
     server.run(client)
-
 
 if __name__ == "__main__":
     main()
