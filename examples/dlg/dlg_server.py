deb8184916772a4267dbbf85324c244578f352e9
Fei Wang <silviafey.w@gmail.com>
2022-06-01 08:39:05 -0400

    Implement weight matching (debugging)

--- examples/dlg/dlg_server.py
+++ examples/dlg/dlg_server.py
@@ -14,8 +14,10 @@ https://papers.nips.cc/paper/2019/file/60a6c4002cc7b29142def8871531281a-Paper.pd
 """
 
 import logging
-import lpips
+from collections import OrderedDict
+from copy import deepcopy
 
+import lpips
 import matplotlib.pyplot as plt
 import torch
 import torch.nn.functional as F
@@ -26,6 +28,8 @@ from torchvision import transforms
 from utils import cross_entropy_for_onehot
 
 criterion = cross_entropy_for_onehot
+# TODO: replace hard coded
+loss_criterion = torch.nn.CrossEntropyLoss()
 tt = transforms.ToPILImage()
 loss_fn = lpips.LPIPS(net='vgg')
 torch.manual_seed(Config().algorithm.random_seed)
@@ -56,8 +60,10 @@ class Server(fedavg.Server):
         # Receive the ground truth for evaluation
         # It will not be used for data reconstruction
         gt_data, gt_label, target_grad = payload[1]
+        target_weight = payload[0]
 
-        if not (hasattr(Config().algorithm, 'share_gradients') and Config().algorithm.share_gradients):
+        if not (hasattr(Config().algorithm, 'share_gradients') and Config().algorithm.share_gradients) and \
+                not (hasattr(Config().algorithm, 'match_weight') and Config().algorithm.match_weight):
             # Obtain the local updates from clients
             deltas_received = self.compute_weight_deltas(updates)
             target_grad = []
@@ -91,22 +97,16 @@ class Server(fedavg.Server):
         mses = []
         lpipss = []
 
+        # Sharing and matching updates
+        if not (hasattr(Config().algorithm, 'share_gradients') and Config().algorithm.share_gradients) \
+                and hasattr(Config().algorithm, 'match_weight') and Config().algorithm.match_weight:
+            closure = self.weight_closure(
+                optimizer, dummy_data, dummy_label, target_weight)
+        else:
+            closure = self.gradient_closure(
+                optimizer, dummy_data, dummy_label, target_grad)
+
         for iters in range(Config().algorithm.num_iters):
-            def closure():
-                optimizer.zero_grad()
-
-                dummy_pred = self.trainer.model(dummy_data)
-                dummy_onehot_label = F.softmax(dummy_label, dim=-1)
-                dummy_loss = criterion(dummy_pred, dummy_onehot_label)
-                dummy_grad = torch.autograd.grad(
-                    dummy_loss, self.trainer.model.parameters(), create_graph=True)
-
-                grad_diff = 0
-                for gx, gy in zip(dummy_grad, target_grad):
-                    grad_diff += ((gx - gy) ** 2).sum()
-                grad_diff.backward()
-                return grad_diff
-
             optimizer.step(closure)
             current_loss = closure().item()
             losses.append(current_loss)
@@ -128,3 +128,58 @@ class Server(fedavg.Server):
         logging.info("[Gradient Leakage Attacking...] Reconstructed label is %d.",
                      torch.argmax(dummy_label, dim=-1).item())
         plt.show()
+
+    def gradient_closure(self, optimizer, dummy_data, dummy_label, target_grad):
+        def closure():
+            optimizer.zero_grad()
+            # self.trainer.model.zero_grad()
+            dummy_pred = self.trainer.model(dummy_data)
+            dummy_onehot_label = F.softmax(dummy_label, dim=-1)
+            dummy_loss = criterion(dummy_pred, dummy_onehot_label)
+            dummy_grad = torch.autograd.grad(
+                dummy_loss, self.trainer.model.parameters(), create_graph=True)
+
+            grad_diff = 0
+            for gx, gy in zip(dummy_grad, target_grad):
+                grad_diff += ((gx - gy) ** 2).sum()
+            grad_diff.backward()
+            return grad_diff
+        return closure
+
+    def weight_closure(self, optimizer, dummy_data, dummy_label, target_weight):
+        def closure():
+            optimizer.zero_grad()
+            # self.trainer.model.zero_grad()
+            dummy_weight = self.loss_steps(dummy_data, dummy_label)
+
+            weight_diff = 0
+            for wx, wy in zip(dummy_weight, [target_weight]):
+                weight_diff += ((wx - wy) ** 2).sum()
+            weight_diff.backward()
+            return weight_diff
+        return closure
+
+    def loss_steps(self, dummy_data, dummy_label):
+        """Take a few gradient descent steps to fit the model to the given input."""
+        model = deepcopy(self.trainer.model)
+        epochs = Config().trainer.epochs
+        batch_size = Config().trainer.batch_size
+        # TODO: use_updates or not
+        for epoch in range(epochs):
+            if batch_size == 0:
+                # TODO: model deep copy
+                dummy_pred = model(dummy_data)
+                labels_ = dummy_label
+            else:
+                idx = epoch % (dummy_data.shape[0] // batch_size)
+                dummy_pred = model(
+                    dummy_data[idx * batch_size:(idx + 1) * batch_size])
+                labels_ = dummy_label[idx * batch_size:(idx + 1) * batch_size]
+            loss = loss_criterion(dummy_pred, labels_)
+            grad = torch.autograd.grad(loss, model.parameters.values(),
+                                       retain_graph=True, create_graph=True, only_inputs=True)
+
+            model.parameters = OrderedDict((name, param - Config().trainer.learning_rate * grad_part)
+                                           for ((name, param), grad_part)
+                                           in zip(model.parameters.items(), grad))
+        return list(model.parameters.values())
