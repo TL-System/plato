2ab0824dae7af5c22b3b59accb3320c7c5e5ce77
CSJdeveloper <csjdeveloper@gmail.cpm>
2022-06-02 16:45:01 -0400

    Changed to code to utilize the resnet from torchvision directly

--- plato/models/encoders_register.py
+++ plato/models/encoders_register.py
@@ -1,11 +1,20 @@
 """
 Implementation of the encoder register
 
+    For the encoder with ResNet as backbone, we do not utilize 
+the ones implemented under the plato/models. The main reason is 
+that the plato's version ignore the AdaptiveAvgPool2d. Then, all current
+methods of self-supervised utilize the resnet from torchvision to perform
+the learning process. For the fair comparsion, we decide to utilize the 
+one from torchvision directly.
+
+
 """
 
 import logging
 
 from torch import nn
+import torchvision
 
 from plato.models import registry as model_register
 from plato.config import Config
@@ -48,26 +57,34 @@ def get():
         "Define the encoder from the model: %s without final fully-connected layers",
         model_name)
 
-    model = model_register.get()
-
     if model_name == "lenet5":
+        model = model_register.get()
         # get encoding dimensions
         #   i.e., the output dim of the encoder
         encode_output_dim = model.fc4.in_features
         encoder = TruncatedLeNetModel(model)
 
     if "vgg" in model_name:
-        encoder = model
+        encoder = model_register.get()
         # get encoding dimensions
         #   i.e., the output dim of the encoder
         encode_output_dim = encoder.fc.in_features
         encoder.fc = Identity()
 
     if "resnet" in model_name:
-        encoder = model
+        resnets = {
+            "resnet_18": torchvision.models.resnet18,
+            "resnet_50": torchvision.models.resnet50,
+        }
+
+        num_classes = 10
+        if hasattr(Config().data, 'num_classes'):
+            num_classes = Config().data.num_classes
+        encoder = resnets[model_name](num_classes=num_classes)
+
         # get encoding dimensions
         #   i.e., the output dim of the encoder
-        encode_output_dim = encoder.linear.in_features
-        encoder.linear = Identity()
+        encode_output_dim = encoder.fc.in_features
+        encoder.fc = Identity()
 
     return encoder, encode_output_dim
