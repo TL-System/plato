2dcdf5cc17968e65a46712e4734717e2abfbc88e
CSJdeveloper <csjdeveloper@gmail.cpm>
2022-06-02 22:01:55 -0400

    Removed the base siamese that is not well tested under current PR;
    It will be added once the PR is merged

--- examples/base_siamese/personalized_model.py
+++
@@ -1,9 +0,0 @@
-""" The personalized model for the client. 
-
-    - Linear evaluation. 
-        Based on the learnt representation, each client creates linear network to
-      perform its own task, such as image classification.
-"""
-
-import torch
-import torch.nn as nn
