Corrected the computation of iterations per epoch when using a learning rate schedule.

examples/FedRep/fedrep_trainer.py
@@ -84,16 +84,11 @@ class Trainer(basic.Trainer):
                                                        batch_size=batch_size,
                                                        sampler=sampler)
 
-        iterations_per_epoch = np.ceil(len(trainset) / batch_size).astype(int)
-        # load the total local update epochs
         epochs = config['epochs']
+
         # load the local update epochs for head optimization
         head_epochs = config[
             'head_epochs'] if 'head_epochs' in config else epochs - 1
-
-        # Sending the model to the device used for training
-        self.model.to(self.device)
-        self.model.train()
 
         # Initializing the loss criterion
         _loss_criterion = getattr(self, "loss_criterion", None)
@@ -108,12 +103,15 @@ class Trainer(basic.Trainer):
         optimizer = get_optimizer(self.model)
 
         # Initializing the learning rate schedule, if necessary
-        if hasattr(config, 'lr_schedule'):
+        if 'lr_schedule' in config:
             lr_schedule = optimizers.get_lr_schedule(optimizer,
-                                                     iterations_per_epoch,
+                                                     len(train_loader),
                                                      train_loader)
         else:
             lr_schedule = None
+
+        self.model.to(self.device)
+        self.model.train()
 
         for epoch in range(1, epochs + 1):
 