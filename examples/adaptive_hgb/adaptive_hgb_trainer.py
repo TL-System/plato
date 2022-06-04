Corrected the computation of iterations per epoch when using a learning rate schedule.

examples/adaptive_hgb/adaptive_hgb_trainer.py
@@ -235,23 +235,23 @@ class Trainer(basic.Trainer):
                                                   num_workers=config.data.get(
                                                       'workers_per_gpu', 1))
 
-        iterations_per_epoch = np.ceil(len(trainset) / batch_size).astype(int)
         epochs = config['epochs']
 
-        # Sending the model to the device used for training
-        self.model.to(self.device)
-        self.model.train()
         # Initializing the optimizer
         get_optimizer = getattr(self, "get_optimizer",
                                 optimizers.get_optimizer)
         optimizer = get_optimizer(self.model)
+
         # Initializing the learning rate schedule, if necessary
-        if hasattr(config, 'lr_schedule'):
+        if 'lr_schedule' in config:
             lr_schedule = optimizers.get_lr_schedule(optimizer,
-                                                     iterations_per_epoch,
+                                                     len(train_loader),
                                                      train_loader)
         else:
             lr_schedule = None
+
+        self.model.to(self.device)
+        self.model.train()
 
         # operate the local training
         supported_modalities = trainset.supported_modalities

examples/mistnetplus/split_learning_trainer.py
@@ -36,18 +36,14 @@ class Trainer(basic.Trainer):
                                                        batch_size=batch_size,
                                                        sampler=sampler)
 
-        iterations_per_epoch = np.ceil(len(trainset) / batch_size).astype(int)
-
-        # Sending the model to the device used for training
-        self.model.to(self.device)
-        self.model.train()
+        epochs = config['epochs']
 
         # Initializing the loss criterion
         _loss_criterion = getattr(self, "loss_criterion", None)
         if callable(_loss_criterion):
             loss_criterion = self.loss_criterion(self.model)
         else:
-            loss_criterion = nn.CrossEntropyLoss()
+            loss_criterion = torch.nn.CrossEntropyLoss()
 
         # Initializing the optimizer
         get_optimizer = getattr(self, "get_optimizer",
@@ -55,12 +51,15 @@ class Trainer(basic.Trainer):
         optimizer = get_optimizer(self.model)
 
         # Initializing the learning rate schedule, if necessary
-        if hasattr(Config().trainer, 'lr_schedule'):
+        if 'lr_schedule' in config:
             lr_schedule = optimizers.get_lr_schedule(optimizer,
-                                                     iterations_per_epoch,
+                                                     len(train_loader),
                                                      train_loader)
         else:
             lr_schedule = None
+
+        self.model.to(self.device)
+        self.model.train()
 
         for __, (examples, labels) in enumerate(train_loader):
             examples, labels = examples.to(self.device), labels.to(self.device)