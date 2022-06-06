640c7a1194bc45d54292bfe120c3b3b1a3c4c3eb
Salma Emara <salma.emara@mail.utoronto.ca>
2022-06-06 00:37:02 -0400

    Added commeents TODO

--- examples/td3_learning/td3.py
+++ examples/td3_learning/td3.py
@@ -1,5 +1,5 @@
 """
-A federated learning training session using td3
+A federated learning training session with clients running td3
 """
 import logging
 
@@ -19,10 +19,13 @@ trainer = td3_learning_trainer.Trainer(globals.state_dim, globals.action_dim)
 evaluations = [td3_learning_trainer.Trainer.evaluate_policy(trainer)]
 
 def main():
-    """ A Plato federated learning training session using TD3. """
+    """ A Plato federated learning training session with clients running TD3. """
     logging.info("Starting RL Environment's process.")
 
     """ A Plato federated learning training session using a custom model. """
+    # TODO: there should be an actor and critic model communicated with clients.
+    # TODO: Not only one single model. 
+    # TODO: This model should be two models. How? I still do not know.
     model = nn.Sequential(
         nn.Linear(28 * 28, 128),
         nn.ReLU(),
