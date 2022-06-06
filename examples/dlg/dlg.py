335f4c18217d588e252dbcbe6c62ac15ed417aa9
Fei Wang <silviafey.w@gmail.com>
2022-06-06 16:41:47 -0400

    Migrate MetaMonkey and loss steps to plato
    The implementation is from
    
    Geiping et al., "Inverting Gradients - How easy is it to break privacy in federated learning?" in Advances in Neural Information Processing Systems 2020.
    
    https://proceedings.neurips.cc/paper/2020/file/c4ede56bbd98819ae6112b20ac6bf145-Paper.pdf

--- examples/dlg/dlg.py
+++ examples/dlg/dlg.py
@@ -2,13 +2,6 @@
 A federated learning training session with the honest-but-curious server.
 The server can analyze periodic gradients from certain clients to
 perform the gradient leakage attacks and reconstruct the training data of the victim clients.
-
-Reference:
-
-Zhu et al., "Deep Leakage from Gradients,"
-in Advances in Neural Information Processing Systems 2019.
-
-https://papers.nips.cc/paper/2019/file/60a6c4002cc7b29142def8871531281a-Paper.pdf
 """
 import dlg_client
 import dlg_server
