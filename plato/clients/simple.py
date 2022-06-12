18a205b1a835df529a473ec5a8b867d3dfc676c1
Fei Wang <silviafey.wang@utoronto.ca>
2022-06-12 17:43:21 -0400

    Supported passing custom models and datasources as classes into Plato. (#201)
    Co-authored-by: Baochun Li <bli@ece.toronto.edu>

--- plato/clients/simple.py
+++ plato/clients/simple.py
@@ -35,7 +35,8 @@ class Client(base.Client):
         self.custom_model = model
         self.model = None
 
-        self.datasource = datasource
+        self.custom_datasource = datasource
+        self.datasource = None
 
         self.custom_algorithm = algorithm
         self.algorithm = None
@@ -82,10 +83,18 @@ class Client(base.Client):
         """Generating data and loading them onto this client."""
         logging.info("[%s] Loading its data source...", self)
 
-        if self.datasource is None or (hasattr(Config().data, 'reload_data')
-                                       and Config().data.reload_data):
+        if self.datasource is None and self.custom_datasource is None or (
+                hasattr(Config().data, 'reload_data')
+                and Config().data.reload_data):
+            # The only case where Config().data.reload_data is set to true is
+            # when clients with different client IDs need to load from different datasets,
+            # such as in the pre-partitioned Federated EMNIST dataset. We do not support
+            # reloading data from a custom datasource at this time.
             self.datasource = datasources_registry.get(
                 client_id=self.client_id)
+        elif self.datasource is None and self.custom_datasource is not None:
+            self.datasource = self.custom_datasource()
+            self.custom_datasource = None
 
         logging.info("[%s] Dataset size: %s", self,
                      self.datasource.num_train_examples())
