41583498b8aa1b4ab088a5159ac444d67c907c1d
Salma Emara <salma.emara@mail.utoronto.ca>
2022-06-13 14:10:22 -0400

    Remove random initialization and normalization

--- examples/td3_learning/td3_learning_model.py
+++ examples/td3_learning/td3_learning_model.py
@@ -17,11 +17,11 @@ class TD3Actor(nn.Module):
     def __init__(self, state_dim, action_dim, max_action):
         super(TD3Actor, self).__init__()
         self.l1 = nn.Linear(state_dim, 400)
-        nn.init.uniform_(self.l1.weight.data)
+        #nn.init.uniform_(self.l1.weight.data)
         self.l2 = nn.Linear(400, 300)
-        nn.init.uniform_(self.l2.weight.data)
+        #nn.init.uniform_(self.l2.weight.data)
         self.l3 = nn.Linear(300, action_dim)
-        nn.init.uniform_(self.l3.weight.data)
+        #nn.init.uniform_(self.l3.weight.data)
         self.max_action = max_action
 
     def forward(self, x, hidden=None):
@@ -29,8 +29,8 @@ class TD3Actor(nn.Module):
         x = F.relu(self.l2(x))
         x = self.max_action * torch.tanh(self.l3(x))
         # Normalize/Scaling aggregation weights so that the sum is 1
-        x += 1  # [-1, 1] -> [0, 2]
-        x /= x.sum()
+        #x += 1  # [-1, 1] -> [0, 2]
+        #x /= x.sum()
         return x
 
 
@@ -48,17 +48,19 @@ class TD3Critic(nn.Module):
         self.l5 = nn.Linear(400, 300)
         self.l6 = nn.Linear(300, 1)
 
-    def forward(self, state, action, hidden1=None, hidden2=None):
+    def forward(self, state, action):
         sa = torch.cat([state, action], 1)
+        # Forward-Propagation on the first Critic Neural Network
         q1 = F.relu(self.l1(sa))
         q1 = F.relu(self.l2(q1))
         q1 = self.l3(q1)
+        # Forward-Propagation on the second Critic Neural Network
         q2 = F.relu(self.l4(sa))
         q2 = F.relu(self.l5(q2))
         q2 = self.l6(q2)
         return q1, q2
 
-    def Q1(self, state, action, hidden=None):
+    def Q1(self, state, action):
         sa = torch.cat([state, action], 1)
         q1 = F.relu(self.l1(sa))
         q1 = F.relu(self.l2(q1))
@@ -72,11 +74,11 @@ class Model:
         self.critic = TD3Critic(globals.state_dim, globals.action_dim)
         self.actor_target = copy.deepcopy(self.actor)
         self.critic_target = copy.deepcopy(self.critic)
-
-       #self.actor = TD3Actor(state_dim, action_dim, max_action)
-        #self.critic = TD3Critic(state_dim, action_dim)
-        #self.actor_target = copy.deepcopy(self.actor)
-        #self.critic_target = copy.deepcopy(self.critic)
+        #self.actor_target = TD3Actor(globals.state_dim, globals.action_dim, globals.max_action)
+        #self.actor_target = self.actor_target.load_state_dict(self.actor.state_dict())
+        #self.critic_target = TD3Critic(globals.state_dim, globals.action_dim)
+        #self.critic_target = self.critic_target.load_state_dict(self.critic.state_dict())
+    
 
     def cpu(self):
         self.actor.cpu()
