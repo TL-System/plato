0348fbb476b24e382e5c6956452c159431d08495
CSJdeveloper <csjdeveloper@gmail.cpm>
2022-06-16 11:27:10 -0400

    Reset the resource allocation of sbatch submission; Added to code to support cancel multiple jobs

---
+++ examples/contrastive_adaptation/scancel_slurm_jobs.py
@@ -0,0 +1,30 @@
+"""
+Implementation of canceling multiples experiments with Slurm.
+
+
+The type of the experiments should be provided by:
+    - ids
+
+Thus, the jobs containing the ids will be cancelled.
+
+"""
+
+import argparse
+import os
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i',
+                        '--ids',
+                        nargs='+',
+                        default=[],
+                        help='The jobs with ids will be cancelled.')
+
+    args = parser.parse_args()
+
+    jobs_id = [int(input_id) for input_id in args.ids]
+    print(f"Cancelling job with id: {jobs_id}")
+
+    for id in jobs_id:
+        os.system("scancel %s" % id)
