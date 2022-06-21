2606d1f651f4717c6fed88ac5c166707b08a6465
CSJdeveloper <csjdeveloper@gmail.cpm>
2022-06-20 22:10:48 -0400

    Removed useless sim run scripts

--- examples/contrastive_adaptation/run_scripts/byol_MNIST_lenet5.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-#SBATCH --time=5:00:00 
-#SBATCH --cpus-per-task=21 
-#SBATCH --gres=gpu:1 
-#SBATCH --mem=100G 
-#SBATCH --output=./slurm_loggings/byol_MNIST_lenet5.out 
- 
-/home/sijia/envs/miniconda3/envs/INFOCOM23/bin/python ./examples/contrastive_adaptation/byol/byol.py -c ./examples/contrastive_adaptation/configs/byol_MNIST_lenet5.yml -b /data/sijia/INFOCOM23/experiments 
--- examples/contrastive_adaptation/run_scripts/moco_CIFAR10_resnet18.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-#SBATCH --time=5:00:00 
-#SBATCH --cpus-per-task=21 
-#SBATCH --gres=gpu:1 
-#SBATCH --mem=100G 
-#SBATCH --output=./slurm_loggings/moco_CIFAR10_resnet18.out 
- 
-/home/sijia/envs/miniconda3/envs/INFOCOM23/bin/python ./examples/contrastive_adaptation/moco/moco.py -c ./examples/contrastive_adaptation/configs/moco_CIFAR10_resnet18.yml -b /data/sijia/INFOCOM23/experiments 
