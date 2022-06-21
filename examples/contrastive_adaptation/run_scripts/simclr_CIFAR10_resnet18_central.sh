f3ffc08f4a78fef8683e0f9510607438bba5665c
CSJdeveloper <csjdeveloper@gmail.cpm>
2022-06-20 22:10:48 -0400

    Removed useless sim run scripts

--- examples/contrastive_adaptation/run_scripts/simclr_CIFAR10_resnet18_central.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-#SBATCH --time=5:00:00 
-#SBATCH --cpus-per-task=21 
-#SBATCH --gres=gpu:1 
-#SBATCH --mem=100G 
-#SBATCH --output=./slurm_loggings/simclr_CIFAR10_resnet18_central.out 
- 
-/home/sijia/envs/miniconda3/envs/INFOCOM23/bin/python ./examples/contrastive_adaptation/simclr/simclr.py -c ./examples/contrastive_adaptation/configs/simclr_CIFAR10_resnet18_central.yml -b /data/sijia/INFOCOM23/experiments 
