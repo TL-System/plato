2455d978dee98e5d95f5d83de4d20304f0cd3513
Yufei-Kang <1175801998@qq.com>
2022-06-30 16:46:34 -0400

    Represent non-iid level by weight_deltas instead of local gradient norm.
    So it's agnostic to client local solver.

--- examples/async_selection/async_selection_client.py
+++ examples/async_selection/async_selection_client.py
@@ -2,12 +2,6 @@ from plato.clients import simple
 from dataclasses import dataclass
 
 
-@dataclass
-class Report(simple.Report):
-    """Client report sent to the FedSarah federated learning server."""
-    local_gradient_norm: float
-
-
 class Client(simple.Client):
     """A personalized federated learning client using the FedRep algorithm."""
     def __init__(self,
@@ -23,9 +17,4 @@ class Client(simple.Client):
         """ Initialize the server control variate and client control variate for trainer. """
         report, weights = await super().train()
 
-        # calculate local gradient norm
-
-        return Report(
-            report.num_samples, report.accuracy, report.training_time,
-            report.comm_time, report.update_response, self.local_gradient_norm
-        ), weights  # return stalenss and local gradient norm to server for next round sampling
+        return report, weights  # return stalenss and local gradient norm to server for next round sampling
