7e3e172eb050653020b2765f4465cb39241d3cf6
Yufei-Kang <1175801998@qq.com>
2022-06-30 15:39:51 -0400

    Build up the communication channel between client and server.
    Clients send local gradient norm and staleness to server. Server receives them and extracts them as lists.

--- examples/async_selection/async_selection.py
+++ examples/async_selection/async_selection.py
@@ -6,10 +6,12 @@ import os
 os.environ['config_file'] = 
 
 import async_selection_server
+import async_selection_client
 
 def main():
     """ A Plato federated learning training session using the FedSarah algorithm. """
     server = async_selection_server.Server()
+    client = async_selection_client.Client()
 
     server.run()
 
--- examples/async_selection/async_selection_client.py
+++ examples/async_selection/async_selection_client.py
@@ -1,26 +1,31 @@
 from plato.clients import simple
+from dataclasses import dataclass
+
+
+@dataclass
+class Report(simple.Report):
+    """Client report sent to the FedSarah federated learning server."""
+    local_gradient_norm: float
 
 
 class Client(simple.Client):
     """A personalized federated learning client using the FedRep algorithm."""
-
     def __init__(self,
                  model=None,
                  datasource=None,
                  algorithm=None,
                  trainer=None):
         super().__init__(model, datasource, algorithm, trainer)
+        self.stalenss = None
+        self.local_gradient_norm = None
 
     async def train(self):
         """ Initialize the server control variate and client control variate for trainer. """
         report, weights = await super().train()
-        # calculate staleness
 
         # calculate local gradient norm
 
-    
-
-    return Report(report.num_samples, report.accuracy, 
-                      report.training_time, comm_time, report.update_response,
-                      2), [weights, self.client_control_variates]# return stalenss and local gradient norm to server for next round sampling
-
+        return Report(
+            report.num_samples, report.accuracy, report.training_time,
+            report.comm_time, report.update_response, self.local_gradient_norm
+        ), weights  # return stalenss and local gradient norm to server for next round sampling
