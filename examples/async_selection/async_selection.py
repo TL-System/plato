b4a9df8025f11c8cc9bdb5a66ca3af6e4bda550e
Yufei-Kang <1175801998@qq.com>
2022-07-01 17:11:20 -0400

    Fixed some bugs.

--- examples/async_selection/async_selection.py
+++ examples/async_selection/async_selection.py
@@ -3,15 +3,16 @@ A federated learning training session with asynchronous client selection.
 """
 import os
 
-os.environ['config_file'] = 
+#os.environ['config_file'] =
 
 import async_selection_server
-import async_selection_client
+#import async_selection_client
+
 
 def main():
     """ A Plato federated learning training session using the FedSarah algorithm. """
     server = async_selection_server.Server()
-    client = async_selection_client.Client()
+    #client = async_selection_client.Client()
 
     server.run()
 
