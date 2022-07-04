de4f21d9c6a1ab2f17b70eb0081495bc87e0b682
Salma Emara <salma.emara@mail.utoronto.ca>
2022-07-04 00:49:00 -0400

    Report fisher gradients

--- examples/park_env/a2c_learning_client.py
+++ examples/park_env/a2c_learning_client.py
@@ -30,7 +30,7 @@ class RLClient(simple.Client):
 
         actor_loss, critic_loss, entropy_loss = self.get_loss()
         actor_grad, critic_grad = self.get_grad()
-        actor_fisher, critic_fisher = self.get_fisher()
+        actor_fisher, critic_fisher, actor_fisher_grad, critic_fisher_grad = self.get_fisher()
         
         return Report(report.num_samples, report.accuracy, report.training_time, \
          report.comm_time, report.update_response, self.client_id, actor_loss, critic_loss, \
