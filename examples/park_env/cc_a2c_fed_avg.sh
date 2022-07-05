dc56d60198faea5016af61291e4d4faf61b47d78
danielliucs <daniell.liu@mail.utoronto.ca>
2022-07-05 11:42:19 -0400

    Multiple yml files as well as updates to sh files

--- examples/park_env/cc_a2c_fed_avg.sh
+++ examples/park_env/cc_a2c_fed_avg.sh
@@ -3,14 +3,15 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=128G
 #SBATCH --account=def-baochun
-#SBATCH --output=results_fed_avg.out
+#SBATCH --output=results_fed_avg_seed_5.out
 
 module load gcc/9.3.0 arrow cuda/11 python/3.9 scipy-stack
 source ~/.federated/bin/activate
-python examples/park_env/a2c.py -c examples/park_env/a2c_fedavg.yml
+python examples/park_env/a2c.py -c examples/park_env/a2c_fedavg_seed_5.yml
 
 #To run, connect to Graham, follow the Running.md file under docs, move this file to the main plato directory and run from there
 #Remember to copy over data folder as Graham does not allow external downloads
 #Tip: make the output file seed specific
 #Make sure that before submitting another batch that it runs the yml file you want, otherwise
 #if you make changes to your yml file it will run that LATEST version of that yml file
+#change port numbers everytime
--- examples/park_env/cc_a2c_fisher_aggregate.sh
+++ examples/park_env/cc_a2c_fisher_aggregate.sh
@@ -3,11 +3,11 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=128G
 #SBATCH --account=def-baochun
-#SBATCH --output=results_fisher.out
+#SBATCH --output=results_fisher_seed_5.out
 
 module load gcc/9.3.0 arrow cuda/11 python/3.9 scipy-stack
 source ~/.federated/bin/activate
-python examples/park_env/a2c.py -c examples/park_env/a2c_fisher_aggregate.yml
+python examples/park_env/a2c.py -c examples/park_env/a2c_fisher_aggregate_seed_5.yml
 
 #To run, connect to Graham, follow the Running.md file under docs, move this file to the main plato directory and run from there
 #Remember to copy over data folder as Graham does not allow external downloads
