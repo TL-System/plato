351f7e166936da928a8988e50df68098412aa4b3
Salma Emara <salma.emara@mail.utoronto.ca>
2022-07-11 16:27:46 -0400

    Corrected the file that plotted the bandwidth

--- examples/park_env/park/envs/abr_sim/traces-all/plot_bandwidth.py
+++ examples/park_env/park/envs/abr_sim/traces-all/plot_bandwidth.py
@@ -2,25 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-PACKET_SIZE = 1500.0  # bytes
 TIME_INTERVAL = 5.0
 BITS_IN_BYTE = 8.0
 MBITS_IN_BITS = 1000000.0
 MILLISECONDS_IN_SECONDS = 1000.0
 N = 100
-LINK_FILE = './bus.ljansbakken-oslo-report.2010-09-28_1407CEST.log'
+LINK_FILE = './3bandwidth_generated_changing.log'
 
 
-bandwidth_all = []
+bandwidth_all, time_all = [], []
 with open(LINK_FILE, 'r') as f:
 	for line in f:
-		throughput = int(float(line.split()[0]))
+		throughput = int(float(line.split()[1]))
 		bandwidth_all.append(throughput)
+		time = int(float(line.split()[0]))
+		time_all.append(time)
 
 bandwidth_all = np.array(bandwidth_all)
 bandwidth_all = bandwidth_all * BITS_IN_BYTE / MBITS_IN_BITS
+time_all = np.array(time_all)
 
-time_all = np.array(range(len(bandwidth_all))) * TIME_INTERVAL
 plt.plot(time_all, bandwidth_all)
 plt.xlabel('Time (second)')
 plt.ylabel('Throughput (Mbit/sec)')
