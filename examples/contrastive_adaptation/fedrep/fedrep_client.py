33c88153e5a5c71fcd26e3ea43433681ddf7aba8
CSJdeveloper <csjdeveloper@gmail.cpm>
2022-07-21 16:31:30 -0400

    Updated the fedrep and fedavg based on the latest revision
    This update does not influence the performance but make it more readable

---
+++ examples/contrastive_adaptation/fedrep/fedrep_client.py
@@ -0,0 +1,26 @@
+"""
+Implementation of the fedrep clients.
+
+"""
+
+import logging
+
+from plato.clients import pers_simple
+from plato.config import Config
+
+
+class Client(pers_simple.Client):
+    """A personalized federated learning client with self-supervised support
+        for the contrastive adaptation method."""
+
+    def load_personalized_model(self):
+        """ Initial the personalized model with the global model. """
+
+        model_name = Config().trainer.model_name
+        personalized_model_name = Config().trainer.personalized_model_name
+        logging.info(
+            "[Client #%d] copy the downloaded global model [%s] (after completion) as its personalized model [%s].",
+            self.client_id, model_name, personalized_model_name)
+        # should directly copy from the downloaded model
+        self.trainer.personalized_model.load_state_dict(
+            self.trainer.model.state_dict())
