b366b96b70a287a3e853dafa355aff2bda9d1ce3
Yufei-Kang <1175801998@qq.com>
2022-07-28 16:26:29 -0400

    Added timestamp and write .sh file itself into the .out file.

--- fedavg_MNIST.sh
+++ fedavg_MNIST.sh
@@ -4,12 +4,29 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=64G
 #SBATCH --output=FedAvg_MNIST_lenet5_500_20eachRound_10least_no_test_stalenss_10.out
-filename='./configs/MNIST/fedavg_lenet5_noiid.yml'
-echo "The configuration filename is: $filename"
+echo "==============================================="
+echo "This .out file is generated at: "
+date
+echo "==============================================="
+echo " " 
+
+filename1 = './fedavg_MNIST.sh'
+echo "The bash filename is: $filename1"
 echo " " 
 while read line; do
 # reading each line
 echo "$line"
 
-done < $filename
+done < $filename1
+echo " " 
+
+filename2='./configs/MNIST/fedavg_lenet5_noiid.yml'
+echo "The configuration filename is: $filename2"
+echo " " 
+while read line; do
+# reading each line
+echo "$line"
+
+done < $filename2
+
 ./run -c ./configs/MNIST/fedavg_lenet5_noniid.yml -b /data/ykang/plato
