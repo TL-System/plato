1e9261cd0719f295ca0d980b7d49007ccb6ef2eb
Yufei-Kang <1175801998@qq.com>
2022-07-29 11:18:46 -0400

    Update fedavg_MNIST.sh

--- fedavg_MNIST.sh
+++ fedavg_MNIST.sh
@@ -4,29 +4,13 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=64G
 #SBATCH --output=FedAvg_MNIST_lenet5_500_20eachRound_10least_no_test_stalenss_10.out
-echo "==============================================="
-echo "This .out file is generated at: "
-date
-echo "==============================================="
-echo " " 
-
-filename1 = './fedavg_MNIST.sh'
-echo "The bash filename is: $filename1"
+filename='./configs/MNIST/fedavg_lenet5_noiid.yml'
+echo "The configuration filename is: $filename"
 echo " " 
 while read line; do
 # reading each line
 echo "$line"
 
-done < $filename1
-echo " " 
-
-filename2='./configs/MNIST/fedavg_lenet5_noiid.yml'
-echo "The configuration filename is: $filename2"
-echo " " 
-while read line; do
-# reading each line
-echo "$line"
-
-done < $filename2
+done < $filename
 
 ./run -c ./configs/MNIST/fedavg_lenet5_noniid.yml -b /data/ykang/plato
