86271f7a860008b7ef9a23f91207552ece315a15
CSJdeveloper <csjdeveloper@gmail.cpm>
2022-08-01 16:37:28 -0400

    Added the method of training from script

---
+++ examples/contrastive_adaptation/script/script_client.py
@@ -0,0 +1,25 @@
+"""
+Implementation of the pfedavg clients
+
+"""
+
+import logging
+
+from plato.clients import pers_simple
+from plato.config import Config
+
+
+class Client(pers_simple.Client):
+    """A personalized federated learning client with pFL-FedAvg."""
+
+    def load_personalized_model(self):
+        """ Initial the personalized model with the global model. """
+
+        model_name = Config().trainer.model_name
+        personalized_model_name = Config().trainer.personalized_model_name
+        logging.info(
+            "[Client #%d] copy the downloaded global model [%s] (after completion) as its personalized model [%s].",
+            self.client_id, model_name, personalized_model_name)
+        # should directly copy from the downloaded model
+        self.trainer.personalized_model.load_state_dict(
+            self.trainer.model.state_dict())
