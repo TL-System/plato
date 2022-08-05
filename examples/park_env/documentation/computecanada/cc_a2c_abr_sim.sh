ae2c6eebb8fb71716b3762e69ed5314575622d3d
Daniel Liu <daniell.liu@mail.utoronto.ca>
2022-08-04 20:23:14 -0400

    cleaned up batch files

--- examples/park_env/documentation/computecanada/cc_a2c_abr_sim.sh
+++ examples/park_env/documentation/computecanada/cc_a2c_abr_sim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --time=10:00:00
-#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
 #SBATCH --nodes=1
 #SBATCH --mem=128G
 #SBATCH --account=def-baochun
@@ -9,11 +9,3 @@
 module load gcc/9.3.0 arrow cuda/11 python/3.9 scipy-stack
 source ~/.federated/bin/activate
 python examples/park_env/a2c_abr_sim.py --seed5
-
-#Change --seed10 to --seedX where X is the seed you want
-#To run, connect to Graham, follow the Running.md file under docs, move this file to the main plato directory and run from there
-#Remember to copy over data folder as Graham does not allow external downloads
-#Tip: make the output file seed specific
-#Make sure that before submitting another batch that it runs the yml file you want, otherwise
-#if you make changes to your yml file it will run that LATEST version of that yml file
-#change port numbers everytime
