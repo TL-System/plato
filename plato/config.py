50dde835de8d385a5a0a599211694cc73d382062
Baochun Li <bli@ece.toronto.edu>
2022-08-08 10:39:51 -0400

    Fixed a minor issue that breaks PyTorch CI tests.

--- plato/config.py
+++ plato/config.py
@@ -313,7 +313,7 @@ class Config:
 
         if torch.cuda.is_available():
             return torch.cuda.device_count()
-        elif torch.backends.mps.is_available():
+        elif torch.has_mps:
             return 1
         else:
             return 0
@@ -347,7 +347,7 @@ class Config:
                 else:
                     device = "cuda:0"
 
-            if torch.backends.mps.is_available():
+            if torch.has_mps:
                 device = "mps"
 
         return device
