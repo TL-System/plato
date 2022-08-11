eebeb6fb13057e3b3b4a431745be2421a88932aa
danielliucs <daniell.liu@mail.utoronto.ca>
2022-08-11 17:23:24 -0400

    redesigned optimizer

--- plato/trainers/optimizers.py
+++ plato/trainers/optimizers.py
@@ -16,172 +16,57 @@ from plato.utils.step import Step
 from collections import OrderedDict
 
 
-registered_optimizers = OrderedDict(
-    [
-        ("SGD", optim.SGD),
-        ("Adam", optim.Adam),
-        ("Adadelta", optim.Adadelta),
-        ("AdaHessian", torch_optim.Adahessian),
-    ]
-)
-registered_optimizers_parameters = OrderedDict(
-    [
-        (
-            "SGD",
-            OrderedDict(
-                [
-                    (
-                        "lr",
-                        Config().trainer.learning_rate
-                        if hasattr(Config().trainer, "learning_rate")
-                        else 0.001,
-                    ),
-                    (
-                        "momentum",
-                        Config().trainer.momentum
-                        if hasattr(Config().trainer, "momentum")
-                        else 0.937,
-                    ),
-                    (
-                        "weight_decay",
-                        Config().trainer.weight_decay
-                        if hasattr(Config().trainer, "weight_decay")
-                        else 0.00058,
-                    ),
-                ]
-            ),
-        ),
-        (
-            "Adam",
-            OrderedDict(
-                [
-                    (
-                        "lr",
-                        Config().trainer.learning_rate
-                        if hasattr(Config().trainer, "learning_rate")
-                        else 0.001,
-                    ),
-                    (
-                        "weight_decay",
-                        Config().trainer.weight_decay
-                        if hasattr(Config().trainer, "weight_decay")
-                        else 0.00058,
-                    ),
-                ]
-            ),
-        ),
-        (
-            "Adadelta",
-            OrderedDict(
-                [
-                    (
-                        "lr",
-                        Config().trainer.learning_rate
-                        if hasattr(Config().trainer, "learning_rate")
-                        else 0.001,
-                    ),
-                    (
-                        "rho",
-                        Config().trainer.rho
-                        if hasattr(Config().trainer, "rho")
-                        else 1.0,
-                    ),
-                    (
-                        "eps",
-                        float(Config().trainer.eps)
-                        if hasattr(Config().trainer, "eps")
-                        else 1e-3,
-                    ),
-                    (
-                        "weight_decay",
-                        Config().trainer.weight_decay
-                        if hasattr(Config().trainer, "weight_decay")
-                        else 0.00058,
-                    ),
-                ]
-            ),
-        ),
-        (
-            "AdaHessian",
-            OrderedDict(
-                [
-                    (
-                        "lr",
-                        Config().trainer.learning_rate
-                        if hasattr(Config().trainer, "learning_rate")
-                        else 0.001,
-                    ),
-                    (
-                        "betas",
-                        (Config().trainer.momentum_b1, Config().trainer.momentum_b2)
-                        if hasattr(Config().trainer, "momentum_b1")
-                        and hasattr(Config().trainer, "momentum_b2")
-                        else (0.9, 0.999),
-                    ),
-                    (
-                        "eps",
-                        float(Config().trainer.eps)
-                        if hasattr(Config().trainer, "eps")
-                        else 1e-3,
-                    ),
-                    (
-                        "weight_decay",
-                        Config().trainer.weight_decay
-                        if hasattr(Config().trainer, "weight_decay")
-                        else 0.00058,
-                    ),
-                    (
-                        "hessian_power",
-                        Config().trainer.hessian_power
-                        if hasattr(Config().trainer, "hessian_power")
-                        else 1.0,
-                    ),
-                ]
-            ),
-        ),
-    ]
-)
-
-
 def get_optimizer(model) -> optim.Optimizer:
     """Obtain the optimizer used for training the model."""
+    registered_optimizers = OrderedDict(
+        [
+            (
+                "SGD",
+                optim.SGD(
+                    model.parameters(),
+                    lr=Config().trainer.learning_rate,
+                    momentum=Config().trainer.momentum,
+                    weight_decay=Config().trainer.weight_decay,
+                ),
+            ),
+            (
+                "Adam",
+                optim.Adam(
+                    model.parameters(),
+                    lr=Config().trainer.learning_rate,
+                    weight_decay=Config().trainer.weight_decay,
+                ),
+            ),
+            (
+                "Adadelta",
+                optim.Adadelta(
+                    model.parameters(),
+                    lr=Config().trainer.learning_rate,
+                    rho=Config().trainer.rho,
+                    eps=float(Config().trainer.eps),
+                    weight_decay=Config().trainer.weight_decay,
+                ),
+            ),
+            (
+                "AdaHessian",
+                torch_optim.Adahessian(
+                    model.parameters(),
+                    lr=Config().trainer.learning_rate,
+                    betas=(Config().trainer.momentum_b1, Config().trainer.momentum_b2),
+                    eps=float(Config().trainer.eps),
+                    weight_decay=Config().trainer.weight_decay,
+                    hessian_power=Config().trainer.hessian_power,
+                ),
+            ),
+        ]
+    )
     optimizer_name = Config().trainer.optimizer
 
+    optimizer = None
+
     optimizer = registered_optimizers.get(optimizer_name, None)
-    if optimizer_name is None:
+
+    if optimizer is None:
         raise ValueError(f"No such optimizer: {optimizer_name}")
-
-    optimizer_parameters = registered_optimizers_parameters[optimizer_name]
-
-    if optimizer_name == "SGD":
-        optimizer(
-            model.parameters(),
-            lr=optimizer_parameters["lr"],
-            momentum=optimizer_parameters["momentum"],
-            weight_decay=optimizer_parameters["weight_decay"],
-        )
-    elif optimizer_name == "Adam":
-        optimizer(
-            model.parameters(),
-            lr=optimizer_parameters["lr"],
-            weight_decay=optimizer_parameters["weight_decay"],
-        )
-    elif optimizer_name == "Adadelta":
-        optimizer(
-            model.parameters(),
-            lr=optimizer_parameters["lr"],
-            rho=optimizer_parameters["rho"],
-            eps=optimizer_parameters["eps"],
-            weight_decay=optimizer_parameters["weight_decay"],
-        )
-    elif optimizer_name == "AdaHessian":
-        optimizer(
-            model.parameters(),
-            lr=optimizer_parameters["lr"],
-            betas=optimizer_parameters["betas"],
-            eps=optimizer_parameters["eps"],
-            weight_decay=optimizer_parameters["weight_decay"],
-            hessian_power=optimizer_parameters["hessian_power"],
-        )
 
     return optimizer
