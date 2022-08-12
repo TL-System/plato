fabe6d3f102b289f2c60cac2f3eecb7594af58e3
kevinsun203 <kevingrandflyer@gmail.com>
2022-08-12 09:33:24 -0400

    Revised processor for attaching pruning masks.

--- plato/processors/send_mask.py
+++ plato/processors/send_mask.py
@@ -3,12 +3,10 @@ Processor for attaching a pruning mask to the payload if pruning
 had been conducted
 """
 
-import os
-import pickle
+import logging
 from typing import OrderedDict
 import torch
 from plato.processors import model
-from plato.config import Config
 
 
 class Processor(model.Processor):
@@ -18,27 +16,19 @@ class Processor(model.Processor):
     """
 
     def process(self, data: OrderedDict) -> OrderedDict:
-        model_name = (
-            Config().trainer.model_name
-            if hasattr(Config().trainer, "model_name")
-            else "custom"
-        )
-        checkpoint_path = Config().params["checkpoint_path"]
 
-        mask_filename = (
-            f"{checkpoint_path}/{model_name}_client{self.client_id}_mask.pth"
-        )
-        if os.path.exists(mask_filename):
-            with open(mask_filename, "rb") as payload_file:
-                client_mask = pickle.load(payload_file)
-                data = [data, client_mask]
+        data = [data, self.trainer.mask]
+        if self.client_id is None:
+            logging.info(
+                "[Server #%d] Attached pruning mask to payload.", self.server_id
+            )
         else:
-            data = [data, None]
-
+            logging.info(
+                "[Client #%d] Attached pruning mask to payload.", self.client_id
+            )
         return data
 
     def _process_layer(self, layer: torch.Tensor) -> torch.Tensor:
         """
         Process individual layer of the model
         """
-        pass
