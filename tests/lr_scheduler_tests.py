ff1bfcc7a7655b31c798c0600f4fb39c8ec63319
Baochun Li <bli@ece.toronto.edu>
2022-08-13 03:41:18 -0400

    Updated to the LR scheduler test in tests/ to use the latest configuration file format.

--- tests/lr_scheduler_tests.py
+++ tests/lr_scheduler_tests.py
@@ -7,6 +7,7 @@ import numpy as np
 from plato.config import Config
 import plato.models.registry as models_registry
 from plato.trainers import optimizers
+from plato.trainers import lr_schedulers
 
 
 class LrSchedulerTest(unittest.TestCase):
@@ -16,17 +17,30 @@ class LrSchedulerTest(unittest.TestCase):
 
         fields = [
             "optimizer",
-            "lr_schedule",
-            "learning_rate",
+            "lr_scheduler",
+            "model_name",
+        ]
+        params = ["SGD", "LambdaLR", "resnet_18"]
+        Config().trainer = namedtuple("trainer", fields)(*params)
+
+        fields = ["optimizer", "learning_rate"]
+        Config().parameters = namedtuple("parameters", fields)
+
+        fields = [
+            "lr",
             "momentum",
             "weight_decay",
+        ]
+        params = [0.1, 0.5, 0.0]
+        Config().parameters.optimizer = namedtuple("optimizer", fields)(*params)
+
+        fields = [
             "gamma",
             "milestone_steps",
             "warmup_steps",
-            "model_name",
         ]
-        params = ["SGD", "LambdaLR", 0.1, 0.5, 0.0, 0.0, "", "", "resnet_18"]
-        Config().trainer = namedtuple("trainer", fields)(*params)
+        params = [0.0, "", ""]
+        Config().parameters.learning_rate = namedtuple("learning_rate", fields)(*params)
 
         self.model = models_registry.get()
         self.optimizer = optimizers.get(self.model)
@@ -42,16 +56,32 @@ class LrSchedulerTest(unittest.TestCase):
 
             fields = [
                 "optimizer",
-                "lr_schedule",
-                "learning_rate",
-                "momentum",
-                "weight_decay",
-                "gamma",
+                "lr_scheduler",
             ]
-            params = ["SGD", "LambdaLR", 0.1, 0.5, 0.0, 0.0]
+            params = ["SGD", "LambdaLR"]
             Config().trainer = namedtuple("trainer", fields)(*params)
 
-            lrs = optimizers.get_lr_schedule(self.optimizer, 10)
+            fields = ["optimizer", "learning_rate"]
+            Config().parameters = namedtuple("parameters", fields)
+
+            fields = [
+                "lr",
+                "momentum",
+                "weight_decay",
+            ]
+            params = [0.1, 0.5, 0.0]
+            Config().parameters.optimizer = namedtuple("optimizer", fields)(*params)
+
+            fields = [
+                "gamma",
+            ]
+            params = [0.0]
+            Config().parameters.learning_rate = namedtuple("learning_rate", fields)(
+                *params
+            )
+
+            lrs = lr_schedulers.get(self.optimizer, 10)
+
             self.assert_lr_equal(0.1)
             for _ in range(100):
                 lrs.step()
@@ -66,19 +96,34 @@ class LrSchedulerTest(unittest.TestCase):
 
             fields = [
                 "optimizer",
-                "lr_schedule",
-                "learning_rate",
+                "lr_scheduler",
+            ]
+            params = ["SGD", "LambdaLR"]
+            Config().trainer = namedtuple("trainer", fields)(*params)
+
+            fields = ["optimizer", "learning_rate"]
+            Config().parameters = namedtuple("parameters", fields)
+
+            fields = [
+                "lr",
                 "momentum",
                 "weight_decay",
+            ]
+            params = [0.1, 0.5, 0.0]
+            Config().parameters.optimizer = namedtuple("optimizer", fields)(*params)
+
+            fields = [
                 "gamma",
                 "milestone_steps",
             ]
-            params = ["SGD", "LambdaLR", 0.1, 0.5, 0.0, 0.1, "2ep,4ep,7ep,8ep"]
+            params = [0.1, "2ep,4ep,7ep,8ep"]
+            Config().parameters.learning_rate = namedtuple("learning_rate", fields)(
+                *params
+            )
 
-            Config().trainer = namedtuple("trainer", fields)(*params)
             self.assert_lr_equal(0.1)
 
-            lrs = optimizers.get_lr_schedule(self.optimizer, 10)
+            lrs = lr_schedulers.get(self.optimizer, 10)
 
             self.assert_lr_equal(0.1)
             for _ in range(19):
@@ -119,18 +164,32 @@ class LrSchedulerTest(unittest.TestCase):
 
             fields = [
                 "optimizer",
-                "lr_schedule",
-                "learning_rate",
+                "lr_scheduler",
+            ]
+            params = ["SGD", "LambdaLR"]
+            Config().trainer = namedtuple("trainer", fields)(*params)
+
+            fields = ["optimizer", "learning_rate"]
+            Config().parameters = namedtuple("parameters", fields)
+
+            fields = [
+                "lr",
                 "momentum",
                 "weight_decay",
+            ]
+            params = [0.1, 0.5, 0.0]
+            Config().parameters.optimizer = namedtuple("optimizer", fields)(*params)
+
+            fields = [
                 "gamma",
                 "warmup_steps",
             ]
-            params = ["SGD", "LambdaLR", 0.1, 0.5, 0.0, 0.0, "20it"]
+            params = [0.1, "20it"]
+            Config().parameters.learning_rate = namedtuple("learning_rate", fields)(
+                *params
+            )
 
-            Config().trainer = namedtuple("trainer", fields)(*params)
-
-            lrs = optimizers.get_lr_schedule(self.optimizer, 10)
+            lrs = lr_schedulers.get(self.optimizer, 10)
 
             for i in range(20):
                 self.assert_lr_equal(i / 20 * 0.1)
