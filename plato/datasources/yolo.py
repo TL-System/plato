e39f10f7ac4774167b8cd10d98bfd8f5c138484c
Baochun Li <bli@ece.toronto.edu>
2022-08-14 10:42:13 -0400

    Fixed an issue that broken PyTorch YOLOv5 CI tests.

--- plato/datasources/yolo.py
+++ plato/datasources/yolo.py
@@ -11,10 +11,11 @@ import logging
 import os
 
 import torch
-from plato.config import Config
-from yolov5.utils.datasets import LoadImagesAndLabels
+
+from yolov5.utils.dataloaders import LoadImagesAndLabels
 from yolov5.utils.general import check_img_size
 
+from plato.config import Config
 from plato.datasources import base
 
 
