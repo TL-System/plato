a7b1eae69e7778d5de33d1548c45ed81b56546fd
Baochun Li <bli@ece.toronto.edu>
2022-08-14 10:24:36 -0400

    Separated YOLOv5 tests to a separate CI workflow.

--- plato/models/yolov5.py
+++ plato/models/yolov5.py
@@ -14,15 +14,16 @@ except ImportError:
 class Model(yolo.Model):
     """The YOLOV5 model with cut layer support."""
 
-    def __init__(self, model_config=None, num_classes=None, **kwargs):
+    def __init__(self, model_config=None, num_classes=None, cut_layer=None, **kwargs):
         super().__init__(cfg=model_config, ch=3, nc=num_classes)
         Config().params["grid_size"] = int(self.stride.max())
+        self.cut_layer = cut_layer
 
-    def forward_to(self, x, cut_layer=4, profile=False):
+    def forward_to(self, x, profile=False):
         y, dt = [], []  # outputs
 
         for m in self.model:
-            if m.i == cut_layer:
+            if m.i == self.cut_layer:
                 return x
 
             if m.f != -1:  # if not from previous layer
@@ -37,11 +38,11 @@ class Model(yolo.Model):
             y.append(x if m.i in self.save else None)  # save output
         return x
 
-    def forward_from(self, x, cut_layer=4, profile=False):
+    def forward_from(self, x, profile=False):
         y, dt = [], []  # outputs
 
         for m in self.model:
-            if m.i < cut_layer:
+            if m.i < self.cut_layer:
                 y.append(None)
                 continue
 
