0072a8a5dbda156ce754b830b8ae6d3b23ab906e
Chen Ying <c.ying@mail.utoronto.ca>
2022-08-15 17:59:09 -0400

    Cleaned up Oort client.

--- examples/oort/oort_client.py
+++ examples/oort/oort_client.py
@@ -13,18 +13,14 @@ class Client(simple.Client):
     A federated learning client that calculates its statistical utility
     """
 
-    def __init__(self, model=None, datasource=None, algorithm=None, trainer=None):
-        super().__init__(model, datasource, algorithm, trainer)
-        self.statistical_utility = None
-
     def customize_report(self, report: SimpleNamespace) -> SimpleNamespace:
         """Wrap up generating the report with any additional information."""
         train_squared_loss_step = self.trainer.run_history.get_metric_values(
             "train_squared_loss_step"
         )
 
-        self.statistical_utility = report.num_samples * np.sqrt(
+        report.statistics_utility = report.num_samples * np.sqrt(
             1.0 / report.num_samples * sum(train_squared_loss_step)
         )
-        report.statistics_utility = self.statistical_utility
+
         return report
