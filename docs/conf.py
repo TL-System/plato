2aa6eb5dabe30365d3694f1b452070ad11f465df
Baochun Li <bli@ece.toronto.edu>
2022-08-24 05:38:15 -0400

    Fixed compatibility issues in examples/customized.

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = "Plato"
 copyright = "The Plato developers"
 author = "Baochun Li"
-release = "0.4.3"
+release = "0.4.4"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
