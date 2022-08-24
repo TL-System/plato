10ee111d4ec4a442f7c4d6766fda607966cb5781
Yufei-Kang <1175801998@qq.com>
2022-08-23 21:19:37 -0400

    Added clients distributions.

--- yufei_results/results_with_fixed_samplers/FEMNIST/avg9/rand1/analyze_client_selection_fedbuff.py
+++ yufei_results/results_with_fixed_samplers/FEMNIST/avg9/rand1/analyze_client_selection_fedbuff.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 """
 Read .out file and draw clients distribution figure
 """
-with open('oort_FEMNIST_lenet5_avg9_round250.out') as f:
+with open('FedBuff_FEMNIST_lenet5_500_20eachRound_avg9_round150.out') as f:
     for line in f.readlines():
 
         if 'Selected clients' in line:
@@ -13,20 +13,19 @@ with open('oort_FEMNIST_lenet5_avg9_round250.out') as f:
             clients = line[loc + 19:]
             loc_end = clients.find(']')
             # output file saves selected clients set
-            outf = open("oort_selected_clients.txt", "a")
+            outf = open("fedbuff_selected_clients.txt", "a")
             outf.write(clients[0:loc_end])
             outf.write(', ')
             outf.close()
 
-with open("oort_selected_clients.txt") as f:
+with open("fedbuff_selected_clients.txt") as f:
     client_set = []
     for line in f.readlines():
         print(line.split())
         fig, ax = plt.subplots()
-        ax.hist(line.split(','), 1000)
-        plt.xticks(rotation='vertical')
+        ax.hist(line.split(','), 500)
         #ax.xlabel('clinet_id')
         #ax.ylabel('# of being selected')
         #ax.title('async_selected_clients_distribution_rand1_round250')
-        figure_file_name = 'oort_selected_clients_distribution.pdf'
+        figure_file_name = 'fedbuff_selected_clients_distribution.pdf'
         plt.savefig(figure_file_name)
