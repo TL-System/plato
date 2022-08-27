932403282fc59e6bbcbd80139bac32fb81542b8e
Yufei-Kang <1175801998@qq.com>
2022-08-27 11:03:54 -0400

    Imported language model based on different model head.

--- plato/models/huggingface.py
+++ plato/models/huggingface.py
@@ -2,13 +2,12 @@
 Obtaining a model from the PyTorch Hub.
 """
 
-from transformers import AutoModelForCausalLM, AutoConfig
+import transformers
 from plato.config import Config
 
 
 class Model:
-    """The CausalLM model loaded from HuggingFace."""
-
+    """The model loaded from HuggingFace based on model head type."""
     @staticmethod
     def get(model_name=None, **kwargs):  # pylint: disable=unused-argument
         """Returns a named model from HuggingFace."""
@@ -18,9 +17,15 @@ class Model:
             "use_auth_token": None,
         }
 
-        config = AutoConfig.from_pretrained(model_name, **config_kwargs)
+        config = transformers.AutoConfig.from_pretrained(
+            model_name, **config_kwargs)
 
-        return AutoModelForCausalLM.from_pretrained(
+        if (hasattr(Config().trainer, "model_head_type")):
+            automodel = getattr(transformers, Config().trainer.model_head_type)
+        else:
+            automodel = getattr(transformers, "AutoModelForCausalLM")
+
+        return automodel.from_pretrained(
             model_name,
             config=config,
             cache_dir=Config().params["model_path"] + "/huggingface",
