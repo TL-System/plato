08fa2fc123d6a833f6831c813f339c31647bf321
Chenghao Hu <ch.hu@mail.utoronto.ca>
2022-10-15 21:51:10 +0000

    Added an example of customizing the processors

---
+++ examples/customized_processor/customize_callback.py
@@ -0,0 +1,26 @@
+""" 
+Customize the inbound and outbound processor list through callbacks. 
+"""
+
+from plato.callbacks.client import ClientCallback
+from dummy_processor import DummyProcessor
+
+
+class CustomizeProcessorCallback(ClientCallback):
+    """
+    A client callback that dynamically inserts a dummy processor to the existing processor list .
+    """
+
+    def on_inbound_process(self, client, inbound_processor):
+        """
+        Insert a dummpy processor to the inbound processor list.
+        """
+        customized_processor = DummyProcessor(client.client_id, client.current_round)
+        inbound_processor.processors.insert(0, customized_processor)
+
+    def on_outbound_process(self, client, outbound_processor):
+        """
+        Insert a dummpy processor to the outbound processor list.
+        """
+        customized_processor = DummyProcessor(client.client_id, client.current_round)
+        outbound_processor.processors.insert(0, customized_processor)
---
+++ examples/customized_processor/dummy_processor.py
@@ -0,0 +1,26 @@
+"""
+Dummpy processor which can be used to test customize processor feature.
+"""
+
+import logging
+from typing import Any
+from plato.processors import base
+
+
+class DummyProcessor(base.Processor):
+    """A dummpy processor to be instantiated and inserted to the processor list in the runtime."""
+
+    def __init__(self, client_id, current_round, **kwargs) -> None:
+        super().__init__(**kwargs)
+
+        self.client_id = client_id
+        self.current_round = current_round
+
+    def process(self, data: Any) -> Any:
+
+        logging.info(
+            "[Client #%s] Customized dummmy processor is activated at round %s.",
+            self.client_id,
+            self.current_round,
+        )
+        return data
