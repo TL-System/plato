93e2d126c2f977a332a2741b406d9f165e4982e8
Chenghao Hu <ch.hu@mail.utoronto.ca>
2022-10-16 15:27:31 +0000

    Updated example to print current processor names

--- examples/customized_processor/customize_callback.py
+++ examples/customized_processor/customize_callback.py
@@ -2,6 +2,7 @@
 Customize the inbound and outbound processor list through callbacks. 
 """
 
+import logging
 from plato.callbacks.client import ClientCallback
 
 from dummy_processor import DummyProcessor
@@ -16,12 +17,42 @@ class CustomizeProcessorCallback(ClientCallback):
         """
         Insert a dummy processor to the inbound processor list.
         """
-        customized_processor = DummyProcessor(client.client_id, client.current_round)
+        logging.info(
+            "[%s] Current inbound processor list: %s.",
+            client,
+            inbound_processor.processors,
+        )
+        customized_processor = DummyProcessor(
+            client_id=client.client_id,
+            current_round=client.current_round,
+            processor_name="DummyProcessor",
+        )
         inbound_processor.processors.insert(0, customized_processor)
+
+        logging.info(
+            "[%s] Inbound processor list after modification: %s.",
+            client,
+            inbound_processor.processors,
+        )
 
     def on_outbound_process(self, client, outbound_processor):
         """
         Insert a dummy processor to the outbound processor list.
         """
-        customized_processor = DummyProcessor(client.client_id, client.current_round)
+        logging.info(
+            "[%s] Current outbound processor list: %s.",
+            client,
+            outbound_processor.processors,
+        )
+        customized_processor = DummyProcessor(
+            client_id=client.client_id,
+            current_round=client.current_round,
+            processor_name="DummyProcessor",
+        )
         outbound_processor.processors.insert(0, customized_processor)
+
+        logging.info(
+            "[%s] Outbound processor list after modification: %s.",
+            client,
+            outbound_processor.processors,
+        )
