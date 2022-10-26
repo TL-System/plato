307d4acbc58c846d9bf6802a085cb5c10bd7f2bf
Chenghao Hu <ch.hu@mail.utoronto.ca>
2022-10-26 03:33:23 +0000

    Updated callbacks based on lastest API.

--- examples/maskcrypt/maskcrypt_callbacks.py
+++ examples/maskcrypt/maskcrypt_callbacks.py
@@ -10,22 +10,25 @@ class MaskCryptCallback(ClientCallback):
     processors.
     """
 
-    def on_inbound_process(self, client, inbound_processor):
+    def on_inbound_received(self, client, inbound_processor):
         current_round = client.current_round
         if current_round % 2 != 0:
             inbound_processor.processors.append(
-                decrypt_processor.Processor(client_id=client.client_id, name="model_decrypt")
+                decrypt_processor.Processor(
+                    client_id=client.client_id,
+                    trainer=client.trainer,
+                    name="model_decrypt",
+                )
             )
 
-    def on_outbound_process(self, client, outbound_processor):
+    def on_outbound_ready(self, client, report, outbound_processor):
         current_round = client.current_round
         if current_round % 2 == 0:
             outbound_processor.processors.append(
-                encrypt_processor.Processor(client_id=client.client_id, name="model_encrypt")
-            )
-        else:
-            outbound_processor.processors.append(
-                mask_processor.Processor(
-                    client=client, client_id=client.client_id, name="mask_generator"
+                encrypt_processor.Processor(
+                    mask=client.final_mask,
+                    client_id=client.client_id,
+                    trainer=client.trainer,
+                    name="model_encrypt",
                 )
             )
