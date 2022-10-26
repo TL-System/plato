2c6f471668c659a1de6a638b32dfbcefd483b622
Chenghao Hu <ch.hu@mail.utoronto.ca>
2022-10-26 16:16:42 +0000

    Updated docstrings.

--- examples/maskcrypt/maskcrypt.py
+++ examples/maskcrypt/maskcrypt.py
@@ -1,15 +1,15 @@
 """
-Customize the inbound and outbound processors through client callbacks.
+MaskCrypt: Federated Learning with Selective Homomorphic Encryption.
 """
-from maskcrypt_callbacks import MaskCryptCallback
-
 import maskcrypt_trainer
 import maskcrypt_client
 import maskcrypt_server
 
+from maskcrypt_callbacks import MaskCryptCallback
+
 
 def main():
-    """A Plato federated learning training session using CustomizeProcessorCallback."""
+    """A Plato federated learning training session using selective homomorphic encryption."""
     trainer = maskcrypt_trainer.Trainer
     client = maskcrypt_client.Client(trainer=trainer, callbacks=[MaskCryptCallback])
     server = maskcrypt_server.Server(trainer=trainer)
