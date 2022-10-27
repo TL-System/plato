d1467d2ced055dbe50d0e2e383b984755d862431
Yufei-Kang <1175801998@qq.com>
2022-10-27 14:41:10 -0400

    Update async_selection.py

--- examples/async_selection/async_selection.py
+++ examples/async_selection/async_selection.py
@@ -3,16 +3,17 @@ A federated learning training session with asynchronous client selection.
 """
 import os
 
-os.environ['config_file'] = './async_mnist_lenet5.yml'
+# os.environ['config_file'] = './async_mnist_lenet5.yml'
 
 import async_selection_server
-#import async_selection_client
+
+# import async_selection_client
 
 
 def main():
-    """ A Plato federated learning training session using the FedSarah algorithm. """
+    """A Plato federated learning training session using the FedSarah algorithm."""
     server = async_selection_server.Server()
-    #client = async_selection_client.Client()
+    # client = async_selection_client.Client()
 
     server.run()
 
