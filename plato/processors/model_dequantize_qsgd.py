e1dc3dec5783b7b1bd8a25e863937717f43cbf3c
kai <kai.shen@mail.utoronto.ca>
2022-11-07 15:31:27 -0500

    revised comments.

--- plato/processors/model_dequantize_qsgd.py
+++ plato/processors/model_dequantize_qsgd.py
@@ -1,15 +1,15 @@
 """
 Implements a Processor to decompress and dequantize upload models.
 
-In more detail, this processor first decompress each received parameter. Next,
-dequantize quantize each upload parameter under given quantization level.
-Hence, the 8-bit received parameter can be transfered to 32-bit parameter.
+In more detail, this processor first decompresses each received parameter.
+Next, dequantize each upload parameter under the given quantization level.
+Hence, 8-bit received parameters can be converted into 32-bit parameters.
 
 Reference:
 
 Alistarh, D., Grubic, D., Li, J., Tomioka, R., & Vojnovic, M. (2017).
 "QSGD: Communication-efficient SGD via gradient quantization and encoding."
-Advances in neural information processing systems, 30.
+Advances in neural information processing systems.
 
 https://proceedings.neurips.cc/paper/2017/file/6c340f25839e6acdc73414517203f5f0-Paper.pdf
 """
@@ -61,9 +61,9 @@ class Processor(model.Processor):
         return output
 
     def _process_layer(self, layer: Any) -> Any:
-        """Decompress and dequantize each individual layer of the model"""
+        """Decompress and dequantize each individual layer of the model."""
 
-        # Step 1: decompress the header
+        # Step 1: decompress the header.
         tuning_param = self.quantization_level - 1
         max_v = unpack("!f", layer[0:4])[0]
         numel = unpack("!I", layer[4:8])[0]
@@ -72,7 +72,7 @@ class Processor(model.Processor):
         for i in range(dimensions):
             size.append(unpack("!h", layer[10 + 2 * i : 12 + 2 * i])[0])
 
-        # Step 2: decompress the content
+        # Step 2: decompress the content.
         layer = layer[10 + 2 * dimensions :]
         zeta = []
         prefix = b"\x00\x00\x00"
@@ -83,7 +83,7 @@ class Processor(model.Processor):
             zeta.append(tmp)
         zeta = torch.tensor(zeta).reshape(size)
 
-        # Step 3: dequantize the content
+        # Step 3: dequantize the content.
         zeta = zeta * max_v / tuning_param
 
         return zeta
--- plato/processors/model_quantize_qsgd.py
+++ plato/processors/model_quantize_qsgd.py
@@ -1,15 +1,15 @@
 """
 Implements a Processor to quantize and compress upload models.
 
-In more detail, this processor first quantize each upload parameter under given
-quantization level. Next, compress and store each quantization value. Hence, the
-32-bit parameter can be transfered to 8-bit parameter.
+In more detail, this processor first quantizes each upload parameter under
+the given quantization level. Next, compress and store each quantized value.
+Hence, these 32-bit parameters can be converted into 8-bit parameters.
 
 Reference:
 
 Alistarh, D., Grubic, D., Li, J., Tomioka, R., & Vojnovic, M. (2017).
 "QSGD: Communication-efficient SGD via gradient quantization and encoding."
-Advances in neural information processing systems, 30.
+Advances in neural information processing systems.
 
 https://proceedings.neurips.cc/paper/2017/file/6c340f25839e6acdc73414517203f5f0-Paper.pdf
 """
@@ -60,7 +60,7 @@ class Processor(model.Processor):
         return output
 
     def _process_layer(self, layer: Any) -> Any:
-        """Quantize and compress each individual layer of the model"""
+        """Quantize and compress each individual layer of the model."""
 
         def add_prob(prob: Any) -> Any:
             """Add 1 to the corresponding positions with given probability."""
@@ -75,34 +75,34 @@ class Processor(model.Processor):
             return torch.reshape(prob, size)
 
         def handler(tensor: Any) -> Any:
-            """Handler function for the compression of quantized parameter"""
+            """Handler function for the compression of quantized values."""
             content = b""
             tensor = tensor.reshape(-1)
             for _, value in enumerate(tensor):
                 num = value.item()
                 if num < 0:
                     num = abs(num) ^ unpack("!i", b"\x00\x00\x00\x80")[0]
-                content += pack("!I", num)[3:4]  # present a parameter in 1 byte
+                content += pack("!I", num)[3:4]  # present each parameter in 1 byte.
             return content
 
-        # Step 1: quantization
-        tuning_param = self.quantization_level - 1  # tuning parameter
-        max_v = torch.max(abs(layer))  # max absolute value
+        # Step 1: quantization.
+        tuning_param = self.quantization_level - 1  # tuning parameter.
+        max_v = torch.max(abs(layer))  # max absolute value.
         neg = (-1) * layer.lt(0) + 1 * layer.ge(0)
-        ratio = abs(layer) / max_v  # |v_i| / ||v||
+        ratio = abs(layer) / max_v  # |v_i| / ||v||.
         level = (ratio * tuning_param - 1).ceil()
         zeta = level + add_prob(ratio * tuning_param - level)
         zeta = zeta.mul(neg).to(int)
 
-        # Step 2: handle head, including v_max, len() and their following dims
-        output = pack("!f", max_v.item())  # ! represents for big-endian
+        # Step 2: handle the header.
+        output = pack("!f", max_v.item())  # ! represents for big-endian.
         output += pack("!I", zeta.numel())
         dimensions = len(zeta.size())
         output += pack("!h", dimensions)
         for i in range(dimensions):
             output += pack("!h", zeta.size(i))
 
-        # Step 3: handle content, each includes 1 sign bit followed by 7 bits
+        # Step 3: handle the content, each consists of 1 sign bit followed by 7 bits.
         output += handler(zeta)
 
         return output
