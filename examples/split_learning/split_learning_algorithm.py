f5ba5a43f19989ff5b0994b273e1d06ed1748bd3
Chenghao Hu <ch.hu@mail.utoronto.ca>
2022-11-08 15:59:36 +0000

    Moved client training from algorithm to trainer.

--- examples/split_learning/split_learning_algorithm.py
+++ examples/split_learning/split_learning_algorithm.py
@@ -11,7 +11,6 @@ https://arxiv.org/pdf/1812.00564.pdf
 
 import logging
 import time
-from copy import deepcopy
 
 import torch
 
@@ -25,18 +24,7 @@ class Algorithm(fedavg.Algorithm):
     server.
     """
 
-    def __init__(self, trainer=None):
-        super().__init__(trainer)
-        self.gradients_list = []
-        self.input_dataset = []
-        self.data_loader = None
-
-
-    def receive_gradients(self, gradients):
-        """Receive gradients from the server."""
-        self.gradients_list = deepcopy(gradients)
-
-    def extract_features(self):
+    def extract_features(self, dataset, sampler):
         """Extracting features using layers before the cut_layer."""
         self.model.to(self.trainer.device)
         self.model.eval()
@@ -44,9 +32,10 @@ class Algorithm(fedavg.Algorithm):
         tic = time.perf_counter()
 
         features_dataset = []
-        self.input_dataset = []
 
-        inputs, targets = next(self.data_loader)
+        inputs, targets = self.trainer.get_train_samples(
+            Config().trainer.batch_size, dataset, sampler
+        )
         with torch.no_grad():
             inputs, targets = inputs.to(self.trainer.device), targets.to(
                 self.trainer.device
@@ -54,7 +43,6 @@ class Algorithm(fedavg.Algorithm):
             logits = self.model.forward_to(inputs)
 
         features_dataset.append((logits.detach().cpu(), targets.detach().cpu()))
-        self.input_dataset.append((inputs.detach().cpu(), targets.detach().cpu()))
 
         toc = time.perf_counter()
         logging.warning(
@@ -66,32 +54,16 @@ class Algorithm(fedavg.Algorithm):
 
         return features_dataset, toc - tic
 
-    def complete_train(self):
+    def complete_train(self, gradients):
         """Update the model on the client/device with the gradients received
         from the server.
         """
-        self.model.to(self.trainer.device)
-        self.model.train()
-
-        data_loader = self.input_dataset
-
         tic = time.perf_counter()
 
-        # Initializing the optimizer
-        optimizer = self.trainer.get_optimizer(self.model)
-
-        grad_index = 0
-
-        for __, (examples, labels) in enumerate(data_loader):
-            examples, labels = examples.to(self.trainer.device), labels.to(
-                self.trainer.device
-            )
-
-            optimizer.zero_grad()
-            outputs = self.model.forward_to(examples)
-            outputs.backward(self.gradients_list[grad_index].to(self.trainer.device))
-            grad_index = grad_index + 1
-            optimizer.step()
+        # Retrieve the training samples and let trainer do the training
+        samples, sampler = self.trainer.retrieve_train_samples()
+        self.trainer.load_gradients(gradients)
+        self.train(samples, sampler)
 
         toc = time.perf_counter()
         logging.warning(
@@ -103,19 +75,10 @@ class Algorithm(fedavg.Algorithm):
         return toc - tic
 
     def train(self, trainset, sampler):
-        """Train the neural network model after the cut layer."""
+        """General training method that trains model with provided trainset and sampler."""
         self.trainer.train(
             feature_dataset.FeatureDataset(trainset.feature_dataset), sampler
         )
-
-    def load_data(self, dataset, sampler):
-        """Setting up the data loader."""
-        data_loader = torch.utils.data.DataLoader(
-            dataset,
-            batch_size=Config().trainer.batch_size,
-            sampler=sampler.get(),
-        )
-        self.data_loader = iter(data_loader)
 
     def update_weights_before_cut(self, weights):
         # Update the weights before cut layer, called when testing accuracy
