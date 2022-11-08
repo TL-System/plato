0ae4d54c8b098aa183763c5c90b4f861e304663d
Chenghao Hu <ch.hu@mail.utoronto.ca>
2022-11-08 16:00:06 +0000

    Refactored the trainer to support both client and server side training.

--- examples/split_learning/split_learning_trainer.py
+++ examples/split_learning/split_learning_trainer.py
@@ -8,6 +8,7 @@ raw patient data," in Proc. AI for Social Good Workshop, affiliated with ICLR 20
 
 https://arxiv.org/pdf/1812.00564.pdf
 """
+from itertools import cycle
 import logging
 import os
 
@@ -15,6 +16,8 @@ import torch
 from plato.config import Config
 
 from plato.trainers import basic
+from plato.datasources import feature
+from plato.samplers import all_inclusive
 
 
 class Trainer(basic.Trainer):
@@ -28,6 +31,15 @@ class Trainer(basic.Trainer):
         callbacks: The callbacks that this trainer uses.
         """
         super().__init__(model=model, callbacks=callbacks)
+        self.last_client_id = None
+        self.last_optimizer = None
+
+        # Client side variables
+        self.training_samples = None
+        self.gradients = None
+        self.data_loader = None
+
+        # Server side variables
         self.cut_layer_grad = []
 
     def get_train_loader(self, batch_size, trainset, sampler, **kwargs):
@@ -51,6 +63,67 @@ class Trainer(basic.Trainer):
 
         Returns: loss values after the current batch has been processed.
         """
+        if self.client_id == 0:
+            return self.server_train_loop(config, examples, labels)
+        else:
+            return self.client_train_loop(examples)
+
+    def train_run_end(self, config):
+        """Aditional tasks after training."""
+        if self.client_id == 0:
+            # Server needs to save gradients, clients not
+            self.save_gradients(config)
+
+    def get_optimizer(self, model):
+        """Return the optimizer used last round to avoid reconfiguration."""
+        if self.last_optimizer is None or self.last_client_id != self.client_id:
+            self.last_optimizer = super().get_optimizer(model)
+            self.last_client_id = self.client_id
+
+        return self.last_optimizer
+
+    """Client side functions"""
+
+    def get_train_samples(self, batch_size, trainset, sampler):
+        """Get a batch of training samples to extract feature,
+        trainer has to save these samples to complete training later.
+        """
+        data_loader = torch.utils.data.DataLoader(
+            dataset=trainset, shuffle=False, batch_size=batch_size, sampler=sampler
+        )
+        data_loader = iter(data_loader)
+        self.training_samples = next(data_loader)
+        return self.training_samples
+
+    def retrieve_train_samples(self):
+        """Retrieve the training samples to complete client training."""
+        # Wrap the training samples with datasource and sampler to be fed into Plato trainer
+        samples = feature.DataSource([[self.training_samples]])
+        sampler = all_inclusive.Sampler(samples)
+        return samples, sampler
+
+    def load_gradients(self, gradients):
+        """Load the gradients which will be used to complete client training."""
+        self.gradients = gradients
+
+    def client_train_loop(self, examples):
+        """Complete the client side training with gradients from server."""
+        self.optimizer.zero_grad()
+        outputs = self.model.forward_to(examples)
+
+        # Back propagate with gradients from server
+        outputs.backward(self.gradients)
+        self.optimizer.step()
+
+        # No loss value on the client side
+        loss = torch.zeros(1)
+        self._loss_tracker.update(loss, examples.size(0))
+        return loss
+
+    """Server side functions"""
+
+    def server_train_loop(self, config, examples, labels):
+
         examples = examples.detach().requires_grad_(True)
 
         loss = super().perform_forward_and_backward_passes(config, examples, labels)
@@ -60,12 +133,12 @@ class Trainer(basic.Trainer):
             loss,
         )
         # Record gradients within the cut layer
-        self.cut_layer_grad.append(examples.grad.clone().detach())
+        self.cut_layer_grad = [examples.grad.clone().detach()]
 
         return loss
 
-    def train_run_end(self, config):
-        """Saving recorded gradients to a file."""
+    def save_gradients(self, config):
+        """Server saves recorded gradients to a file."""
         model_name = config["model_name"]
         model_path = Config().params["model_path"]
 
@@ -78,3 +151,15 @@ class Trainer(basic.Trainer):
         logging.info(
             "[Server #%d] Gradients saved to %s.", os.getpid(), model_gradients_path
         )
+
+    def get_gradients(self):
+        """Read gradients from a file."""
+        model_path = Config().params["model_path"]
+        model_name = Config().trainer.model_name
+
+        model_gradients_path = f"{model_path}/{model_name}_gradients.pth"
+        logging.info(
+            "[Server #%d] Loading gradients from %s.", os.getpid(), model_gradients_path
+        )
+
+        return torch.load(model_gradients_path)
