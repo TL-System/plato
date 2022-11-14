3a009b362af441cd8df52ed3e0cf7decf064838b
Dixi Yao <pspo-qfl@163.com>
2022-11-14 16:11:09 -0500

    added two vits: t2tvit and deepvit

--- plato/models/vit.py
+++ plato/models/vit.py
@@ -2,9 +2,12 @@
 Obtaining a ViT model for image classification from HuggingFace.
 """
 
+import torch
 from torch import nn
-from transformers import AutoModelForImageClassification, AutoConfig
+from transformers import AutoConfig, AutoModelForImageClassification
+
 from plato.config import Config
+from plato.models import dvit, t2tvit
 
 
 class ResolutionAdjustedModel(nn.Module):
@@ -40,6 +43,68 @@ class ResolutionAdjustedModel(nn.Module):
         return outputs.logits
 
 
+class T2TVIT(nn.Module):
+    """Wrap up t2t-vit."""
+
+    def __init__(self, name) -> nn.Module:
+        super().__init__()
+
+        model_name = getattr(t2tvit.models, name)
+        t2t = model_name(num_classes=Config().trainer.num_classes)
+
+        if (
+            hasattr(Config().parameters, "model")
+            and hasattr(Config().parameters.model, "pretrained")
+            and Config().parameters.model.pretrained
+        ):
+            t2tvit.utils.load_for_transfer_learning(
+                t2t,
+                Config().parameters.model.pretrain_path,
+                use_ema=True,
+                strict=False,
+                num_classes=Config().trainer.num_classes,
+            )
+        self.model = t2t
+        self.resolution = 224
+
+    def forward(self, x):
+        """forward function"""
+        if x.size(-1) != self.resolution:
+            x = nn.functional.interpolate(x, size=self.resolution, mode="bicubic")
+        return self.model(x)
+
+
+class DeepViT(nn.Module):
+    """Wrap up deep vit."""
+
+    def __init__(self, name) -> nn.Module:
+        super().__init__()
+
+        model_name = getattr(dvit.models.deep_vision_transformer, name)
+        deepvit = model_name(
+            pretrained=False, num_classes=Config.trainer.num_classes, in_chans=3
+        )
+        if (
+            hasattr(Config().parameters, "model")
+            and hasattr(Config().parameters.model, "pretrained")
+            and Config().parameters.model.pretrained
+        ):
+            state_dict = torch.load(
+                Config().parameters.model.pretrain_path, map_location="cpu"
+            )
+            del state_dict["head.weight"]
+            del state_dict["head.bias"]
+            deepvit.load_state_dict(state_dict)
+        self.model = deepvit
+        self.resolution = 224
+
+    def forward(self, x):
+        """forward function"""
+        if x.size(-1) != self.resolution:
+            x = nn.functional.interpolate(x, size=self.resolution, mode="bicubic")
+        return self.model(x)
+
+
 class Model:
     """
     The Transformer and other models loaded from HuggingFace.
@@ -50,6 +115,11 @@ class Model:
     @staticmethod
     def get(model_name=None, **kwargs):  # pylint: disable=unused-argument
         """Returns a named model from HuggingFace."""
+        if "t2t" in model_name:
+            return T2TVIT(model_name)
+        if "deepvit" in model_name:
+            return DeepViT(model_name)
+
         config_kwargs = {
             "cache_dir": None,
             "revision": "main",
