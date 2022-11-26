70d3b7fd36fe3b923b6d8f1baa7fbbf8790dfe07
CSJdeveloper <CSJDeveloper@users.noreply.github.com>
2022-11-26 12:27:47 -0500

    Added the unit test for the encoder

--- tests/model_tests.py
+++ tests/model_tests.py
@@ -14,14 +14,16 @@ class ModelsTest(unittest.TestCase):
         super().setUp()
         __ = Config()
 
+        # 1. define the main model to be the encoder
         self.model = models_registry.get()
         self.optimizer = optimizers.get(self.model)
 
+        # 2. define the personalized model to be normal resnet
         self.personalized_model = models_registry.get(
+            model_type=Config().trainer.personalized_model_type,
             model_name=Config().trainer.personalized_model_name,
             model_params=Config().parameters.personalized_model._asdict(),
         )
-        # 2. optimizer
         self.personalized_optimizer = optimizers.get(
             self.personalized_model,
             optimizer_name=Config().trainer.personalized_optimizer,
@@ -29,25 +31,16 @@ class ModelsTest(unittest.TestCase):
         )
 
     def test_model_config(self):
-        # test the defined models
+        # test the defined models based on the
+        # hyper-parameters
 
-        print(self.model)
-        print(self.personalized_model)
-        # # test whether loading different hyper-parameters:
-        # # 1. for the model
-        # self.assertNotEqual(self.model.__str__(), personalized_model.__str__())
-        # self.assertNotEqual(
-        #     self.model.fc5.out_features, personalized_model.linear.out_features
-        # )
-        # # 2. for the optimizer.
-        # self.assertNotEqual(
-        #     self.optimizer.param_groups[0]["lr"],
-        #     personalized_optimizer.param_groups[0]["lr"],
-        # )
-        # self.assertNotEqual(
-        #     self.optimizer.param_groups[0]["weight_decay"],
-        #     personalized_optimizer.param_groups[0]["weight_decay"],
-        # )
+        # 1. whether the defined model is the encoder
+        self.assertEqual(self.model.encoding_dim, 120)
+
+        # 2. test whether the defined personalized model is
+        # a normal resnet with output's dimension 'num_classes'
+        self.assertEqual(self.personalized_model.linear.out_features, 20)
+
 
 
 if __name__ == "__main__":
