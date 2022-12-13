7c7d0e284b34167c634c2b853331c383d27598a7
CSJdeveloper <CSJDeveloper@users.noreply.github.com>
2022-12-13 12:38:58 -0500

    Adjusted the unit test 'fedavg_tests' to be compatible with latest Plato

--- tests/fedavg_tests.py
+++ tests/fedavg_tests.py
@@ -3,6 +3,7 @@ import copy
 import unittest
 import numpy as np
 import torch
+from types import SimpleNamespace
 
 from plato.clients import simple
 from plato.algorithms import fedavg as fedavg_alg
@@ -11,7 +12,7 @@ from plato.trainers import basic
 
 
 class InnerProductModel(torch.nn.Module):
-    def __init__(self, n):
+    def __init__(self, n: int = 10):
         super().__init__()
         self.layer = torch.nn.Linear(n, 1, bias=False)
         self.layer.weight.data = torch.arange(n, dtype=torch.float32)
@@ -37,7 +38,7 @@ async def test_fedavg_aggregation(self):
 
     print("\nTesting federated averaging.")
     updates = []
-    model = copy.deepcopy(self.model)
+    model = InnerProductModel
 
     trainer = basic.Trainer
     algorithm = fedavg_alg.Algorithm
@@ -46,83 +47,160 @@ async def test_fedavg_aggregation(self):
 
     weights = copy.deepcopy(self.algorithm.extract_weights())
     print(f"Report 1 weights: {weights}")
-    updates.append((0, simple.Report(1, 100, 0, 0, 0), weights, 0))
+    updates.append(
+        SimpleNamespace(
+            client_id=1,
+            report=SimpleNamespace(
+                client_id=1,
+                num_samples=100,
+                accuracy=0,
+                training_time=0,
+                comm_time=0,
+                update_response=False,
+            ),
+            payload=weights,
+            staleness=0,
+        )
+    )
 
-    self.model.train()
+    self.trainer.model.train()
 
     self.optimizer.zero_grad()
-    self.model.loss_criterion(self.model(self.example), self.label).backward()
+    self.trainer.model.loss_criterion(
+        self.trainer.model(self.example), self.label
+    ).backward()
     self.optimizer.step()
-    self.assertEqual(44.0, self.model(self.example).item())
+    self.assertEqual(44.0, self.trainer.model(self.example).item())
     weights = copy.deepcopy(self.algorithm.extract_weights())
     print(f"Report 2 weights: {weights}")
-    updates.append((0, simple.Report(1, 100, 0, 0, 0), weights, 0))
+    updates.append(
+        SimpleNamespace(
+            client_id=2,
+            report=SimpleNamespace(
+                client_id=2,
+                num_samples=100,
+                accuracy=0,
+                training_time=0,
+                comm_time=0,
+                update_response=False,
+            ),
+            payload=weights,
+            staleness=0,
+        )
+    )
 
     self.optimizer.zero_grad()
-    self.model.loss_criterion(self.model(self.example), self.label).backward()
+    self.trainer.model.loss_criterion(
+        self.trainer.model(self.example), self.label
+    ).backward()
     self.optimizer.step()
-    self.assertEqual(43.2, np.round(self.model(self.example).item(), 4))
+    self.assertEqual(43.2, np.round(self.trainer.model(self.example).item(), 4))
     weights = copy.deepcopy(self.algorithm.extract_weights())
     print(f"Report 3 Weights: {weights}")
-    updates.append((0, simple.Report(1, 100, 0, 0, 0), weights, 0))
+    updates.append(
+        SimpleNamespace(
+            client_id=3,
+            report=SimpleNamespace(
+                client_id=3,
+                num_samples=100,
+                accuracy=0,
+                training_time=0,
+                comm_time=0,
+                update_response=False,
+            ),
+            payload=weights,
+            staleness=0,
+        )
+    )
 
     self.optimizer.zero_grad()
-    self.model.loss_criterion(self.model(self.example), self.label).backward()
+    self.trainer.model.loss_criterion(
+        self.trainer.model(self.example), self.label
+    ).backward()
     self.optimizer.step()
-    self.assertEqual(42.56, np.round(self.model(self.example).item(), 4))
+    self.assertEqual(42.56, np.round(self.trainer.model(self.example).item(), 4))
     weights = copy.deepcopy(self.algorithm.extract_weights())
     print(f"Report 4 Weights: {weights}")
-    updates.append((0, simple.Report(1, 100, 0, 0, 0), weights, 0))
+    updates.append(
+        SimpleNamespace(
+            client_id=4,
+            report=SimpleNamespace(
+                client_id=4,
+                num_samples=100,
+                accuracy=0,
+                training_time=0,
+                comm_time=0,
+                update_response=False,
+            ),
+            payload=weights,
+            staleness=0,
+        )
+    )
 
-    print(f"Weights before federated averaging: {server.model.layer.weight.data}")
+    print(
+        f"Weights before federated averaging: {server.trainer.model.layer.weight.data}"
+    )
+    weights_received = [update.payload for update in updates]
+    baseline_weights = server.algorithm.extract_weights()
+    deltas_received = server.algorithm.compute_weight_deltas(
+        baseline_weights, weights_received
+    )
+    deltas = await server.aggregate_deltas(updates, deltas_received)
 
-    deltas = await server.aggregate_deltas(updates)
     updated_weights = server.algorithm.update_weights(deltas)
-    server.algorithm.load_weights(updated_weights)
 
-    print(f"Weights after federated averaging: {server.model.layer.weight.data}")
-    self.assertEqual(42.56, np.round(self.model(self.example).item(), 4))
+    server.algorithm.load_weights(updated_weights)
+    print(
+        f"Weights after federated averaging: {server.trainer.model.layer.weight.data}"
+    )
+    self.assertEqual(42.56, np.round(self.trainer.model(self.example).item(), 4))
 
 
 class FedAvgTest(unittest.TestCase):
     def setUp(self):
         super().setUp()
-        self.model = InnerProductModel(10)
+        self.model = InnerProductModel
         self.example = torch.ones(1, 10)
         self.label = torch.ones(1) * 40.0
         self.trainer = basic.Trainer(model=self.model)
         self.algorithm = fedavg_alg.Algorithm(trainer=self.trainer)
-        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=0.01)
+        self.optimizer = torch.optim.SGD(self.trainer.model.parameters(), lr=0.01)
 
     def test_forward(self):
         self.assertIsNotNone(self.model)
         weights = self.algorithm.extract_weights()
         print("\nTesting forward pass.")
         print(f"Weights: {weights}")
-        self.assertEqual(45.0, self.model(self.example).item())
+        self.assertEqual(45.0, self.trainer.model(self.example).item())
 
     def test_backward(self):
         print("\nTesting backward pass.")
-        self.model.train()
+        self.trainer.model.train()
 
         self.optimizer.zero_grad()
-        self.model.loss_criterion(self.model(self.example), self.label).backward()
+        self.trainer.model.loss_criterion(
+            self.trainer.model(self.example), self.label
+        ).backward()
         self.optimizer.step()
-        self.assertEqual(44.0, self.model(self.example).item())
+        self.assertEqual(44.0, self.trainer.model(self.example).item())
         weights = self.algorithm.extract_weights()
         print(f"Weights: {weights}")
 
         self.optimizer.zero_grad()
-        self.model.loss_criterion(self.model(self.example), self.label).backward()
+        self.trainer.model.loss_criterion(
+            self.trainer.model(self.example), self.label
+        ).backward()
         self.optimizer.step()
-        self.assertEqual(43.2, np.round(self.model(self.example).item(), 4))
+        self.assertEqual(43.2, np.round(self.trainer.model(self.example).item(), 4))
         weights = self.algorithm.extract_weights()
         print(f"Weights: {weights}")
 
         self.optimizer.zero_grad()
-        self.model.loss_criterion(self.model(self.example), self.label).backward()
+        self.trainer.model.loss_criterion(
+            self.trainer.model(self.example), self.label
+        ).backward()
         self.optimizer.step()
-        self.assertEqual(42.56, np.round(self.model(self.example).item(), 4))
+        self.assertEqual(42.56, np.round(self.trainer.model(self.example).item(), 4))
         weights = self.algorithm.extract_weights()
         print(f"Weights: {weights}")
 
