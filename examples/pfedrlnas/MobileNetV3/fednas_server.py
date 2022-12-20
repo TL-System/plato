9dc45d2434a4163416420716640c2ac9ba61dba2
daxixi <pspo-qfl@163.com>
2022-12-20 09:08:54 -0500

    removed flops

--- examples/pfedrlnas/MobileNetV3/fednas_server.py
+++ examples/pfedrlnas/MobileNetV3/fednas_server.py
@@ -78,21 +78,21 @@ class Server(fedavg.Server):
         self.process_end = time.time()
 
     def server_will_close(self):
-        flops = []
+        #flops = []
         for i in range(1, Config().clients.total_clients):
             cfg = self.subnets_config[i]
             if cfg:
                 logging.info("the config of client %s is %s", str(i), str(cfg))
-                self.algorithm.set_active_subnet(cfg)
-                flops.append(self.algorithm.model.model.compute_active_subnet_flops())
-        logging.info(
-            fonts.colourize(
-                f"[{self}] Average Flops of models is {np.mean(np.array(flops))}."
-            )
-        )
+                #self.algorithm.set_active_subnet(cfg)
+                #flops.append(self.algorithm.model.model.compute_active_subnet_flops())
+        #logging.info(
+        #    fonts.colourize(
+        #        f"[{self}] Average Flops of models is {np.mean(np.array(flops))}."
+        #    )
+        #)
         save_config = f"{Config().server.model_path}/subnet_configs.pickle"
         with open(save_config, "wb") as file:
-            pickle.dump((self.subnets_config, flops), file)
+            pickle.dump(self.subnets_config, file)
 
     def save_to_checkpoint(self) -> None:
         save_config = f"{Config().server.model_path}/subnet_configs.pickle"
