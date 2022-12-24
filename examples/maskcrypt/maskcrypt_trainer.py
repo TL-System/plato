a30b15bf9b1e5fc627fa96a5c43a4dbf24cd85ae
Chenghao Hu <ch.hu@mail.utoronto.ca>
2022-12-24 23:03:58 +0000

    Refactored masckrypt trainer to compute gradient in callbacks.

--- examples/maskcrypt/maskcrypt_trainer.py
+++ examples/maskcrypt/maskcrypt_trainer.py
@@ -1,81 +1,81 @@
-"""The customized trainer designed for MaskCrpyt."""
+"""Customize the default trainer with callbacks to support MaskCrpyt."""
 import logging
 import os
 import torch
-
-from typing import OrderedDict
+import maskcrypt_utils
 from plato.config import Config
-from plato.trainers import basic
+from plato.callbacks.trainer import TrainerCallback as pTrainerCallback
+from transformers import TrainerCallback as hTrainerCallback
 
 
-class Trainer(basic.Trainer):
-    """The trainer with gradient computation when local training is finished."""
+class basicTrainerCallback(pTrainerCallback):
+    """Trainer callback for the basic trainer in Plato."""
 
-    def __init__(self, model=None):
-        super().__init__(model=model)
-        self.gradient = OrderedDict()
-
-    def train_run_end(self, config):
+    def on_train_run_end(self, trainer, config):
         """Compute gradients on local data when training is finished."""
         logging.info(
-            "[Client #%d] Training completed, computing gradient.", self.client_id
+            "[Client #%d] Training completed, computing gradient.", trainer.client_id
         )
         # Set the existing gradients to zeros
-        [x.grad.zero_() for x in list(self.model.parameters())]
-        self.model.to(self.device)
-        for idx, (examples, labels) in enumerate(self.train_loader):
-            examples, labels = examples.to(self.device), labels.to(self.device)
-            outputs = self.model(examples)
+        gradients = {}
+        [x.grad.zero_() for x in list(trainer.model.parameters())]
+        trainer.model.to(trainer.device)
+        for idx, (examples, labels) in enumerate(trainer.train_loader):
+            examples, labels = examples.to(trainer.device), labels.to(trainer.device)
+            outputs = trainer.model(examples)
             loss_criterion = torch.nn.CrossEntropyLoss()
             loss = loss_criterion(outputs, labels)
-            loss = loss * (len(labels) / len(self.sampler))
+            loss = loss * (len(labels) / len(trainer.sampler))
             loss.backward()
 
-        param_dict = dict(list(self.model.named_parameters()))
-        state_dict = self.model.state_dict()
+        param_dict = dict(list(trainer.model.named_parameters()))
+        state_dict = trainer.model.state_dict()
         for name in state_dict.keys():
             if name in param_dict:
-                self.gradient[name] = param_dict[name].grad
+                gradients[name] = param_dict[name].grad
             else:
-                self.gradient[name] = torch.zeros(state_dict[name].shape)
+                gradients[name] = torch.zeros(state_dict[name].shape)
 
-        model_type = config["model_name"]
-        filename = f"{model_type}_gradient_{self.client_id}_{config['run_id']}.pth"
-        self._save_gradient(filename)
+        maskcrypt_utils.save_gradients(
+            gradients=gradients, ppid=os.getppid(), config=Config()
+        )
 
-    def get_gradient(self):
-        """Read gradients from file and return to client."""
-        model_type = Config().trainer.model_name
-        run_id = Config().params["run_id"]
-        filename = f"{model_type}_gradient_{self.client_id}_{run_id}.pth"
-        return self._load_gradient(filename)
 
-    def _save_gradient(self, filename=None, location=None):
-        """Saving the gradients to a file."""
-        model_path = Config().params["model_path"] if location is None else location
-        model_name = Config().trainer.model_name
+class huggingfaceTrainerCallback(hTrainerCallback):
+    """Trainer callback for the huggingface trainer in Plato."""
 
-        try:
-            if not os.path.exists(model_path):
-                os.makedirs(model_path)
-        except FileExistsError:
-            pass
+    def on_train_end(self, args, state, control, **kwargs):
+        """Compute gradients on local data when training is finished."""
+        logging.info(
+            "[Client #%d] Training completed, computing gradient.", trainer.client_id
+        )
+        model = kwargs["model"]
+        train_loader = kwargs["train_dataloader"]
 
-        if filename is not None:
-            model_path = f"{model_path}/{filename}"
-        else:
-            model_path = f"{model_path}/{model_name}.pth"
+        [x.grad.zero_() for x in list(model.parameters())]
+        for _, samples in enumerate(train_loader):
+            samples = {k: v.to(model.device) for k, v in samples.items()}
+            loss = model(**samples).loss
+            loss = loss * (len(samples) / len(train_loader.sampler))
+            loss.backward()
 
-        torch.save(self.gradient, model_path)
+        gradients = {}
+        param_dict = dict(list(model.named_parameters()))
+        state_dict = model.state_dict()
+        for name in state_dict.keys():
+            if name in param_dict:
+                gradients[name] = param_dict[name].grad
+            else:
+                gradients[name] = torch.zeros(state_dict[name].shape)
 
-    def _load_gradient(self, filename=None, location=None):
-        """Load gradients from a file."""
-        model_path = Config().params["model_path"] if location is None else location
-        model_name = Config().trainer.model_name
+        maskcrypt_utils.save_gradients(
+            gradients=gradients, config=Config(), ppid=os.getppid()
+        )
 
-        if filename is not None:
-            model_path = f"{model_path}/{filename}"
-        else:
-            model_path = f"{model_path}/{model_name}.pth"
 
-        return torch.load(model_path)
+def get():
+    """Return a callback for Plato trainers to compute gradients when training is done."""
+    if Config().trainer.type == "HuggingFace":
+        return [huggingfaceTrainerCallback]
+    else:
+        return [basicTrainerCallback]
