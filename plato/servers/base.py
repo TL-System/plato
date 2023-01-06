TL-System/plato

commit 392c3cb25e7ea4253f7aa7a2c6e801c78c3615db

Fixed memory overflow issues during training where Heapq does not pop under some conditions. (#287)

plato/servers/base.py
@@ -895,7 +895,12 @@ class Server:
             },
         )
 
-        heapq.heappush(self.reported_clients, client_info)
+        if (
+            self.asynchronous_mode
+            and self.simulate_wall_time
+            and len(self.current_reported_clients) >= len(self.selected_clients)
+        ):
+            heapq.heappush(self.reported_clients, client_info)
         self.current_reported_clients[client_info[2]["client_id"]] = True
         del self.training_clients[client_id]
 

commit 9779f01e5a660f2b26389454aab8438d2df1154b

Fixed an issue in server/base.py, introduced by PR #287.

plato/servers/base.py
@@ -898,7 +898,6 @@ class Server:
         if (
             self.asynchronous_mode
             and self.simulate_wall_time
-            and len(self.current_reported_clients) >= len(self.selected_clients)
         ):
             heapq.heappush(self.reported_clients, client_info)
         self.current_reported_clients[client_info[2]["client_id"]] = True