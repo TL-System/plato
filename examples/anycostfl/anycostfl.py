692ec2966841e0f4a001e83c75f039cab67e28c7
Dixi Yao <pspo-qfl@163.com>
2023-02-07 09:01:40 -0500

    uploaded anycost fl algorithm.

---
+++ examples/anycostfl/anycostfl.py
@@ -0,0 +1,25 @@
+"""
+A federated learning training session using AnyCostFL
+
+Peichun Li, Guoliang Cheng, Xumin Huang, Jiawen Kang, Rong Yu, Yuan Wu, Miao Pan
+“AnycostFL: Efficient On-Demand Federated Learning over Heterogeneous Edge Device,”
+in InfoCom 2023.
+
+"""
+
+from mobilenetv3 import MobileNetV3
+
+from anycostfl_client import Client
+from anycostfl_server import Server
+from anycostfl_algorithm import Algorithm
+
+def main():
+    """A Plato federated learning training session using the AnyCostFL algorithm."""
+    model = MobileNetV3
+    server = Server(model=model, algorithm=Algorithm)
+    client = Client(model=model)
+    server.run(client)
+
+
+if __name__ == "__main__":
+    main()
