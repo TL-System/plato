6f66362a2fc315e9d91c9edd1f24dd54a13420a6
Baochun Li <bli@ece.toronto.edu>
2023-03-29 17:37:12 -0400

    Converted T2T-ViT, dvit, and NASViT as git submodules.

--- plato/models/dvit/run.sh
+++
@@ -1,1 +0,0 @@
-./distributed_train.sh 1 ../dataset_lmdb -c scripts/DeepViT_S_CNN_patch.yaml 
--- plato/models/t2tvit/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) [2012]-[2021] Shanghai Yitu Technology Co., Ltd.
-#
-# This source code is licensed under the Clear BSD License
-# LICENSE file in the root directory of this file
-# All rights reserved.
-from .t2t_vit import *
-from .t2t_vit_se import *
-from .t2t_vit_dense import *
-from .t2t_vit_ghost import *
