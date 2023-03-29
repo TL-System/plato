e8b2272a2f959322186febc83a56cb00fc22136c
Sijia Chen <CSJDeveloper@users.noreply.github.com>
2023-03-29 12:05:00 -0400

    Added the unit test

--- tests/client_tests.py
+++ tests/client_tests.py
@@ -2,14 +2,14 @@
 Testing a federated learning client.
 """
 import asyncio
-from plato.clients import simple
+from plato.clients import registry as client_registry
 
 
 async def test_training(client):
-    """ Testing the training loop on the client. """
+    """Testing the training loop on the client."""
     print("Testing training on the client.")
 
-    report, weights = await client.train()
+    report, weights = await client._train()
 
     print("Client model trained.")
     print(f"Report to be sent to the server: {report}")
@@ -17,11 +17,12 @@ async def test_training(client):
 
 
 def main():
-    """ Starting a simple client. """
-    client = simple.Client()
+    """Starting a simple client."""
+    client = client_registry.get()
     client.client_id = 1
+    client._load_data()
     client.configure()
-    client.load_data()
+    client._allocate_data()
     asyncio.run(test_training(client))
 
 
