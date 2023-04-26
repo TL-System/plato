ff01ee48feb0884e5c63befa6e57b1f68b7e0a08
Yufei-Kang <1175801998@qq.com>
2023-04-25 20:38:15 -0400

    Update detector_server.py

--- examples/detector/detector_server.py
+++ examples/detector/detector_server.py
@@ -1,1 +1,26 @@
-import 
+"""
+A customized server with detector so that poisoned model updates can be filtered out.
+"""
+
+import logging
+
+from plato.config import Config
+from plato.servers import fedavg
+from collections import OrderedDict
+
+import numpy as np
+import torch
+
+class Server(fedavg.Server):
+
+    def __init__(
+        self, model=None, datasource=None, algorithm=None, trainer=None, callbacks=None
+    ):
+        super().__init__(
+            model=model,
+            datasource=datasource,
+            algorithm=algorithm,
+            trainer=trainer,
+            callbacks=callbacks,
+        )
+    def configure():
