8f476da18a70d0b1c313d1d3a5f5b83ad4f1230b
Yufei-Kang <1175801998@qq.com>
2023-04-25 22:00:42 -0400

    Update detector_client.py

--- examples/detector/detector_client.py
+++ examples/detector/detector_client.py
@@ -1,1 +1,34 @@
-i
+"""
+A federated learning client that is capable to perform model poisoning attacks.
+"""
+
+import logging
+import os
+import pickle
+
+from plato.clients import simple
+from plato.config import Config
+from plato.utils import fonts
+
+import attacks
+
+
+class Client(simple.Client):
+    """A client who is able to perform model poisoning attack"""
+
+    def __init__(
+        self,
+        model=None,
+        datasource=None,
+        algorithm=None,
+        trainer=None,
+        callbacks=None,
+        trainer_callbacks=None,
+    ):
+        super().__init__(
+            model, datasource, algorithm, trainer, callbacks, trainer_callbacks
+        )
+
+    def configure(self) -> None:
+        """Initialize the attack related parameter"""
+        return super().configure()
