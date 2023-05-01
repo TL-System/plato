e8da7fc173fbb15d5015261031af229ff3c8768c
Sijia Chen <CSJDeveloper@users.noreply.github.com>
2023-04-30 23:39:53 -0400

    Created a detailed docs for pFL with SSL

--- examples/pflSSL/byol/byol.py
+++ examples/pflSSL/byol/byol.py
@@ -1,15 +1,11 @@
 """
 The implementation for the BYOL [1] method.
 
-The official code: https://github.com/lucidrains/byol-pytorch
-
-The third-party code: https://github.com/sthalles/PyTorch-BYOL
-
-Reference:
-
-[1]. https://arxiv.org/pdf/2006.07733.pdf
+[1]. Jean-Bastien Grill, et.al, Bootstrap Your Own Latent A New Approach to Self-Supervised Learning.
+https://arxiv.org/pdf/2006.07733.pdf.
 
 Source code: https://github.com/lucidrains/byol-pytorch
+The third-party code: https://github.com/sthalles/PyTorch-BYOL
 """
 import copy
 
