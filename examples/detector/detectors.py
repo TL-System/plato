ebfd3fc4d4c51cad43a5048621b93644c933db56
Yufei-Kang <1175801998@qq.com>
2023-05-17 17:42:26 -0400

    Added function to approximate integrated Hessian value.
    This value is used to make prediction. Richard H Byrd, Jorge Nocedal, and Robert B Schnabel. 1994. Representations of quasi-Newton matrices and their use in limited memory methods. Mathematical Programming (1994)

--- examples/detector/detectors.py
+++ examples/detector/detectors.py
@@ -2,6 +2,11 @@ import torch
 import logging
 from plato.config import Config
 from scipy.stats import norm
+<<<<<<< Updated upstream
+=======
+import numpy as np
+from mxnet import nd
+>>>>>>> Stashed changes
 
 
 def get():
@@ -22,6 +27,29 @@ def get():
 
     raise ValueError(f"No such defence: {detector_type}")
 
+<<<<<<< Updated upstream
+=======
+def lbfgs(args, S_k_list, Y_k_list, v):
+    # Approximate integrated Hessian value based on weights
+    curr_S_k = nd.concat(*S_k_list, dim=1)
+    curr_Y_k = nd.concat(*Y_k_list, dim=1)
+    S_k_time_Y_k = nd.dot(curr_S_k.T, curr_Y_k)
+    S_k_time_S_k = nd.dot(curr_S_k.T, curr_S_k)
+    R_k = np.triu(S_k_time_Y_k.asnumpy())
+    L_k = S_k_time_Y_k - nd.array(R_k, ctx=mx.gpu(args.gpu))
+    sigma_k = nd.dot(Y_k_list[-1].T, S_k_list[-1]) / (nd.dot(S_k_list[-1].T, S_k_list[-1]))
+    D_k_diag = nd.diag(S_k_time_Y_k)
+    upper_mat = nd.concat(*[sigma_k * S_k_time_S_k, L_k], dim=1)
+    lower_mat = nd.concat(*[L_k.T, -nd.diag(D_k_diag)], dim=1)
+    mat = nd.concat(*[upper_mat, lower_mat], dim=0)
+    mat_inv = nd.linalg.inverse(mat)
+
+    approx_prod = sigma_k * v
+    p_mat = nd.concat(*[nd.dot(curr_S_k.T, sigma_k * v), nd.dot(curr_Y_k.T, v)], dim=0)
+    approx_prod -= nd.dot(nd.dot(nd.concat(*[sigma_k * curr_S_k, curr_Y_k], dim=1), mat_inv), p_mat)
+
+    return approx_prod
+>>>>>>> Stashed changes
 
 def fl_detector(weights_attacked):
     """https://arxiv.org/pdf/2207.09209.pdf"""
