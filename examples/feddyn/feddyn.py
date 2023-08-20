a57eb3131254e408ea7fd40124c60c85ae9876a1
Dixi Yao <pspo-qfl@163.com>
2023-08-20 15:20:52 +0800

    Added FedDyn as one of the examples. (#346)

---
+++ examples/feddyn/feddyn.py
@@ -0,0 +1,26 @@
+"""
+An implementation of the FedDyn algorithm.
+
+D. Acar, et al., "Federated Learning Based on Dynamic Regularization ,"
+in the Proceedings of ICLR 2021.
+
+https://openreview.net/forum?id=B7v4QMR6Z9w
+
+Source code: https://github.com/alpemreacar/FedDyn
+"""
+from plato.servers import fedavg
+from plato.clients import simple
+
+import feddyn_trainer
+
+
+def main():
+    """A Plato federated learning training session using FedProx."""
+    trainer = feddyn_trainer.Trainer
+    client = simple.Client(trainer=trainer)
+    server = fedavg.Server(trainer=trainer)
+    server.run(client)
+
+
+if __name__ == "__main__":
+    main()
