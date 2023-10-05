2779952762891682f3188dba3e7c73392b21b628
Dixi Yao <pspo-qfl@163.com>
2023-10-05 16:47:21 -0400

    changed the server trainer.

--- examples/feddyn/feddyn.py
+++ examples/feddyn/feddyn.py
@@ -18,7 +18,7 @@ def main():
     """A Plato federated learning training session using FedProx."""
     trainer = feddyn_trainer.Trainer
     client = simple.Client(trainer=trainer)
-    server = fedavg.Server(trainer=trainer)
+    server = fedavg.Server()
     server.run(client)
 
 
