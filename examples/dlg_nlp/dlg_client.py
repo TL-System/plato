12ff2474c2f8e0accd23e8821da55b4e918b69a0
Fei Wang <917522022@qq.com>
2023-10-07 00:38:49 -0600

    Port LAMP into dlg_nlp

---
+++ examples/dlg_nlp/dlg_client.py
@@ -0,0 +1,18 @@
+import pickle
+
+from plato.clients import simple
+from plato.config import Config
+
+
+class Client(simple.Client):
+    async def inbound_processed(self, processed_inbound_payload):
+        """Add extra information along with the original payload for attack validation at server."""
+        report, outbound_payload = await super().inbound_processed(
+            processed_inbound_payload
+        )
+        file_path = f"{Config().params['model_path']}/{self.client_id}.pickle"
+        with open(file_path, "rb") as handle:
+            gt_data, gt_labels, target_grad = pickle.load(handle)
+
+        outbound_payload = (outbound_payload, gt_data, gt_labels, target_grad)
+        return report, outbound_payload
