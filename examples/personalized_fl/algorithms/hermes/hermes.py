d79c7527a73702d4a7b3d4162a34ae36500dae40
Sijia Chen <CSJDeveloper@users.noreply.github.com>
2023-10-10 12:25:16 -0400

    Included the hermes's client

--- examples/personalized_fl/algorithms/hermes/hermes.py
+++ examples/personalized_fl/algorithms/hermes/hermes.py
@@ -6,7 +6,6 @@ A. Li, J. Sun, P. Li, Y. Pu, H. Li, and Y. Chen,
 in Proc. 27th Annual International Conference on Mobile Computing and Networking (MobiCom), 2021.
 """
 
-from pflbases import personalized_client
 from pflbases import fedavg_partial
 
 from pflbases.trainer_callbacks import separate_trainer_callbacks
@@ -14,13 +13,14 @@ from pflbases.client_callbacks import base_callbacks
 
 import hermes_trainer
 import hermes_server
+import hermes_client
 
 
 def main():
     """A Plato federated learning training session using the Hermes algorithm."""
     trainer = hermes_trainer.Trainer
 
-    client = personalized_client.Client(
+    client = hermes_client.Client(
         trainer=trainer,
         algorithm=fedavg_partial.Algorithm,
         callbacks=[base_callbacks.ClientPayloadCallback],
