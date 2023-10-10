26c83d7182643c850d18133a26eb4798697b7f82
Sijia Chen <CSJDeveloper@users.noreply.github.com>
2023-10-10 12:16:38 -0400

    Removed personalization process from the client

--- examples/personalized_fl/algorithms/hermes/hermes_trainer.py
+++ examples/personalized_fl/algorithms/hermes/hermes_trainer.py
@@ -1,13 +1,6 @@
 """
 The training loop that takes place on clients.
 
-
-As each client of Hermes do not hold the personalized model but receives one 
-from the server, there is no need to do any operations on the personalized_model.
-But the received model will be personalized model directly.
-
-Therefore, personalized_learning will be set to False all the time.
-
 """
 
 import logging
@@ -39,14 +32,6 @@ class Trainer(personalized_trainer.Trainer):
             if hasattr(Config().clients, "accuracy_threshold")
             else 0.5
         )
-
-    def set_training_mode(self, personalized_mode: bool):
-        """Set the learning model of this trainer.
-
-        As the received model is the personalized model of the client,
-        the personalized_learning will be set to False all the time.
-        """
-        self.personalized_learning = False
 
     def train_run_start(self, config):
         """Conducts pruning if needed before training."""
