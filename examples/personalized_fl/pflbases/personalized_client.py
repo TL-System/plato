1ba02442c947d38e1dc8c01a4ac68cd7790b2081
Sijia Chen <CSJDeveloper@users.noreply.github.com>
2023-10-19 14:30:01 -0400

    Redesigned the personalized client by removing invalid or unnecessary code

--- examples/personalized_fl/pflbases/personalized_client.py
+++ examples/personalized_fl/pflbases/personalized_client.py
@@ -35,6 +35,8 @@ from plato.config import Config
 from plato.utils import fonts
 from plato.utils.filename_formatter import NameFormatter
 
+from pflbases import checkpoint_operator
+
 
 class Client(simple.Client):
     """A basic personalized federated learning client."""
@@ -64,28 +66,12 @@ class Client(simple.Client):
         self.custom_personalized_model = personalized_model
         self.personalized_model = None
 
-        # the learning model to be performed in this client
-        # by default, performing `normal` fl's local update
-        # there are two options:
-        # 1.- normal
-        # 2.- personalization
-        self.learning_mode = "normal"
-
-        # which group that client belongs to
-        # there are two options:
-        # 1. participant
-        # 2. nonparticipant
-        self.client_group = "participant"
+        # the path of the initial personalized model of this client
+        self.init_personalized_model_path = None
 
         # whether this client contains the corresponding
         # personalized model
         self.new_client = False
-
-    def process_server_response(self, server_response) -> None:
-        """Additional client-specific processing on the server response."""
-
-        super().process_server_response(server_response)
-        self.learning_mode = server_response["learning_mode"]
 
     def configure(self) -> None:
         """Performing the general client's configure and then initialize the
@@ -105,109 +91,63 @@ class Client(simple.Client):
         ):
             self.personalized_model = self.custom_personalized_model
 
-        if self.trainer.personalized_model is None:
+        # get the initial personalized model path
+        self.init_personalized_model_path = self.get_init_personalized_model_path()
+
+        # if this client does not a personalized model yet.
+        # define a an initial one and save to the disk
+        if not self.exist_init_personalized_model():
+            # define its personalized model
             self.trainer.define_personalized_model(self.personalized_model)
-
+            self.trainer.save_personalized_model(
+                filename=os.path.basename(self.init_personalized_model_path),
+                location=self.trainer.get_checkpoint_dir_path(),
+            )
         self.personalized_model = self.trainer.personalized_model
-        self.trainer.set_training_mode(personalized_mode=self.is_personalized_learn())
-
-    def load_personalized_model(self):
-        """Load the personalized model.
-
-        This function is necessary for personalized federated learning in
-        Plato. Because, in general, when one client is called the first time,
-        its personalized model should be randomly intialized. Howerver,
-        Plato utilizes the `process` to simulate the client and only the client
-        id of each `process` is changed.
-
-        Therefore, in each round, the selected client (each `process`) should load
-        its personalized model instead of using the current self.personalized_model
-        trained by others.
-
-        By default,
-        1. the personalized model will be loaded from the initialized one.
-        2. load the latest persisted personalized model.
-        """
-        personalized_model_name = self.trainer.personalized_model_name
-        logging.info(
-            fonts.colourize(
-                "[Client #%d] Loading its personalized model named %s.", colour="blue"
-            ),
-            self.client_id,
-            personalized_model_name,
-        )
-        filename = self.is_new_client()
-
-        if not self.new_client:
-            self.trainer.create_unique_personalized_model(filename)
-
-        # when `persist_personalized_model` is set to be True, it means
-        # that each client want to load its latest trained personalzied
-        # model instead of using the initial one.
-        if (
-            hasattr(Config().algorithm.personalization, "persist_personalized_model")
-            and Config().algorithm.personalization.persist_personalized_model
-        ):
-            # load the client's latest personalized model
-            desired_round = self.current_round - 1
-
-            logging.info(
-                fonts.colourize(
-                    "[Client #%d] Loading latest personalized model.", colour="blue"
-                ),
-                self.client_id,
-            )
-        else:
-            # client does not want to use its trained personalzied model
-            # thus, load the initial personalized model saved by
-            # `self.persist_initial_personalized_model`
-            # i.e., rollback
-            desired_round = 0
-            logging.info(
-                fonts.colourize(
-                    "[Client #%d] Loading initial unique personalized model.",
-                    colour="blue",
-                ),
-                self.client_id,
-            )
-
-        checkpoint_dir_path = self.trainer.get_checkpoint_dir_path()
-        loaded_status = self.trainer.rollback_model(
-            rollback_round=desired_round,
-            location=checkpoint_dir_path,
-        )
-        self.trainer.personalized_model.load_state_dict(
-            loaded_status["model"], strict=True
-        )
 
     def inbound_received(self, inbound_processor):
         """Reloading the personalized model for this client before any operations."""
         super().inbound_received(inbound_processor)
 
-        # load the personalized model before training
+        # load the personalized model when
+        # 1. the personalization is performed per round
+        # 2. the current round is larger than the total rounds,
+        #   which means the final personalization.
         if (
-            hasattr(Config().algorithm.personalization, "load_model_per_round")
-            and Config().algorithm.personalization.load_model_per_round
-        ) or (
-            self.is_personalized_learn() and self.trainer.personalized_model is not None
+            self.train.is_round_personalization()
+            or self.trainer.is_final_personalization()
         ):
-            self.load_personalized_model()
+            self.get_personalized_model()
 
-        # assign the testset and testset sampler to the trainer
-        self.trainer.set_testset(self.testset)
-        self.trainer.set_testset_sampler(self.testset_sampler)
+    def get_personalized_model(self):
+        """Getting the personalized model of the client."""
 
-    def is_personalized_learn(self):
-        """Whether this client will perform personalization."""
-        return self.learning_mode == "personalization"
+        # always get the latest personalized model.
+        desired_round = self.current_round - 1
+        location = self.get_checkpoint_dir_path()
 
-    def is_participant_group(self):
-        """Whether this client participants the federated training."""
-        return self.client_group == "participant"
+        filename, is_searched = checkpoint_operator.search_client_checkpoint(
+            client_id=self.client_id,
+            checkpoints_dir=location,
+            model_name=self.trainer.personalized_model_name,
+            current_round=desired_round,
+            run_id=None,
+            epoch=None,
+            prefix=self.trainer.personalized_model_checkpoint_prefix,
+            anchor_metric="round",
+            mask_words=["epoch"],
+            use_latest=True,
+        )
+        if is_searched:
+            self.trainer.load_personalized_model(filename, location=location)
+        else:
+            self.trainer.load_personalized_model(
+                filename=os.path.basename(self.init_personalized_model_path),
+                location=self.trainer.get_checkpoint_dir_path(),
+            )
 
-    def is_new_client(self):
-        """Whether this client is a new one, which is never selected by the
-        server, as a result, no unique personalized model is maintained."""
+    def get_init_personalized_model_path(self):
+        """Get the path of the personalized model."""
         checkpoint_dir_path = self.trainer.get_checkpoint_dir_path()
 
         filename = NameFormatter.get_format_name(
@@ -219,8 +159,11 @@ class Client(simple.Client):
             prefix=self.trainer.personalized_model_checkpoint_prefix,
             ext="pth",
         )
-        checkpoint_file_path = os.path.join(checkpoint_dir_path, filename)
+        model_path = os.path.join(checkpoint_dir_path, filename)
 
-        self.new_client = os.path.exists(checkpoint_file_path)
+        return model_path
 
-        return filename
+    def exist_init_personalized_model(self):
+        """Whether this client is unselected on."""
+
+        return not os.path.exists(self.init_personalized_model_path)
