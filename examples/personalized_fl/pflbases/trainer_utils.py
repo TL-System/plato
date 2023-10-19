fe9c0c52c32a34187af944fea64e8600aa46f232
Sijia Chen <CSJDeveloper@users.noreply.github.com>
2023-10-19 14:30:23 -0400

    Added the random seed setting

--- examples/personalized_fl/pflbases/trainer_utils.py
+++ examples/personalized_fl/pflbases/trainer_utils.py
@@ -1,9 +1,24 @@
 """
 The necessary tools used by trainers.
 """
-
+import random
 import logging
 from pflbases import fedavg_partial
+
+import torch
+import numpy as np
+
+
+def set_random_seeds(seed: int = 0):
+    """Setting the random seed for all parts toward reproducibility."""
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # if you are using multi-GPU.
+    np.random.seed(seed)  # Numpy module.
+    random.seed(seed)  # Python random module.
+    torch.manual_seed(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
 
 
 def freeze_model(model, modules_name=None, log_info: str = ""):
