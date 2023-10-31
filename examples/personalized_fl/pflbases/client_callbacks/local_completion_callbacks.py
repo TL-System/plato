4cf374955b90d950d0417f201eb1faa22282c75f
Sijia Chen <CSJDeveloper@users.noreply.github.com>
2023-10-31 14:18:06 -0400

    Upgraded the local completion callback to make it work

--- examples/personalized_fl/pflbases/client_callbacks/local_completion_callbacks.py
+++ examples/personalized_fl/pflbases/client_callbacks/local_completion_callbacks.py
@@ -2,7 +2,7 @@
 Customize client callbacks for assigning the modules of client's local model
 to the received payload.
 """
-
+import os
 import logging
 from typing import Any
 
@@ -13,7 +13,7 @@ from pflbases.client_callbacks import base_callbacks
 
 
 class PayloadCompletionProcessor(base.Processor):
-    """A processor relying on the hyper-parameter `completion_module_names`
+    """A processor relying on the hyper-parameter `local_module_names`
     to complete parameters of payload with the loaded local model, which
     is the updated global model in the previous round."""
 
@@ -25,40 +25,66 @@ class PayloadCompletionProcessor(base.Processor):
         """Processing the received payload by assigning modules of local model of
         each client."""
 
-        # Extract the `completion_module_names` of the model head.
-        assert hasattr(Config().algorithm, "completion_module_names")
+        # Extract the `local_module_names` of the model head.
+        assert hasattr(Config().algorithm, "local_module_names")
+        local_module_names = Config().algorithm.local_module_names
 
-        completion_module_names = Config().algorithm.completion_module_names
-        local_model_modules = self.trainer.model.cpu().state_dict()
+        # Local the previous saved model
+        filename = f"client_{self.trainer.client_id}_local_model.pth"
+        location = Config().params["checkpoint_path"]
+
+        if os.path.exists(os.path.join(location, filename)):
+            self.trainer.load_model(filename, location=location)
+
+        model_modules = self.trainer.model.cpu().state_dict()
         logging.info(
-            "[Client #%d] Loaded the local model containing modules: %s.",
+            "[Client #%d] Local model contains: %s.",
             self.trainer.client_id,
-            self.algorithm.extract_module_names(list(local_model_modules.keys())),
+            self.algorithm.extract_module_names(list(model_modules.keys())),
         )
 
-        local_completion_modules = self.algorithm.get_target_weights(
-            model_parameters=local_model_modules, module_names=completion_module_names
+        local_modules = self.algorithm.get_module_weights(
+            model_parameters=model_modules, module_names=local_module_names
         )
-
-        data.update(local_completion_modules)
+        data.update(local_modules)
 
         logging.info(
-            "[Client #%d] Completed the payload with extracted modules: %s.",
+            "[Client #%d] Completed the payload by extracting local modules: %s.",
             self.trainer.client_id,
-            self.algorithm.extract_module_names(list(local_completion_modules.keys())),
+            self.algorithm.extract_module_names(list(local_modules.keys())),
         )
 
         return data
 
 
-class ClientModelLocalCompletionCallback(base_callbacks.ClientPayloadCallback):
+class SaveLocalModelProcessor(base.Processor):
+    """
+    A processor for clients to always save the trained local model.
+    """
+
+    def __init__(self, client_id, trainer, **kwargs) -> None:
+        super().__init__(**kwargs)
+
+        self.client_id = client_id
+        self.trainer = trainer
+
+    def process(self, data: Any):
+        """Save the local model before sending the data."""
+        filename = f"client_{self.client_id}_local_model.pth"
+        location = Config().params["checkpoint_path"]
+        self.trainer.save_model(filename, location=location)
+
+        return data
+
+
+class PayloadCompletionCallback(base_callbacks.ClientPayloadCallback):
     """
     A client callback for processing payload by assigning parameters of the local
     model to it.
     """
 
     def on_inbound_received(self, client, inbound_processor):
-        """Completing the recevied payload with the local updated model,
+        """Complete the recevied payload with the local updated model,
         which is the locally updated global model in the previous round."""
         super().on_inbound_received(client, inbound_processor)
 
@@ -69,3 +95,12 @@ class ClientModelLocalCompletionCallback(base_callbacks.ClientPayloadCallback):
                 name="LocalPayloadCompletionProcessor",
             )
         )
+
+    def on_outbound_ready(self, client, report, outbound_processor):
+        """Save local model of the client."""
+        send_payload_processor = SaveLocalModelProcessor(
+            client_id=client.client_id,
+            trainer=client.trainer,
+            name="SaveLocalModelProcessor",
+        )
+        outbound_processor.processors.insert(0, send_payload_processor)
