2bbf2eafa687ff58a1a78f8aceda68ce182057a2
Sijia Chen <CSJDeveloper@users.noreply.github.com>
2023-10-31 14:31:13 -0400

    Update fedavg_finetune_trainer.py

--- examples/personalized_fl/algorithms/fedavg_finetune/fedavg_finetune_trainer.py
+++ examples/personalized_fl/algorithms/fedavg_finetune/fedavg_finetune_trainer.py
@@ -1,7 +1,7 @@
 """
 The trainer for the fedavg algorithm with fine-tuning.
 """
-
+from pflbases import trainer_utils
 
 from plato.trainers import basic
 from plato.config import Config
@@ -11,7 +11,13 @@ class Trainer(basic.Trainer):
     """A trainer to create a personalized training process after the final round."""
 
     def train_run_start(self, config):
-        """A new train model to load the epochs for personalization."""
+        """Freeze the body during personalization."""
         super().train_run_start(config)
         if self.current_round > Config().trainer.rounds:
+            # Freeze the model body while only optimizing the head
+            # furing the final personalization
+            trainer_utils.freeze_model(
+                self.model, Config().algorithm.global_module_names
+            )
+            # Set the number of epochs for personalization
             config["epochs"] = Config().algorithm.personalization.epochs
