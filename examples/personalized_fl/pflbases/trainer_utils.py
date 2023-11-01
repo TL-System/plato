3e3174130db001e39947361220b2084f5c3b0687
Sijia Chen <CSJDeveloper@users.noreply.github.com>
2023-10-31 22:53:55 -0400

    Removed the extract_module_names from the base code

--- examples/personalized_fl/pflbases/trainer_utils.py
+++ examples/personalized_fl/pflbases/trainer_utils.py
@@ -3,6 +3,8 @@ The necessary tools used by trainers.
 """
 import os
 import re
+import string
+from typing import List
 
 
 def freeze_model(model, layer_names=None):
@@ -42,3 +44,36 @@ def get_latest_checkpoint(checkpoint_dir: str):
             latest_checkpoint_filename = ckp_file
 
     return latest_checkpoint_filename
+
+
+def extract_module_names(parameter_names: List[str]):
+    """
+    Extract module names from the given parameter names. A parameter name is a list of names
+    connected by `.`, such as `encoder.conv1.weight`.
+    """
+    split_char = "."
+
+    # Converting `encoder.conv1.weight`` to [encoder, conv1, weight]
+    translator = str.maketrans("", "", string.punctuation)
+    splitted_names = [
+        [subname.translate(translator).lower() for subname in name.split(split_char)]
+        for name in parameter_names
+    ]
+
+    # With [encoder, conv1, weight], [encoder, conv1, bias], diff_idx = 1.
+    diff_idx = 0
+    for idx, subnames in enumerate(zip(*splitted_names)):
+        if len(set(subnames)) > 1:
+            diff_idx = idx
+            break
+
+    # Extract the first `diff_idx` parameter names as module names
+    extracted_names = []
+    for para_name in parameter_names:
+        splitted_names = para_name.split(split_char)
+        core_names = splitted_names[: diff_idx + 1]
+        module_name = f"{split_char}".join(core_names)
+        if module_name not in extracted_names:
+            extracted_names.append(module_name)
+
+    return extracted_names
