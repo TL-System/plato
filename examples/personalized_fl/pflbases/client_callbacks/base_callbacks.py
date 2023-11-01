6e10630319ad1569574ba1106ccdb17de29a7431
Sijia Chen <CSJDeveloper@users.noreply.github.com>
2023-10-31 21:42:31 -0400

    Adjusted the base callback as the extract module is a static method

--- examples/personalized_fl/pflbases/client_callbacks/base_callbacks.py
+++ examples/personalized_fl/pflbases/client_callbacks/base_callbacks.py
@@ -7,6 +7,7 @@ from typing import Any
 
 from plato.processors import base
 from plato.callbacks import client as client_callbacks
+from pflbases.fedavg_partial import Algorithm
 
 
 class PayloadStatusProcessor(base.Processor):
@@ -14,10 +15,6 @@ class PayloadStatusProcessor(base.Processor):
     A default payload status processor to present what modules are
     contained in the received payload.
     """
-
-    def __init__(self, algorithm, **kwargs) -> None:
-        super().__init__(**kwargs)
-        self.algorithm = algorithm
 
     def process(self, data: Any) -> Any:
         if isinstance(data, (list, tuple)):
@@ -27,7 +24,7 @@ class PayloadStatusProcessor(base.Processor):
         logging.info(
             "[Client #%d] Received the payload containing modules: %s.",
             self.trainer.client_id,
-            self.algorithm.extract_module_names(list(payload.keys())),
+            Algorithm.extract_module_names(list(payload.keys())),
         )
 
         return data
@@ -40,5 +37,5 @@ class ClientPayloadCallback(client_callbacks.ClientCallback):
 
     def on_inbound_received(self, client, inbound_processor):
         inbound_processor.processors.append(
-            PayloadStatusProcessor(trainer=client.trainer, algorithm=client.algorithm)
+            PayloadStatusProcessor(trainer=client.trainer)
         )
