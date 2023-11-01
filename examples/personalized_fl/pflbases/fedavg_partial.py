c859a37ad70e97d643b857ae42eb73d680275323
Sijia Chen <CSJDeveloper@users.noreply.github.com>
2023-11-01 15:21:54 -0400

    Added and revised comments of fedavg_partial

--- examples/personalized_fl/pflbases/fedavg_partial.py
+++ examples/personalized_fl/pflbases/fedavg_partial.py
@@ -1,5 +1,5 @@
 """
-Loading the model weights for personalized FL clients.
+A base algorithm to load and save local layers of a model.
 """
 import os
 import logging
@@ -11,22 +11,23 @@ from plato.config import Config
 
 
 class Algorithm(fedavg.Algorithm):
-    """A base algorithm for extracting layers from a model."""
+    """A base algorithm to load local layers to the received weights and save
+    local layers after the local training."""
 
     def load_weights(self, weights):
         """
-        Loads the first part of the model with the global received from the server
-            and the second part of the model with the saved local model.
+        Loads local layers included in `local_layer_names` to the received weights which,
+        will be loaded to the model.
         """
-
         if hasattr(Config().algorithm, "local_layer_names"):
-            # Load the local model that has previously been saved
+            # Get the filename of the previous saved local layer
             model_name = Config().trainer.model_name
             model_path = Config().params["model_path"]
             filename = f"{model_path}/{model_name}_{self.client_id}_local_layers.pth"
+            # Load local layers to the weights when the file exists
             if os.path.exists(filename):
                 local_layers = torch.load(filename, map_location=torch.device("cpu"))
-
+                # Update the received weights with the loaded local layers
                 weights.update(local_layers)
 
                 logging.info(
