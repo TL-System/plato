dda0f773e068ba1fec78e933069226e58455f803
Dixi Yao <pspo-qfl@163.com>
2023-11-01 14:47:22 -0400

    added saving local layers before giving the weights to outbound processor.

--- examples/personalized_fl/pflbases/fedavg_partial.py
+++ examples/personalized_fl/pflbases/fedavg_partial.py
@@ -5,6 +5,7 @@ import os
 import logging
 from collections import OrderedDict
 
+import torch
 from plato.algorithms import fedavg
 from plato.config import Config
 
@@ -17,24 +18,17 @@ class Algorithm(fedavg.Algorithm):
         Loads the first part of the model with the global received from the server
             and the second part of the model with the saved local model.
         """
+
         if hasattr(Config().algorithm, "local_layer_names"):
             # Load the local model that has previously been saved
-            filename = f"client_{self.trainer.client_id}_local_model.pth"
-            location = Config().params["checkpoint_path"]
-            if os.path.exists(os.path.join(location, filename)):
-                local_layer_names = Config().algorithm.local_layer_names
-
-                # Extract weights of desired local layers
-                local_layers = OrderedDict(
-                    [
-                        (name, param)
-                        for name, param in enumerate(self.model.cpu().state_dict())
-                        if any(
-                            param_name in name.strip().split(".")
-                            for param_name in local_layer_names
-                        )
-                    ]
-                )
+            model_name = Config().trainer.model_name
+            model_path = Config().params["model_path"]
+            filename = f"{model_path}/{model_name}_{self.client_id}_local_layers.pth"
+            if os.path.exists(filename):
+                local_layers = torch.load(filename, map_location=torch.device("cpu"))
+                # Remove after local layers are loaded.
+                # Will save local layers after new weights are trained.
+                os.remove(filename)
 
                 weights.update(local_layers)
 
@@ -44,3 +38,24 @@ class Algorithm(fedavg.Algorithm):
                 )
 
         self.model.load_state_dict(weights, strict=True)
+
+    def extract_weights(self, model=None):
+        weights = super().extract_weights(model)
+        # Save the local layers before giving it to outbound processor.
+        if hasattr(Config().algorithm, "local_layer_names"):
+            # Extract weights of desired local layers
+            local_layers = OrderedDict(
+                [
+                    (name, param)
+                    for name, param in weights.items()
+                    if any(
+                        param_name in name.strip().split(".")
+                        for param_name in Config().algorithm.local_layer_names
+                    )
+                ]
+            )
+            model_name = Config().trainer.model_name
+            model_path = Config().params["model_path"]
+            filename = f"{model_path}/{model_name}_{self.client_id}_local_layers.pth"
+            torch.save(local_layers, filename)
+        return weights
