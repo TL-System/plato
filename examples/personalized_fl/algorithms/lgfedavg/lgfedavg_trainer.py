c37979d6df4389c4d589c6c8022625cdacb347a2
Dixi Yao <pspo-qfl@163.com>
2023-11-01 15:44:25 -0400

    revised lgfedavg with simpler coder.

--- examples/personalized_fl/algorithms/lgfedavg/lgfedavg_trainer.py
+++ examples/personalized_fl/algorithms/lgfedavg/lgfedavg_trainer.py
@@ -3,7 +3,6 @@ A personalized federated learning trainer using LG-FedAvg.
 
 """
 
-
 from pflbases import trainer_utils
 from plato.trainers import basic
 from plato.config import Config
@@ -14,44 +13,14 @@ class Trainer(basic.Trainer):
 
     def perform_forward_and_backward_passes(self, config, examples, labels):
         """Performing one iteration of LG-FedAvg."""
-        self.optimizer.zero_grad()
 
-        outputs = self.forward_examples(examples)
+        # First freeze the first part of the model and optimize the second part.
+        trainer_utils.freeze_model(self.model, Config().algorithm.local_layer_names)
+        trainer_utils.activate_model(self.model, Config().algorithm.global_layer_names)
+        loss = super().perform_forward_and_backward_passes(config, examples, labels)
 
-        loss = self._loss_criterion(outputs, labels)
-
-        if "create_graph" in config:
-            loss.backward(create_graph=config["create_graph"])
-        else:
-            loss.backward()
-
-        # first freeze the head and optimize the body
-        trainer_utils.freeze_model(self.model, Config().algorithm.head_layer_names)
-        trainer_utils.activate_model(self.model, Config().algorithm.body_layer_names)
-        self.optimizer.step()
-
-        # repeat the same optimization relying the optimized
-        # body of the model
-        self.optimizer.zero_grad()
-
-        outputs = self.forward_examples(examples)
-
-        loss = self._loss_criterion(outputs, labels)
-        self._loss_tracker.update(loss, labels.size(0))
-
-        if "create_graph" in config:
-            loss.backward(create_graph=config["create_graph"])
-        else:
-            loss.backward()
-
-        # first freeze the head and optimize the body
-        trainer_utils.freeze_model(
-            self.model,
-            Config().algorithm.body_layer_names,
-            log_info=None,
-        )
-        trainer_utils.activate_model(self.model, Config().algorithm.head_layer_names)
-
-        self.optimizer.step()
-
+        # Second freeze the second part of the model and optimize the first part.
+        trainer_utils.freeze_model(self.model, Config().algorithm.global_layer_names)
+        trainer_utils.activate_model(self.model, Config().algorithm.local_layer_names)
+        loss = super().perform_forward_and_backward_passes(config, examples, labels)
         return loss
