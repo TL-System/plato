946bf1228d911679bad77628cd6661d81d598439
Sijia Chen <CSJDeveloper@users.noreply.github.com>
2023-11-02 09:48:45 -0400

    Revised the names and comments of SMoG

--- examples/personalized_fl/algorithms/SSL/smog/smog_model.py
+++ examples/personalized_fl/algorithms/SSL/smog/smog_model.py
@@ -19,6 +19,8 @@ from plato.config import Config
 
 
 class SMoG(nn.Module):
+    """Core structure of the SMoG model."""
+
     def __init__(self, encoder=None):
         super().__init__()
         self.temperature = (
@@ -50,6 +52,7 @@ class SMoG(nn.Module):
         prediction_hidden_dim = Config().trainer.prediction_hidden_dim
         prediction_out_dim = Config().trainer.prediction_out_dim
 
+        # Define the projector.
         self.projection_head = SMoGProjectionHead(
             self.encoding_dim, projection_hidden_dim, projection_out_dim
         )
@@ -66,6 +69,8 @@ class SMoG(nn.Module):
         self.n_groups = Config().trainer.n_groups
         n_prototypes = Config().trainer.n_prototypes
         beta = Config().trainer.smog_beta
+
+        # Define the prototypes
         self.smog = SMoGPrototypes(
             group_features=torch.rand(self.n_groups, n_prototypes), beta=beta
         )
@@ -74,6 +79,7 @@ class SMoG(nn.Module):
         self.n_iteration = 0
 
     def _cluster_features(self, features: torch.Tensor) -> torch.Tensor:
+        """Cluster the features using sklearn."""
         # Cluster the features using sklearn
         # (note: faiss is probably more efficient).
         features = features.cpu().numpy()
@@ -83,45 +89,50 @@ class SMoG(nn.Module):
         return clustered
 
     def reset_group_features(self, memory_bank):
+        """Reset the group features based on the clusters."""
         # See https://arxiv.org/pdf/2207.06167.pdf Table 7b).
         features = memory_bank.bank
         group_features = self._cluster_features(features.t())
         self.smog.set_group_features(group_features)
 
     def reset_momentum_weights(self):
+        """Reset the momentum weights."""
         # See https://arxiv.org/pdf/2207.06167.pdf Table 7b).
         self.encoder_momentum = copy.deepcopy(self.encoder)
         self.projection_head_momentum = copy.deepcopy(self.projection_head)
         deactivate_requires_grad(self.encoder_momentum)
         deactivate_requires_grad(self.projection_head_momentum)
 
-    def forward_direct(self, samples):
-        features = self.encoder(samples).flatten(start_dim=1)
+    def forward_view(self, views):
+        """Forward views of the samples."""
+        features = self.encoder(views).flatten(start_dim=1)
         encoded = self.projection_head(features)
         predicted = self.prediction_head(encoded)
         return encoded, predicted
 
     def forward_momentum(self, samples):
+        """Forward the momentum mechanism."""
         features = self.encoder_momentum(samples).flatten(start_dim=1)
         encoded = self.projection_head_momentum(features)
         return encoded
 
     def forward(self, multiview_samples):
-        samples1, samples2 = multiview_samples
+        """Forward the multiview samples."""
+        views1, views2 = multiview_samples
 
         if self.n_iteration % 2:
             # Swap batches every two iterations.
-            samples2, samples1 = samples1, samples2
+            views2, views1 = views1, views2
 
-        samples1_encoded, samples1_predicted = self.forward_direct(samples1)
+        views1_encoded, views1_predicted = self.forward_view(views1)
 
-        samples2_encoded = self.forward_momentum(samples2)
+        views2_encoded = self.forward_momentum(views2)
 
         # Update group features and get group assignments.
-        assignments = self.smog.assign_groups(samples2_encoded)
-        group_features = self.smog.get_updated_group_features(samples1_encoded)
+        assignments = self.smog.assign_groups(views2_encoded)
+        group_features = self.smog.get_updated_group_features(views1_encoded)
         logits = self.smog(
-            samples1_predicted, group_features, temperature=self.temperature
+            views1_predicted, group_features, temperature=self.temperature
         )
         self.smog.set_group_features(group_features)
 
