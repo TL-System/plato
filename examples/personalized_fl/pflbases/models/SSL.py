3152f90ed190bd8a91ba3c557292af6ed4848d91
Sijia Chen <CSJDeveloper@users.noreply.github.com>
2023-11-01 23:53:33 -0400

    Revised the simsiam algorithm

--- examples/personalized_fl/pflbases/models/SSL.py
+++ examples/personalized_fl/pflbases/models/SSL.py
@@ -6,7 +6,6 @@ import copy
 import torch
 from torch import nn
 
-from lightly.models.modules import SimSiamPredictionHead, SimSiamProjectionHead
 from sklearn.cluster import KMeans
 from lightly.models.modules.heads import (
     SMoGPredictionHead,
@@ -20,47 +19,6 @@ from lightly.models.utils import deactivate_requires_grad
 
 from plato.models.cnn_encoder import Model as encoder_registry
 from plato.config import Config
-
-
-class SimSiam(nn.Module):
-    def __init__(self, encoder=None):
-        super().__init__()
-
-        encoder_name = Config().trainer.encoder_name
-        encoder_params = (
-            Config().params.encoder if hasattr(Config().params, "encoder") else {}
-        )
-        # Define the encoder based on the model_name in config.
-        if encoder is not None:
-            self.encoder = encoder
-        else:
-            self.encoder = encoder_registry.get(
-                model_name=encoder_name, **encoder_params
-            )
-
-        self.projection_head = SimSiamProjectionHead(
-            self.encoder.encoding_dim,
-            Config().trainer.projection_hidden_dimm,
-            Config().trainer.projection_out_dim,
-        )
-        self.prediction_head = SimSiamPredictionHead(
-            Config().trainer.projection_out_dim,
-            Config().trainer.prediction_hidden_dim,
-            Config().trainer.prediction_out_dim,
-        )
-
-    def forward_direct(self, samples):
-        encoded_samples = self.encoder(samples).flatten(start_dim=1)
-        projected_samples = self.projection_head(encoded_samples)
-        output = self.prediction_head(projected_samples)
-        projected_samples = projected_samples.detach()
-        return projected_samples, output
-
-    def forward(self, multiview_samples):
-        samples1, samples2 = multiview_samples
-        projected_samples1, output1 = self.forward_direct(samples1)
-        projected_samples2, output2 = self.forward_direct(samples2)
-        return (projected_samples1, output2), (projected_samples2, output1)
 
 
 class SMoG(nn.Module):
