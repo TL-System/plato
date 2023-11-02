84f51922526928daef1d6325bef94511516d1b92
Dixi Yao <pspo-qfl@163.com>
2023-11-02 10:53:14 -0400

    cleaned up swav code.

--- examples/personalized_fl/algorithms/SSL/swav/swav.py
+++ examples/personalized_fl/algorithms/SSL/swav/swav.py
@@ -10,7 +10,6 @@ Source code: https://github.com/facebookresearch/swav
 
 from pflbases import fedavg_personalized
 
-from pflbases.models import SSL
 from pflbases import ssl_client
 from pflbases import ssl_trainer
 from pflbases import ssl_datasources
@@ -28,7 +27,9 @@ def main():
         datasource=ssl_datasources.SSLDataSource,
         trainer=ssl_trainer.Trainer,
     )
-    server = fedavg_personalized.Server(model=SSL.SwaV, trainer=ssl_trainer.Trainer)
+    server = fedavg_personalized.Server(
+        model=swav_model.SwaV, trainer=ssl_trainer.Trainer
+    )
 
     server.run(client)
 
