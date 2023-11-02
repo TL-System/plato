779dd7d6ffc2c0214c63a5fd1bb9db89be890e2c
Sijia Chen <CSJDeveloper@users.noreply.github.com>
2023-11-02 16:45:32 -0400

    Update fedbabu.py

--- examples/personalized_fl/algorithms/fedbabu/fedbabu.py
+++ examples/personalized_fl/algorithms/fedbabu/fedbabu.py
@@ -9,11 +9,10 @@ https://openreview.net/pdf?id=HuaYQfggn5u
 Source code: https://github.com/jhoon-oh/FedBABU
 """
 
-from pflbases import fedavg_personalized
-from pflbases import fedavg_personalized
-from pflbases import fedavg_personalized
-
 import fedbabu_trainer
+
+from plato.servers import fedavg_personalized as personalized_server
+from plato.clients import fedavg_personalized as personalized_client
 
 
 def main():
@@ -21,10 +20,8 @@ def main():
     A personalized federated learning session for FedBABU algorithm under the supervised setting.
     """
     trainer = fedbabu_trainer.Trainer
-    client = fedavg_personalized.Client(
-        trainer=trainer, algorithm=fedavg_personalized.Algorithm
-    )
-    server = fedavg_personalized.Server(trainer=trainer)
+    client = personalized_client.Client(trainer=trainer)
+    server = personalized_server.Server(trainer=trainer)
 
     server.run(client)
 
