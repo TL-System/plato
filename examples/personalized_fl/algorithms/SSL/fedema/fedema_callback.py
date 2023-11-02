82750b6857cb1d3f29a0e3c775f167601b81e2ca
Sijia Chen <CSJDeveloper@users.noreply.github.com>
2023-11-02 10:40:49 -0400

    Revised the code of FedEMA and made it compatible with pflbases

--- examples/personalized_fl/algorithms/SSL/fedema/fedema_callback.py
+++ examples/personalized_fl/algorithms/SSL/fedema/fedema_callback.py
@@ -3,14 +3,27 @@ Customize the processor for FedEMA.
 """
 import logging
 from typing import Any
+from collections import OrderedDict
 
-import torch
+from moving_average import ModelEMA
 
 from plato.config import Config
 from plato.callbacks.client import ClientCallback
 from plato.processors import base
 
-from moving_average import ModelEMA
+
+def extract_encoder(model_layers, encoder_layer_names):
+    """Extract the encoder layers from the model layers."""
+    return OrderedDict(
+        [
+            (name, param)
+            for name, param in model_layers.items()
+            if any(
+                param_name in name.strip().split(".")
+                for param_name in encoder_layer_names
+            )
+        ]
+    )
 
 
 class GlobalLocalDivergenceProcessor(base.Processor):
@@ -19,56 +32,44 @@ class GlobalLocalDivergenceProcessor(base.Processor):
     and the local model.
     """
 
-    def __init__(self, algorithm, **kwargs) -> None:
-        super().__init__(**kwargs)
-        self.algorithm = algorithm
-
     def process(self, data: Any) -> Any:
         """Processing the received payload by assigning layers of local model of
         each client."""
 
         divergence_scale = data[1]
 
-        # extract the `encoder_layer_names` of the model head
+        # Extract the `encoder_layer_names` of the model head
         assert hasattr(Config().algorithm, "encoder_layer_names")
 
         local_model_layers = self.trainer.model.cpu().state_dict()
         global_layers = data[0]
 
-        global_layer_names = Config().algorithm.global_layer_names
         encoder_layer_names = Config().algorithm.encoder_layer_names
 
-        local_encoder_layers = self.algorithm.get_target_weights(
-            model_parameters=local_model_layers, layer_names=encoder_layer_names
-        )
-        global_encoder_layers = self.algorithm.get_target_weights(
-            model_parameters=global_layers, layer_names=encoder_layer_names
-        )
+        # Get encoder layers of the local and global models
+        local_encoder_layers = extract_encoder(local_model_layers, encoder_layer_names)
+        global_encoder_layers = extract_encoder(global_layers, encoder_layer_names)
+
         logging.info(
-            "[Client #%d] Computing global and local divergence on layers: %s.",
+            "[Client #%d] Computing global and local divergence.",
             self.trainer.client_id,
-            self.algorithm.extract_layer_names(list(local_encoder_layers.keys())),
         )
 
-        # compute the divergence between encoders of local and global models
+        # Compute the divergence between encoders of local and global models
         l2_distance = ModelEMA.get_parameters_diff(
             parameter_a=local_encoder_layers,
             parameter_b=global_encoder_layers,
         )
 
-        # EMA update
+        # Perform EMA update
         divergence_scale = min(l2_distance * divergence_scale, 1)
 
-        # The local model
-        local_layers = self.algorithm.get_target_weights(
-            model_parameters=local_model_layers, layer_names=global_layer_names
-        )
         ema_operator = ModelEMA(beta=divergence_scale)
         ema_parameters = ema_operator.update_parameters_moving_average(
-            previous_parameters=local_layers,
+            previous_parameters=local_model_layers,
             current_parameters=global_layers,
         )
-        # update the ema parameters
+        # Update the ema parameters
         data[0].update(ema_parameters)
 
         logging.info(
@@ -92,11 +93,6 @@ class FedEMACallback(ClientCallback):
         """
         extract_payload_processor = GlobalLocalDivergenceProcessor(
             trainer=client.trainer,
-            algorithm=client.algorithm,
-            name="LocalPayloadCompletionProcessor",
+            name="GlobalLocalDivergenceProcessor",
         )
         inbound_processor.processors.insert(0, extract_payload_processor)
-
-        logging.info(
-            "[%s] List of inbound processors: %s.", client, inbound_processor.processors
-        )
