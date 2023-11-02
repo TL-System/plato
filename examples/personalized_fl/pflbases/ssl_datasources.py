8196a1f55f9200861a537ec5c93028100441ceb3
Sijia Chen <CSJDeveloper@users.noreply.github.com>
2023-11-01 21:21:04 -0400

    Cleaned the comment and cleaned invalid code

--- examples/personalized_fl/pflbases/ssl_datasources.py
+++ examples/personalized_fl/pflbases/ssl_datasources.py
@@ -29,7 +29,7 @@ registered_transforms = {
 
 
 def get_transforms(transforms_block: dict):
-    """Obtaining train/test transforms for the corresponding data."""
+    """Obtains train/test transforms for the corresponding data."""
 
     data_transforms = {}
 
@@ -50,7 +50,6 @@ def get_transforms(transforms_block: dict):
         else:
             raise ValueError(f"No such data source: {transform_name}")
 
-        logging.info("Data train transform: %s", transform_config["name"])
         data_transforms.update({"train_transform": dataset_transform})
 
     return data_transforms
@@ -61,11 +60,10 @@ class SSLDataSource(base.DataSource):
     input to define the datasource.
     """
 
-    def __init__(self, transforms_block: dict = None):
+    def __init__(self):
         super().__init__()
         # Use the transform set in the config file.
-        if transforms_block is None:
-            transforms_block = Config().algorithm.data_transforms._asdict()
+        transforms_block = Config().algorithm.data_transforms._asdict()
 
         data_transforms = get_transforms(transforms_block)
 
