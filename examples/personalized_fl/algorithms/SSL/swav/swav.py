ef9e0290c2b1f97dcc890d4b419629c63cceccdb
Sijia Chen <CSJDeveloper@users.noreply.github.com>
2023-11-02 00:48:39 -0400

    Revised SwAV

--- examples/personalized_fl/algorithms/SSL/swav/swav.py
+++ examples/personalized_fl/algorithms/SSL/swav/swav.py
@@ -7,14 +7,15 @@ https://arxiv.org/abs/2006.09882, NeurIPS 2020.
 Source code: https://github.com/facebookresearch/swav
 """
 
-from pflbases import fedavg_personalized_server
-from pflbases import fedavg_partial
+from pflbases import fedavg_personalized
 
-from pflbases.client_callbacks import local_completion_callbacks
 from pflbases.models import SSL
 from pflbases import ssl_client
 from pflbases import ssl_trainer
 from pflbases import ssl_datasources
+
+
+import swav_model
 
 
 def main():
@@ -23,20 +24,11 @@ def main():
     """
     trainer = ssl_trainer.Trainer
     client = ssl_client.Client(
-        model=SSL.SwaV,
-        datasource=ssl_datasources.TransformedDataSource,
-        personalized_datasource=ssl_datasources.TransformedDataSource,
+        model=swav_model.SwaV,
+        datasource=ssl_datasources.SSLDataSource,
         trainer=trainer,
-        algorithm=fedavg_partial.Algorithm,
-        callbacks=[
-            local_completion_callbacks.ClientModelLocalCompletionCallback,
-        ],
     )
-    server = fedavg_personalized_server.Server(
-        model=SSL.SwaV,
-        trainer=trainer,
-        algorithm=fedavg_partial.Algorithm,
-    )
+    server = fedavg_personalized.Server(model=SSL.SwaV, trainer=trainer)
 
     server.run(client)
 
