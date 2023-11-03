2bda402735be88a9a982947a16f21617e763509c
Sijia Chen <CSJDeveloper@users.noreply.github.com>
2023-11-03 10:54:34 -0400

    Revised comments and adjusted variable names

--- examples/ssl/algorithms/byol/byol_model.py
+++ examples/ssl/algorithms/byol/byol_model.py
@@ -30,12 +30,13 @@ class BYOLModel(nn.Module):
                 model_name=encoder_name, **encoder_params
             )
 
-        # A projector project higher dimension features to output dimensions.
+        # A projector projects higher dimension features to output dimensions
         self.projector = BYOLProjectionHead(
             self.encoder.encoding_dim,
             Config().trainer.projection_hidden_dim,
             Config().trainer.projection_out_dim,
         )
+        # A predictor predicts the output dimensions to the prediction dimensions
         self.predictor = BYOLPredictionHead(
             Config().trainer.projection_out_dim,
             Config().trainer.prediction_hidden_dim,
@@ -51,19 +52,19 @@ class BYOLModel(nn.Module):
         deactivate_requires_grad(self.momentum_encoder)
         deactivate_requires_grad(self.momentum_projector)
 
-    def forward_view(self, sample):
+    def forward_view(self, view_sample):
         """Foward one view to get the output."""
-        encoded_sample = self.encoder(sample).flatten(start_dim=1)
-        projected_sample = self.projector(encoded_sample)
-        output = self.predictor(projected_sample)
+        encoded_view = self.encoder(view_sample).flatten(start_dim=1)
+        projected_view = self.projector(encoded_view)
+        output = self.predictor(projected_view)
         return output
 
-    def forward_momentum(self, sample):
+    def forward_momentum(self, view_sample):
         """Foward one view to get the output in a momentum manner."""
-        encoded_example = self.momentum_encoder(sample).flatten(start_dim=1)
-        projected_example = self.momentum_projector(encoded_example)
-        projected_example = projected_example.detach()
-        return projected_example
+        encoded_view = self.momentum_encoder(view_sample).flatten(start_dim=1)
+        projected_view = self.momentum_projector(encoded_view)
+        projected_view = projected_view.detach()
+        return projected_view
 
     def forward(self, multiview_samples):
         """Main forward function of the model."""
