81c77e270206e936dcf76145202bc2fde8209d6a
Sijia Chen <CSJDeveloper@users.noreply.github.com>
2023-11-02 22:42:00 -0400

    Added more comments

--- examples/ssl/algorithms/moco/mocov2_trainer.py
+++ examples/ssl/algorithms/moco/mocov2_trainer.py
@@ -16,6 +16,7 @@ class Trainer(ssl_trainer.Trainer):
     def __init__(self, model=None, callbacks=None):
         super().__init__(model, callbacks)
 
+        # The value used in the model momentum mechanism
         self.momentum_val = 0
 
     def train_epoch_start(self, config):
@@ -24,15 +25,12 @@ class Trainer(ssl_trainer.Trainer):
         epoch = self.current_epoch
         total_epochs = config["epochs"] * config["rounds"]
         global_epoch = (self.current_round - 1) * config["epochs"] + epoch
+        # Compute the momentum value during the regular federated training process
         if not self.current_round > Config().trainer.rounds:
             self.momentum_val = cosine_schedule(global_epoch, total_epochs, 0.996, 1)
 
     def train_step_start(self, config, batch=None):
-        """
-        At the start of every iteration,
-            update the models for generating momentum
-            with new momemtum parameter: momentum value.
-        """
+        """At the start of every iteration, the model should be updated to generate momentum."""
         super().train_step_start(config)
         if not self.current_round > Config().trainer.rounds:
             update_momentum(
