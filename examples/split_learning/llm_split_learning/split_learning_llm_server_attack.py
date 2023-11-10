ba298cad74ed6f7f3fb4610d3fe8a168ea85854a
Dixi Yao <pspo-qfl@163.com>
2023-11-09 22:45:30 -0500

    added attacking process

--- examples/split_learning/llm_split_learning/split_learning_llm_server_attack.py
+++ examples/split_learning/llm_split_learning/split_learning_llm_server_attack.py
@@ -2,6 +2,7 @@
 A dishonest server which will try to attack and reconstruct user private data
     with the received intermediate features.
 """
+import multiprocessing as mp
 from plato.servers import split_learning as split_learning_server
 from plato.config import Config
 
@@ -11,6 +12,31 @@ class DishonestServer(split_learning_server.Server):
     A dishonest server will decide whether to attack based on the given attacking interval.
     """
 
+    def __init__(
+        self, model=None, datasource=None, algorithm=None, trainer=None, callbacks=None
+    ):
+        super().__init__(model, datasource, algorithm, trainer, callbacks)
+        self.bleu_score = 0.0
+        self.rouge_score = 0.0
+        self.attack_accuracy = 0.0
+        self.attack_started = False
+
+    def attack(self, update):
+        """
+        Conduct the attack in the background. Otherwise, the client will notice
+            as the aggregation time is abnormally long.
+        """
+        self.attack_started = True
+        intermediate_features, labels = update
+        reconstructed_token_ids = self.trainer.attack(intermediate_features)
+        evaluation_metrics = self.trainer.evaluate_attack(
+            reconstructed_token_ids, labels
+        )
+        self.bleu_score = evaluation_metrics["BLEU"]
+        self.rouge_score = evaluation_metrics["ROGUE"]
+        self.attack_accuracy = evaluation_metrics["attack accuracy"]
+        self.attack_started = False
+
     async def aggregate_weights(self, updates, baseline_weights, weights_received):
         """Aggregate weight updates from the clients or train the model."""
         await super().aggregate_weights(updates, baseline_weights, weights_received)
@@ -19,7 +45,20 @@ class DishonestServer(split_learning_server.Server):
         if (
             report.type == "features"
             and self.current_round % Config().parameters.attack.interval == 0
+            and not self.attack_started
         ):
             # probably add some concurrency flag
-            intermediate_features, __ = update
-            reconstructed_words = self.trainer.attack(intermediate_features)
+            proc = mp.Process(
+                target=self.attack,
+                args=(update),
+            )
+            proc.start()
+
+    def get_logged_items(self):
+        """Get items to be logged by the LogProgressCallback class in a .csv file."""
+        logged_items = super().get_logged_items()
+        logged_items["BLEU"] = self.bleu_score
+        logged_items["ROGUE"] = self.rouge_score
+        logged_items["attack accuracy"] = self.attack_accuracy
+
+        return logged_items
