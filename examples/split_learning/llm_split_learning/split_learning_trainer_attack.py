ccd2b5f408bb7644c65614acc9038de9c5f77b3c
Dixi Yao <pspo-qfl@163.com>
2023-11-11 21:33:54 -0500

    removed method input id as no gradients will be added.

--- examples/split_learning/llm_split_learning/split_learning_trainer_attack.py
+++ examples/split_learning/llm_split_learning/split_learning_trainer_attack.py
@@ -34,7 +34,9 @@ class DishonestTrainer(HonestTrainer):
         Train models for attack for one step.
         """
         if attack_parameters.method == "input_ids":
-            outputs = self.model.guessed_client_model(input_ids=reconstructed_data)
+            outputs = self.model.guessed_client_model(
+                input_ids=reconstructed_data.long()
+            )
         else:
             outputs = self.model.guessed_client_model(inputs_embeds=reconstructed_data)
         loss = torch.nn.functional.mse_loss(
@@ -51,15 +53,9 @@ class DishonestTrainer(HonestTrainer):
         """
         self.model.calibrate_guessed_client()
         attack_parameters = Config().parameters.attack
-        assert attack_parameters.method in ["input_ids,embeddings"]
-        if attack_parameters.method == "input_ids":
-            reconstructed_data = torch.zeros(
-                (intermediate_features.shape[0], intermediate_features.shape[1])
-            ).requires_grad_(True)
-        else:
-            reconstructed_data = torch.zeros(
-                intermediate_features.shape
-            ).requires_grad_(True)
+        reconstructed_data = torch.zeros(intermediate_features.shape).requires_grad_(
+            True
+        )
 
         # Assign optimizer for attackings
         optimizer_guessed_client = torch.optim.SGD(
@@ -118,30 +114,29 @@ class DishonestTrainer(HonestTrainer):
             torch.device("cpu")
         )
         intermediate_features = intermediate_features.detach().cpu()
-        # if attack method is embedding, we will generate the reconstructed input ids
+        # We will generate the reconstructed input ids
         #   from the reconstructed embeddings.
-        if attack_parameters == "embedding":
-            embedding_weights = self.model.guessed_client.wte.weight.data
-            embedding_weights = embedding_weights.reshape(
-                1, 1, embedding_weights.size(0), embedding_weights.size(1)
-            ).expand(
-                reconstructed_data.size(0),
-                reconstructed_data.size(1),
-                embedding_weights.size(0),
-                embedding_weights.size(1),
-            )
-            reconstructed_data = reconstructed_data.reshape(
-                reconstructed_data.size(0),
-                reconstructed_data.size(1),
-                1,
-                reconstructed_data.size(2),
-            )
-            distance = torch.norm(
-                reconstructed_data - self.model.guessed_client.wte.weight.data,
-                dim=3,
-                p=2,
-            )
-            reconstructed_data = distance.argmin(dim=2)
+        embedding_weights = self.model.guessed_client.wte.weight.data
+        embedding_weights = embedding_weights.reshape(
+            1, 1, embedding_weights.size(0), embedding_weights.size(1)
+        ).expand(
+            reconstructed_data.size(0),
+            reconstructed_data.size(1),
+            embedding_weights.size(0),
+            embedding_weights.size(1),
+        )
+        reconstructed_data = reconstructed_data.reshape(
+            reconstructed_data.size(0),
+            reconstructed_data.size(1),
+            1,
+            reconstructed_data.size(2),
+        )
+        distance = torch.norm(
+            reconstructed_data - self.model.guessed_client.wte.weight.data,
+            dim=3,
+            p=2,
+        )
+        reconstructed_data = distance.argmin(dim=2)
 
         reconstructed_data = reconstructed_data.long()
         labels = labels.to(self.device)
@@ -157,10 +152,8 @@ class DishonestTrainer(HonestTrainer):
         self.sample_counts += reconstructed_data.size(0)
         evaluation_metrics["attack_accuracy"] = self.accuracy_sum / self.sample_counts
 
-        predicted_text = self.tokenizer.decode(
-            reconstructed_data.detach().cpu().numpy().tolist()
-        )
-        ground_truth = self.tokenizer.decode(labels.detach().cpu().numpy().tolist())
+        predicted_text = self.tokenizer.batch_decode(reconstructed_data.detach().cpu())
+        ground_truth = self.tokenizer.batch_decode(labels.detach().cpu())
         self.rouge_score.update(predicted_text, ground_truth)
         evaluation_metrics["ROUGE"] = self.rouge_score.compute()
         return evaluation_metrics
