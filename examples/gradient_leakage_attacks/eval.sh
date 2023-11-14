24054e3731152bd0d28e5a3136dd38b903eca8d3
Fei Wang <silviafey.wang@utoronto.ca>
2023-11-14 15:10:21 -0500

    Remove unnecessary files

--- examples/gradient_leakage_attacks/eval.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --time=1:00:00
-#SBATCH --cpus-per-task=12
-#SBATCH --gres=gpu:1
-#SBATCH --mem=72G
-#SBATCH --output=eval.out
-
-python dlg.py -c rec_resnet32_cifar100_eval.yml
