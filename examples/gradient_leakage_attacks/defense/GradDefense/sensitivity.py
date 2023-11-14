de40803b800340764bfd4df18b61d3be3c8e17bf
Fei Wang <silviafey.wang@utoronto.ca>
2023-11-14 16:57:24 -0500

    Reformat files

--- examples/gradient_leakage_attacks/defense/GradDefense/sensitivity.py
+++ examples/gradient_leakage_attacks/defense/GradDefense/sensitivity.py
@@ -3,14 +3,15 @@ import torch.nn as nn
 from torch.utils.data.dataloader import DataLoader
 
 
-def compute_sens(model: nn.Module,
-                 rootset_loader: DataLoader,
-                 device: torch.device,
-                 loss_fn=nn.CrossEntropyLoss()):
-
+def compute_sens(
+    model: nn.Module,
+    rootset_loader: DataLoader,
+    device: torch.device,
+    loss_fn=nn.CrossEntropyLoss(),
+):
     x, y = next(iter(rootset_loader))
-    #print('x shape is', x.shape)
-    #print('y shape is', y.shape)
+    # print('x shape is', x.shape)
+    # print('y shape is', y.shape)
 
     x = x.to(device).requires_grad_()
     y = y.to(device)
@@ -22,17 +23,19 @@ def compute_sens(model: nn.Module,
         pred = model(x)
     loss = loss_fn(pred, y)
     # Backward propagation
-    dy_dx = torch.autograd.grad(outputs=loss,
-                                inputs=model.parameters(),
-                                create_graph=True)
+    dy_dx = torch.autograd.grad(
+        outputs=loss, inputs=model.parameters(), create_graph=True
+    )
 
     vector_jacobian_products = []
     for layer in dy_dx:
         # Input-gradient Jacobian
-        d2y_dx2 = torch.autograd.grad(outputs=layer,
-                                      inputs=x,
-                                      grad_outputs=torch.ones_like(layer),
-                                      retain_graph=True)[0]
+        d2y_dx2 = torch.autograd.grad(
+            outputs=layer,
+            inputs=x,
+            grad_outputs=torch.ones_like(layer),
+            retain_graph=True,
+        )[0]
         vector_jacobian_products.append(d2y_dx2.detach().clone())
 
     sensitivity = []
