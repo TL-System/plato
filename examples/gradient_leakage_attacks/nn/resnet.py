6b10fac0e223b882994a377a615d2ff70f13a1eb
Fei Wang <silviafey.wang@utoronto.ca>
2023-12-04 14:22:35 -0500

    Fixed bugs of incompatible devices

--- examples/gradient_leakage_attacks/nn/resnet.py
+++ examples/gradient_leakage_attacks/nn/resnet.py
@@ -130,7 +130,7 @@ class Model(torchvision.models.ResNet):
 
         x = self.pool(x)
         x = torch.flatten(x, 1)
-        feature = x.clone().detach()
+        feature = x.clone()
         x = self.fc(x)
 
         return x, feature
