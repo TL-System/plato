dc39f48c20fd56e3fac1a73ec6bfb2d3f6b7a3b3
Fei Wang <silviafey.wang@utoronto.ca>
2024-03-04 15:34:33 -0500

    Incorporated ViT into DLG.

--- examples/gradient_leakage_attacks/dlg_model.py
+++ examples/gradient_leakage_attacks/dlg_model.py
@@ -6,7 +6,6 @@ which will be used in the defense Soteria.
 
 from plato.config import Config
 from typing import Union
-from transformers import ViTForImageClassification
 
 from nn import (
     lenet,
@@ -25,10 +24,8 @@ def get(**kwargs: Union[str, dict]):
 
     if model_name.split("_")[0] == "resnet":
         return resnet.get(model_name=model_name)
-    
-    if model_name == "vit":
-        return ViTForImageClassification.from_pretrained(
-                    "google/vit-base-patch16-224-in21k", num_labels=Config().parameters.model.num_classes
-                )
+
+    if Config().trainer.model_type == "vit":
+        return None
 
     raise ValueError(f"No such model: {model_name}")
