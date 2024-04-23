61570c07c349982cdd07a136c8d5c4f1c4e90094
Yufei-Kang <41565676+Yufei-Kang@users.noreply.github.com>
2024-04-23 10:41:29 -0400

    Implemented Lid-based label correction module.

--- examples/customized_client_training/noisy/toy_run.py
+++ examples/customized_client_training/noisy/toy_run.py
@@ -9,11 +9,13 @@ from plato.clients import simple
 def main():
     if hasattr(Config().data, "noise"):
         datasource = NoisyDataSource
+        callbacks = [SetupPseudoLabelCallback, EvalPseudoLabelCallback]
     else:
         datasource = None
+        callbacks = None
         
     trainer = toy_trainer.Trainer
-    client = simple.Client(datasource=datasource, trainer=trainer, callbacks=[SetupPseudoLabelCallback, EvalPseudoLabelCallback])
+    client = simple.Client(datasource=datasource, trainer=trainer, callbacks=callbacks)
     server = fedavg.Server(datasource=datasource, trainer=trainer)
     server.run(client)
 
