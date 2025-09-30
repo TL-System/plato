66ae1583e9971ba1c884cd9a66f3493503eb443f
Yuting Zhang <ytlluvia.zhang@mail.utoronto.ca>
2025-09-29 22:50:53 -0400

    Cleaned up algorithm registry imports and structure
    - Remove mindspore and tenserflow frameworks, specific algorithm references

--- plato/algorithms/registry.py
+++ plato/algorithms/registry.py
@@ -4,41 +4,27 @@ The registry for algorithms that contains framework-specific implementations.
 Having a registry of all available classes is convenient for retrieving an instance
 based on a configuration at run-time.
 """
+
 import logging
 
 from plato.config import Config
 
-if hasattr(Config().trainer, "use_mindspore"):
-    from plato.algorithms.mindspore import (
-        fedavg as fedavg_mindspore,
-        mistnet as mistnet_mindspore,
-    )
 
-    registered_algorithms = {
-        "fedavg": fedavg_mindspore.Algorithm,
-        "mistnet": mistnet_mindspore.Algorithm,
-    }
+from plato.algorithms import (
+    fedavg,
+    mistnet,
+    fedavg_gan,
+    fedavg_personalized,
+    split_learning,
+)
 
-elif hasattr(Config().trainer, "use_tensorflow"):
-    from plato.algorithms.tensorflow import fedavg as fedavg_tensorflow
-
-    registered_algorithms = {"fedavg": fedavg_tensorflow.Algorithm}
-else:
-    from plato.algorithms import (
-        fedavg,
-        mistnet,
-        fedavg_gan,
-        fedavg_personalized,
-        split_learning,
-    )
-
-    registered_algorithms = {
-        "fedavg": fedavg.Algorithm,
-        "mistnet": mistnet.Algorithm,
-        "fedavg_gan": fedavg_gan.Algorithm,
-        "fedavg_personalized": fedavg_personalized.Algorithm,
-        "split_learning": split_learning.Algorithm,
-    }
+registered_algorithms = {
+    "fedavg": fedavg.Algorithm,
+    "mistnet": mistnet.Algorithm,
+    "fedavg_gan": fedavg_gan.Algorithm,
+    "fedavg_personalized": fedavg_personalized.Algorithm,
+    "split_learning": split_learning.Algorithm,
+}
 
 
 def get(trainer=None):
