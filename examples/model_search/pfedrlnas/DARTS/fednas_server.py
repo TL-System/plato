96725fd8f92a82db4a266bd1ea4650afe90ab278
Yuting Zhang <ytlluvia.zhang@mail.utoronto.ca>
2025-10-03 16:04:32 -0400

    Fixed AttributeError in DARTS. - AttributeError: 'Server' object has no attribute 'subnets_config'

--- examples/model_search/pfedrlnas/DARTS/fednas_server.py
+++ examples/model_search/pfedrlnas/DARTS/fednas_server.py
@@ -1,6 +1,7 @@
 """
 Implement new algorithm: personalized federarted NAS.
 """
+
 import logging
 import pickle
 
@@ -18,9 +19,7 @@ class Server(fedavg.Server):
 
         return server_response
 
-    async def aggregate_weights(
-        self, updates, baseline_weights, weights_received
-    ):  # pylint: disable=unused-argument
+    async def aggregate_weights(self, updates, baseline_weights, weights_received):  # pylint: disable=unused-argument
         """Aggregates weights of models with different architectures."""
         masks_normal = [update.report.mask_normal for update in updates]
         masks_reduce = [update.report.mask_reduce for update in updates]
@@ -71,7 +70,7 @@ class Server(fedavg.Server):
                 logging.info("the config of client %s is %s", str(i), str(cfg))
                 cfgs.append(cfg)
         # Use model_path if available, otherwise use default models/pretrained directory
-        if hasattr(Config().server, 'model_path'):
+        if hasattr(Config().server, "model_path"):
             model_dir = Config().server.model_path
         else:
             model_dir = "./models/pretrained"
@@ -80,14 +79,24 @@ class Server(fedavg.Server):
             pickle.dump((cfgs), file)
 
     def save_to_checkpoint(self) -> None:
+        # Similar way used in server_will_close
+        cfgs = []
+        for i in range(1, Config().clients.total_clients + 1):
+            cfg = self.algorithm.model.genotype(
+                self.algorithm.model.alphas_normal[i - 1],
+                self.algorithm.model.alphas_reduce[i - 1],
+            )
+            if cfg:
+                cfgs.append(cfg)
+
         # Use model_path if available, otherwise use default models/pretrained directory
-        if hasattr(Config().server, 'model_path'):
+        if hasattr(Config().server, "model_path"):
             model_dir = Config().server.model_path
         else:
             model_dir = "./models/pretrained"
         save_config = f"{model_dir}/subnet_configs.pickle"
         with open(save_config, "wb") as file:
-            pickle.dump(self.subnets_config, file)
+            pickle.dump(cfgs, file)
         save_config = f"{model_dir}/baselines.pickle"
         with open(save_config, "wb") as file:
             pickle.dump(self.algorithm.model.baseline, file)
